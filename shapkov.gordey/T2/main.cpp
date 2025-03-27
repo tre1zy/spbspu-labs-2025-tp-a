85b7adab44b6d60435683d5908f39e6cf842111b
TheEmfield <emfield337@gmail.com>
2025-03-27 17:46:52 +0300

    meow meow

--- shapkov.gordey/T2/main.cpp
+++ shapkov.gordey/T2/main.cpp
@@ -1,28 +1,33 @@
 #include <iostream>
-#include "types.hpp"
-#include <sstream>
-#include <delimiter.hpp>
 #include "dataStruct.hpp"
+#include <iterator>
+#include <vector>
+#include <limits>
+#include <algorithm>
 
 int main()
 {
-  shapkov::DataStruct data;
-  //std::istringstream input("(:N -23 :D 36  :)");
-  //std::istringstream input1("5.45d");
-  //std::istringstream input2("\"Data\":");
-  //std::istringstream input3("(:N -23 :D 36  :) 5.45 \"Data\"");
-  //input3 >> shapkov::ratioIO{ data.key2 } >> shapkov::doubleScientificIO{ data.key1 } >> shapkov::stringIO{ data.key3 };
-  std::cin >> data;
-  std::cout << data << '\n';
-  std::cout << data.key1 << '\n';
-  //std::istringstream input4("key r");
-  //input4 >> shapkov::labelIO{ "key" } >> shapkov::delimiterIO{ 'f' };
-  if (std::cin)
+  std::vector< shapkov::DataStruct > data;
+  while (!std::cin.eof())
   {
-    std::cout << "all right\n";
+    std::copy(
+      std::istream_iterator< shapkov::DataStruct >(std::cin),
+      std::istream_iterator< shapkov::DataStruct >(),
+      std::back_inserter(data)
+    );
+    if (std::cin.fail())
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
   }
-  else
-  {
-    std::cout << "bad job\n";
-  }
+  std::sort(data.begin(), data.end(), shapkov::dataComparator);
+  std::cout << "Data:\n";
+  std::copy(
+    std::begin(data),
+    std::end(data),
+    std::ostream_iterator< shapkov::DataStruct >(std::cout, "\n")
+  );
+
+  return 0;
 }
