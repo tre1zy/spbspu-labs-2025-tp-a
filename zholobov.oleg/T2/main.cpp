e8955ab69b5158130e8a87fb07363ff037b0b2af
Zholobov Oleg <zholobov.of@edu.spbstu.ru>
2025-04-01 23:23:30 +0300

    added some code
    declarations for operator>>/<< for [RAT LSP]
    reading data from std::cin using std::copy

--- zholobov.oleg/T2/main.cpp
+++ zholobov.oleg/T2/main.cpp
@@ -1,5 +1,6 @@
-
+#include <algorithm>
 #include <iostream>
+#include <iterator>
 #include <string>
 #include <utility>
 #include <vector>
@@ -10,10 +11,20 @@ struct DataStruct {
   std::string key3;
 };
 
+std::istream& operator>>(std::istream& input, std::pair< long long, unsigned long long >& value);
+
+std::ostream& operator<<(std::ostream& output, const std::pair< long long, unsigned long long >& value);
+
 std::istream& operator>>(std::istream& input, DataStruct& value);
+
 std::ostream& operator<<(std::ostream& output, const DataStruct& value);
 
 int main()
 {
+  std::vector< DataStruct > data;
+  std::copy(std::istream_iterator< DataStruct >(std::cin),
+            std::istream_iterator< DataStruct >(),
+            std::back_inserter(data));
+
   return 0;
 }
