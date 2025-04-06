0b6428db391adcb631767b0ba943316ea4d23f21
Ksenia-rgb <ksenialymova@mail.ru>
2025-04-06 22:24:58 +0300

    CG

--- alymova.ksenia/T2/main.cpp
+++ alymova.ksenia/T2/main.cpp
@@ -10,14 +10,14 @@ int main()
   using namespace alymova;
   StreamGuard os_sg(std::cout);
   StreamGuard is_sg(std::cin);
-  
+
   std::vector< DataStruct > data;
   std::copy(
     std::istream_iterator< DataStruct >(std::cin),
     std::istream_iterator< DataStruct >(),
     std::back_inserter(data)
   );
-  
+
   std::copy(
     std::begin(data),
     std::end(data),
