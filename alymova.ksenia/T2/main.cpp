b76177bf1912781959bb14407691cf1b275fbb7e
Ksenia-rgb <ksenialymova@mail.ru>
2025-04-07 17:06:01 +0300

    try fix VG sort

--- alymova.ksenia/T2/main.cpp
+++ alymova.ksenia/T2/main.cpp
@@ -13,11 +13,31 @@ int main()
   StreamGuard is_sg(std::cin);
 
   std::vector< DataStruct > data;
-  std::copy(
-    std::istream_iterator< DataStruct >(std::cin),
-    std::istream_iterator< DataStruct >(),
-    std::back_inserter(data)
-  );
+  while (!std::cin.eof())
+  {
+    DataStruct d;
+    std::cin >> d;
+    data.push_back(d);
+    if (std::cin.fail())
+    {
+      data.pop_back();
+      std::cin.clear(std::cin.rdstate() ^ std::ios_base::failbit);
+    }
+  }
+  /*while (!std::cin.eof())
+  {
+    std::copy(
+      std::istream_iterator< DataStruct >(std::cin),
+      std::istream_iterator< DataStruct >(),
+      std::back_inserter(data)
+    );
+    if (std::cin.fail())
+    {
+      data.pop_back();
+      std::cin.clear(std::cin.rdstate() ^ std::ios_base::failbit);
+    }
+  }*/
+  std::cout << data.size();
   std::sort(data.begin(), data.end(), comparator);
   std::copy(
     std::begin(data),
