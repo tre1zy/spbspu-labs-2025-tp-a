b9fcaa7fe4d09b895ec76062844766375603b0a2
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2025-04-08 23:16:30 +0300

    input

--- maslov.aleksey/T2/main.cpp
+++ maslov.aleksey/T2/main.cpp
@@ -1,2 +1,35 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <iterator>
+
+struct DataStruct
+ {
+  double key1;
+  char key2;
+  std::string key3;
+ };
+
 int main()
-{}
+{
+  std::vector< DataStruct > data;
+  while (!std::cin.eof())
+  {
+    std::copy
+    (
+      std::istream_iterator< DataStruct >(std::cin),
+      std::istream_iterator< DataStruct >(),
+      std::back_inserter(data)
+    );
+    if (!std::cin)
+    {
+      std::cin.clear();
+    }
+  }
+  std::copy
+  (
+    std::begin(data),
+    std::end(data),
+    std::ostream_iterator< DataStruct >(std::cout, "\n")
+  );
+}
