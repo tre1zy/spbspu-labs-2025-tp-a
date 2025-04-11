b5e0e1c2d97a2a013561963c6d3aabdd65407118
abramov_vladislav_ <vladabramov06@yandex.ru>
2025-04-11 13:06:04 +0300

    implementation without random places of keys

--- abramov.vladislav/T2/datastruct.cpp
+++ abramov.vladislav/T2/datastruct.cpp
@@ -17,3 +17,105 @@ std::ostream &abramov::operator<<(std::ostream &out, const DataStruct &data)
   out << ":)";
   return out;
 }
+
+std::istream &abramov::operator>>(std::istream &in, DelimiterIO &&dest)
+{
+  std::istream::sentry s(in);
+  if (!s)
+  {
+    return in;
+  }
+  char c = '0';
+  in >> c;
+  if (in && (c != dest.delim))
+  {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
+std::istream &abramov::operator>>(std::istream &in, DoubleIO &&dest)
+{
+  std::istream::sentry s(in);
+  if (!s)
+  {
+    return in;
+  }
+  in >> dest.ref >> DelimiterIO{ 'd' };
+  return in;
+}
+
+std::istream &abramov::operator>>(std::istream &in, UllIO &&dest)
+{
+  std::istream::sentry s(in);
+  if (!s)
+  {
+    return in;
+  }
+  in >> dest.ref >> DelimiterIO{ 'u' } >> DelimiterIO{ 'l' } >> DelimiterIO{ 'l' };
+  return in;
+}
+std::istream &abramov::operator>>(std::istream &in, StringIO &&dest)
+{
+  std::istream::sentry s(in);
+  if (!s)
+  {
+    return in;
+  }
+  return std::getline(in, dest.ref, ' ');
+}
+
+std::istream &abramov::operator>>(std::istream &in, KeyIO &&dest)
+{
+  std::istream::sentry s(in);
+  if (!s)
+  {
+    return in;
+  }
+  std::string data = "";
+  if ((in >> StringIO{ data }) && (data != dest.key))
+  {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
+std::istream &abramov::operator>>(std::istream &in, DataStruct &data)
+{
+  std::istream::sentry s(in);
+  if (!s)
+  {
+    return in;
+  }
+  DataStruct input;
+  in >> DelimiterIO{ '(' } >> DelimiterIO{ ':' };
+  in >> KeyIO{ "key1" };
+  in >> DoubleIO{ data.key1 };
+  in >> DelimiterIO{ ':' } >> KeyIO{ "key2" };
+  in >> UllIO{ data.key2 } >> DelimiterIO{ ':' } >> KeyIO{ "key3" };
+  std::string tmp;
+  std::getline(in, tmp, ':');
+  tmp = tmp.substr(1, tmp.size() - 2);
+  data.key3 = tmp;
+  in >> DelimiterIO{ ')' };
+  return in;
+}
+
+bool abramov::operator<(const DataStruct &lhs, const DataStruct &rhs)
+{
+  if (lhs.key1 == lhs.key1)
+  {
+    if (lhs.key2 == rhs.key2)
+    {
+      return lhs.key3.size() < rhs.key3.size();
+    }
+    else
+    {
+      return lhs.key2 < rhs.key2;
+    }
+  }
+  else
+  {
+    return lhs.key1 < rhs.key1;
+  }
+}
--- abramov.vladislav/T2/datastruct.hpp
+++ abramov.vladislav/T2/datastruct.hpp
@@ -10,7 +10,37 @@ namespace abramov
     unsigned long long int key2;
     std::string key3;
   };
-
   std::ostream &operator<<(std::ostream &out, const DataStruct &data);
+  std::istream &operator>>(std::istream &in, DataStruct &data);
+  bool operator<(const DataStruct &lhs, const DataStruct &rhs);
+  struct DelimiterIO
+  {
+    char delim;
+  };
+  std::istream &operator>>(std::istream &in, DelimiterIO &&dest);
+
+  struct DoubleIO
+  {
+    double &ref;
+  };
+  std::istream &operator>>(std::istream &in, DoubleIO &&dest);
+
+  struct UllIO
+  {
+    unsigned long long int &ref;
+  };
+  std::istream &operator>>(std::istream &in, UllIO &&dest);
+
+  struct StringIO
+  {
+    std::string &ref;
+  };
+  std::istream &operator>>(std::istream &in, StringIO &&dest);
+
+  struct KeyIO
+  {
+    std::string key;
+  };
+  std::istream &operator>>(std::istream &in, KeyIO &&dest);
 }
 #endif
