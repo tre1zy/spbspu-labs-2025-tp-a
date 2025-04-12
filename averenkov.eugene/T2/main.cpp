34f93fe7676afe039200f0d48906a6d7c39d1db6
Savishna <bravissimo28@mail.ru>
2025-04-12 12:55:26 +0300

    added main and some operators

--- averenkov.eugene/T2/main.cpp
+++ averenkov.eugene/T2/main.cpp
@@ -1,2 +1,29 @@
+#include "DataStruct.h"
+#include <vector>
+#include <iterator>
+#include <algorithm>
+
 int main()
-{}
+{
+  std::vector<DataStruct> data;
+  std::istream_iterator<DataStruct> inputBegin(std::cin);
+  std::istream_iterator<DataStruct> inputEnd;
+  while (inputBegin != inputEnd)
+  {
+    data.push_back(*inputBegin);
+    if (!std::cin)
+    {
+      std::cin.clear();
+      std::string line;
+      std::getline(std::cin, line);
+    }
+    ++inputBegin;
+  }
+  std::sort(data.begin(), data.end());
+  std::copy(
+    data.begin(),
+    data.end(),
+    std::ostream_iterator<DataStruct>(std::cout, "\n")
+  );
+  return 0;
+}
