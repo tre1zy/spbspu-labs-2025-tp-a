93b6230603297100b0b762c3f18cddff1e71f879
Nekich06 <p.nikita.73526@gmail.com>
2025-04-14 00:01:07 +0300

    Removed sorting

--- petrov.nikita/T2/main.cpp
+++ petrov.nikita/T2/main.cpp
@@ -23,7 +23,6 @@ int main()
     std::cin.clear();
     std::cin.ignore(1024, '\n');
   }
-  petrov::sort(data);
   std::copy(
       std::begin(data),
       std::end(data),
@@ -34,79 +33,4 @@ int main()
 void petrov::sort(std::vector< DataStruct > & vector)
 {
   bool swapped = false;
-  for (size_t k = 1; k <= vector.size(); k++)
-  {
-    if (vector[k].key1 != vector [k - 1].key1)
-    {
-      for (size_t i = 0; i < vector.size() - 1; i++)
-      {
-        swapped = false;
-        for (size_t j = 0; j < vector.size() - i - 1; j++)
-        {
-          if (vector[j].key1 > vector[j + 1].key1)
-          {
-            std::swap(vector[j], vector[j + 1]);
-            swapped = true;
-          }
-        }
-        if (!swapped)
-        {
-          break;
-        }
-      }
-      return;
-    }
-  }
-  for (size_t k = 1; k <= vector.size(); k++)
-  {
-    if (vector[k].key2 != vector [k - 1].key2)
-    {
-      if (vector[k].key1 != vector [k - 1].key1)
-      {
-        for (size_t i = 0; i < vector.size() - 1; i++)
-        {
-          swapped = false;
-          for (size_t j = 0; j < vector.size() - i - 1; j++)
-          {
-            if (vector[j].key2 > vector[j + 1].key2)
-            {
-              std::swap(vector[j], vector[j + 1]);
-              swapped = true;
-            }
-          }
-          if (!swapped)
-          {
-            break;
-          }
-        }
-        return;
-      }
-    }
-  }
-  for (size_t k = 1; k <= vector.size(); k++)
-  {
-    if (vector[k].key3.length() != vector [k - 1].key3.length())
-    {
-      if (vector[k].key1 != vector [k - 1].key1)
-      {
-        for (size_t i = 0; i < vector.size() - 1; i++)
-        {
-          swapped = false;
-          for (size_t j = 0; j < vector.size() - i - 1; j++)
-          {
-            if (vector[j].key3.length() > vector[j + 1].key3.length())
-            {
-              std::swap(vector[j], vector[j + 1]);
-              swapped = true;
-            }
-          }
-          if (!swapped)
-          {
-            break;
-          }
-        }
-        return;
-      }
-    }
-  }
 }
