cc00800d5d98f9a26d578fc25db4956ade98c1e9
CuatHimBong <egvika20051@gmail.com>
2025-04-14 02:50:54 +0300

    Added datastruct.hpp

---
+++ kazak.egor/T2/datastruct.hpp
@@ -0,0 +1,50 @@
+#ifndef DATASTRUCT_HPP
+#define DATASTRUCT_HPP
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+namespace kazak
+{
+  struct DataStruct
+  {
+    unsigned long long key1;
+    std::pair< long long, unsigned long long > key2;
+    std::string key3;
+  };
+
+  struct DelimiterIO
+  {
+    char exp;
+  };
+
+  struct ULLHexIO
+  {
+    unsigned long long& ref;
+  };
+
+  struct RationalIO
+  {
+    std::pair<long long, unsigned long long>& ref;
+  };
+
+  struct StringIO
+  {
+    std::string& ref;
+  };
+
+  struct LabelIO
+  {
+    std::string exp;
+  };
+
+  std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
+  std::istream& operator>>(std::istream& in, ULLHexIO&& dest);
+  std::istream& operator>>(std::istream& in, RationalIO&& dest);
+  std::istream& operator>>(std::istream& in, StringIO&& dest);
+  std::istream& operator>>(std::istream& in, LabelIO&& dest);
+  std::istream& operator>>(std::istream& in, DataStruct& dest);
+  std::ostream& operator<<(std::ostream& out, const DataStruct& dest);
+}
+
+#endif
