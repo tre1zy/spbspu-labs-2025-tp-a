fbb0c9991c0e9d149b5fc6b4f928b48560b7357e
fedorov.oleg <fedorovo727@gmail.com>
2025-04-13 16:45:25 +0300

    fixes on out system; choosing only acceptable

--- fedorov.oleg/T2/main.cpp
+++ fedorov.oleg/T2/main.cpp
@@ -1,28 +1,29 @@
-// Variant DBL LIT | CMP LSP
-
 #include "data_struct.hpp"
 
-#include <algorithm>
 #include <iostream>
 #include <iterator>
+#include <limits>
 #include <vector>
+#include <algorithm>
 
 int main()
 {
-  using namespace fedorov;
+  std::cout << "start\n";
+  using fedorov::DataStruct;
+  using input_it_t = std::istream_iterator<DataStruct>;
+  using output_it_t = std::ostream_iterator<DataStruct>;
 
   std::vector<DataStruct> data;
 
-  std::copy(std::istream_iterator<DataStruct>{std::cin}, std::istream_iterator<DataStruct>{},
-            std::back_inserter(data));
-
-  data.erase(
-      std::remove_if(data.begin(), data.end(), [](const DataStruct &) { return std::cin.fail(); }),
-      data.end());
+  while ( !std::cin.eof() ) {
+    std::copy(input_it_t{std::cin}, input_it_t{}, std::back_inserter(data));
+    if ( std::cin.fail() ) {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+  }
 
   std::sort(data.begin(), data.end());
-
-  std::copy(data.begin(), data.end(), std::ostream_iterator<DataStruct>{std::cout, "\n"});
-
+  std::copy(data.begin(), data.end(), output_it_t{std::cout, "\n"});
   return 0;
 }
