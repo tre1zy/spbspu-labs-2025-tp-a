c3cb866e3690f941e8026b2634d5c45b42bc9fc1
Sergey Dribas <serdribas@gmail.com>
2025-04-13 19:39:21 +0300

    release stream guard

---
+++ dribas.sergey/T2/streamGuard.hpp
@@ -0,0 +1,21 @@
+#ifndef SCOPEGUARD_HPP
+#define SCOPEGUARD_HPP
+#include <ios>
+
+namespace dribas
+{
+  class StreamGuard
+  {
+  public:
+    StreamGuard(std::basic_ios< char >&);
+    ~StreamGuard();
+  private:
+    std::basic_ios< char >::fmtflags fmtflags_;
+    std::streamsize width_;
+    std::streamsize precision_;
+    char fill_;
+    std::basic_ios< char >& stream_;
+  };
+}
+
+#endif
