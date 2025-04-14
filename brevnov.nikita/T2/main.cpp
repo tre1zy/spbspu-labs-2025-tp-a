23ff231a451f6768ba3568ae755b60fb39537bf2
brev1ch <flirktv@gmail.com>
2025-04-14 15:33:24 +0300

    main.cpp

--- brevnov.nikita/T2/main.cpp
+++ brevnov.nikita/T2/main.cpp
@@ -1,6 +1,31 @@
+#include <algorithm>
+#include <vector>
+#include <limits>
+#include <iterator>
 
 int main()
 {
-  
-  
+  std::vector< brevnov::DataStruct > data;
+  while(!std::cin.eof())
+  {
+    std::copy
+    (
+      std::istream_iterator< brevnov::DataStruct >(std::cin),
+      std::istream_iterator< brevnov::DataStruct >(),
+      std::back_inserter(data)
+    );
+    if (!std::cin)
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+  std::sort(data.begin(), data.end(), brevnov::compare);
+  std::copy
+  (
+    std::begin(data),
+    std::end(data),
+    std::ostream_iterator< brevnov::DataStruct >(std::cout, "\n")
+  );
+  return 0;
 }
