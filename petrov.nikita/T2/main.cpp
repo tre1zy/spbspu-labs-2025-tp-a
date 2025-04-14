5bcd606978bd20a3f74e5ea5d2e2f33c658857bf
Nekich06 <p.nikita.73526@gmail.com>
2025-04-14 10:28:10 +0300

    Implemented Another Sorting

--- petrov.nikita/T2/main.cpp
+++ petrov.nikita/T2/main.cpp
@@ -7,6 +7,9 @@
 namespace petrov
 {
   void sort(std::vector< DataStruct > & vector);
+  void sortByFirstKey(std::vector< DataStruct > & vector);
+  void sortBySecondKey(std::vector< DataStruct > & vector);
+  void sortByThirdKey(std::vector< DataStruct > & vector);
 }
 
 int main()
@@ -23,6 +26,7 @@ int main()
     std::cin.clear();
     std::cin.ignore(1024, '\n');
   }
+  sort(data);
   std::copy(
       std::begin(data),
       std::end(data),
@@ -30,3 +34,82 @@ int main()
   );
 }
 
+void petrov::sort(std::vector < DataStruct > & vector)
+{
+  sortByFirstKey(vector);
+  sortBySecondKey(vector);
+  sortByThirdKey(vector);
+}
+
+void petrov::sortByFirstKey(std::vector< DataStruct > & vector)
+{
+  bool swapped = false;
+  for (size_t i = 0; i < vector.size() - 1; i++) 
+  {
+    swapped = false;
+    for (size_t j = 0; j < vector.size() - i - 1; j++) 
+    {
+      if (vector[j].key1 > vector[j + 1].key1) 
+      {
+        std::swap(vector[j].key1, vector[j + 1].key1);
+        swapped = true;
+      }
+    }
+    if (!swapped)
+    {
+      break;
+    }
+  }
+}
+
+void petrov::sortBySecondKey(std::vector< DataStruct > & vector)
+{
+  size_t k = 0;
+  while (vector[k].key1 == vector[k + 1].key2)
+  {
+    ++k;
+  }
+  bool swapped = false;
+  for (size_t i = k; i < vector.size() - 1; i++) 
+  {
+    swapped = false;
+    for (size_t j = k; j < vector.size() - i - 1; j++) 
+    {
+      if (vector[j].key2 > vector[j + 1].key2) 
+      {
+        std::swap(vector[j].key2, vector[j + 1].key2);
+        swapped = true;
+      }
+    }
+    if (!swapped)
+    {
+      break;
+    }
+  }
+}
+
+void petrov::sortByThirdKey(std::vector< DataStruct > & vector)
+{
+  size_t k = 0;
+  while (vector[k].key2 == vector[k + 1].key2)
+  {
+    ++k;
+  }
+  bool swapped = false;
+  for (size_t i = k; i < vector.size() - 1; i++) 
+  {
+    swapped = false;
+    for (size_t j = k; j < vector.size() - i - 1; j++) 
+    {
+      if (vector[j].key3.length() > vector[j + 1].key3.length()) 
+      {
+        std::swap(vector[j].key3, vector[j + 1].key3);
+        swapped = true;
+      }
+    }
+    if (!swapped)
+    {
+      break;
+    }
+  }
+}
