0bce7bf7647be813f62b9e88ae40fdae1dde4a7d
belyaevEDU <dubstephimself@outlook.com>
2025-04-14 23:10:34 +0300

    add stream guard

---
+++ belyaev.veniamin/T2/stream-guard.cpp
@@ -0,0 +1,15 @@
+#include "stream-guard.hpp"
+
+belyaev::StreamGuard::StreamGuard(std::basic_ios< char>& s):
+  s_(s),
+  fill_(s.fill()),
+  precision_(s.precision()),
+  width_(s.width()),
+  flags_(s.flags())
+{}
+
+belyaev::StreamGuard::~StreamGuard()
+{
+  s_.precision(precision_);
+  s_.flags(flags_);
+}
