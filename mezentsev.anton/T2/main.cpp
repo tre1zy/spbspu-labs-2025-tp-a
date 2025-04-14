3c050a1caab2af073007889f688d37a1cf559a68
MezentsevAnton <fortniter135.anton@mail.ru>
2025-04-14 19:46:20 +0300

    it's bilo slojno

---
+++ mezentsev.anton/T2/main.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <vector>
+#include <limits>
+#include <iterator>
+#include "data.h"
+
+int main()
+{
+  std::vector< mezentsev::Data > data;
+  while(!std::cin.eof())
+  {
+    std::copy
+    (
+      std::istream_iterator< mezentsev::Data >(std::cin),
+      std::istream_iterator< mezentsev::Data >(),
+      std::back_inserter(data)
+    );
+    if (!std::cin)
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+  std::sort(data.begin(), data.end(), mezentsev::compare);
+  std::copy
+  (
+    std::begin(data),
+    std::end(data),
+    std::ostream_iterator< mezentsev::Data >(std::cout, "\n")
+  );
+  return 0;
+}
