e95217c7af09e63732e245b2730a23d6ebed4e33
Tuc0Sa1amanka <sparowj765@gmail.com>
2025-04-15 00:30:37 +0300

    implementation of class Data Struct

---
+++ lanovenko.ivan/T2/data_struct.hpp
@@ -0,0 +1,19 @@
+#ifndef DATA_STRUCT_HPP
+#define DATA_STRUCT_HPP
+
+#include <string>
+#include <iostream>
+
+namespace lanovenko
+{
+  struct DataStruct
+  {
+    char key1;
+    std::pair < long long, unsigned long long > key2;
+    std::string key3;
+  };
+  std::istream& operator>>(std::istream& in, DataStruct& dest);
+  std::ostream& operator<<(std::ostream& out, const DataStruct& dest);
+}
+
+#endif
