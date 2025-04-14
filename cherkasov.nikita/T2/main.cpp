8e6ed794411731fc2a86a1a8d3708144d9cf08ad
cherkasoov <cherkasov5509@gmail.com>
2025-04-14 16:12:13 +0300

    ...

--- cherkasov.nikita/T2/main.cpp
+++ cherkasov.nikita/T2/main.cpp
@@ -1,4 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include "dataStruct.hpp"
+#include "stream-guard.hpp"
 
 int main()
-{}
+{
+  using namespace alymova;
+  StreamGuard coutGuard(std::cout);
+  StreamGuard cinGuard(std::cin);
+
+  std::vector<DataStruct> data;
+  std::copy(
+    std::istream_iterator<DataStruct>(std::cin),
+    std::istream_iterator<DataStruct>(),
+    std::back_inserter(data)
+  );
+
+  std::sort(data.begin(), data.end());
+
+  std::copy(
+    data.begin(),
+    data.end(),
+    std::ostream_iterator<DataStruct>(std::cout, "\n")
+  );
+}
