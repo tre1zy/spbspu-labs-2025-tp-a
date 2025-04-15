c4d736ce5bfc265b9a232b6e80c9e8caa6427878
ysffmn <andrewnikonov387@gmail.com>
2025-04-15 20:45:52 +0300

    reversed comparator

--- nikonov.andrew/T2/main.cpp
+++ nikonov.andrew/T2/main.cpp
@@ -5,17 +5,17 @@
 #include "DataStruct.hpp"
 namespace nikonov::detail
 {
-  bool sortComparator(DataStruct& a, DataStruct& b)
+  bool mySortComparator(DataStruct& a, DataStruct& b)
   {
     if (a.key1_ == b.key1_)
     {
       if (a.key2_ == b.key2_)
       {
-        return a.key3_.length() > b.key3_.length();
+        return a.key3_.length() < b.key3_.length();
       }
-      return a.key2_ > b.key2_;
+      return a.key2_ < b.key2_;
     }
-    return a.key1_ > b.key1_;
+    return a.key1_ < b.key1_;
   }
 }
 int main()
@@ -32,6 +32,6 @@ int main()
     std::copy(data_istream_it(std::cin), data_istream_it(), std::back_inserter(dS));
   }
   std::copy(data_istream_it(std::cin), data_istream_it(), std::back_inserter(dS));
-  std::sort(dS.begin(), dS.end(), nikonov::detail::sortComparator);
+  std::sort(dS.begin(), dS.end(), nikonov::detail::mySortComparator);
   std::copy(dS.begin(), dS.end(), std::ostream_iterator< DataStruct >(std::cout, "\n"));
 }
