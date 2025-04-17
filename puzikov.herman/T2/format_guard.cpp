2ec7f02fad2acc56c3eaba431eb8aaf1d7b25184
Herman Puzikov <your_local_baddy@segfaultchan.anonaddy.me>
2025-04-18 00:34:22 +0300

    wrong init order in format guard constructor fix

--- puzikov.herman/T2/format_guard.cpp
+++ puzikov.herman/T2/format_guard.cpp
@@ -2,9 +2,9 @@
 
 puzikov::FormatGuard::FormatGuard(std::basic_ios< char > &s):
   s_(s),
-  fill_(s.fill()),
   precision_(s.precision()),
-  fmt_(s.flags())
+  fmt_(s.flags()),
+  fill_(s.fill())
 {
 }
 
@@ -14,4 +14,3 @@ puzikov::FormatGuard::~FormatGuard()
   s_.precision(precision_);
   s_.flags(fmt_);
 }
-
