ad7fbf3de2ade92c45c7825877a0dff235400d80
Mishaa105 <truhanov2006@mail.ru>
2025-04-17 18:54:21 +0300

    Initial commit

---
+++ trukhanov.mikhail/T2/data_input.cpp
@@ -0,0 +1,93 @@
+#include "data_input.hpp"
+#include "stream_guard.hpp"
+#include <iomanip>
+
+namespace Trukhanov
+{
+    std::istream& operator>>(std::istream& in, DelimiterIO&& dest)
+    {
+        std::istream::sentry sentry(in);
+        if (!sentry)
+        {
+            return in;
+        }
+        char c = '\0';
+        in >> c;
+        if (in && (c != dest.exp))
+        {
+            in.setstate(std::ios::failbit);
+        }
+        return in;
+    }
+
+    std::istream& operator>>(std::istream& in, LabelIO&& dest)
+    {
+        std::istream::sentry sentry(in);
+        if (!sentry) return in;
+        for (char expected : dest.exp) {
+            char actual = '\0';
+            in.get(actual);
+            if (!in || actual != expected) {
+                in.setstate(std::ios::failbit);
+                return in;
+            }
+        }
+        return in;
+    }
+
+
+
+
+    std::istream& operator>>(std::istream& in, OctalULLIO&& dest)
+    {
+        std::istream::sentry sentry(in);
+        if (!sentry)
+        {
+            return in;
+        }
+        StreamGuard guard(in);
+        in >> std::oct >> dest.ref;
+        return in;
+    }
+
+    std::istream& operator>>(std::istream& in, RationalIO&& dest)
+    {
+        std::istream::sentry sentry(in);
+        if (!sentry)
+        {
+            return in;
+        }
+        long long numerator = 0;
+        unsigned long long denominator = 0;
+
+        in >> DelimiterIO{ '(' }
+            >> DelimiterIO{ ':' }
+            >> LabelIO{ "N" }
+            >> numerator
+            >> DelimiterIO{ ':' }
+            >> LabelIO{ "D" }
+            >> denominator
+            >> DelimiterIO{ ':' }
+        >> DelimiterIO{ ')' };
+
+        if (in && denominator != 0)
+        {
+            dest.ref = { numerator, denominator };
+        }
+        else
+        {
+            in.setstate(std::ios::failbit);
+        }
+        return in;
+    }
+
+    std::istream& operator>>(std::istream& in, StringIO&& dest)
+    {
+        std::istream::sentry sentry(in);
+        if (!sentry)
+        {
+            return in;
+        }
+        return std::getline(in >> DelimiterIO{ '"' }, dest.ref, '"');
+    }
+}
---
+++ trukhanov.mikhail/T2/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include "data_struct.hpp"
+
+int main()
+{
+    using Trukhanov::DataStruct;
+
+    std::vector<DataStruct> data;
+    std::string line;
+
+    while (std::getline(std::cin, line))
+    {
+        std::istringstream iss(line);
+        DataStruct ds;
+
+        if (iss >> ds)
+        {
+            data.push_back(ds);
+        }
+        else
+        {
+            std::cerr << "[ERROR] Invalid format: " << line << "\n";
+        }
+
+        iss.clear();
+    }
+
+    std::sort(data.begin(), data.end(), [](const DataStruct& a, const DataStruct& b) {
+        if (a.key1 != b.key1) return a.key1 < b.key1;
+        if (a.key2 != b.key2) return a.key2 < b.key2;
+        return a.key3.length() < b.key3.length();
+        });
+
+    std::copy(
+        data.begin(),
+        data.end(),
+        std::ostream_iterator<DataStruct>(std::cout, "\n")
+    );
+
+    return 0;
+}
