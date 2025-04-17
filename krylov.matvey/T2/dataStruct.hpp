8145cb620dcacb5965fcd272329a38dae1f95728
root <root@kryloff.>
2025-04-17 21:52:20 +0300

    Interface added

--- krylov.matvey/T2/dataStruct.hpp
+++ krylov.matvey/T2/dataStruct.hpp
@@ -10,7 +10,26 @@ namespace krylov
     unsigned long long key2;
     std::string key3;
   };
-  
+  std::ostream &operator<<(std::ostream &out, const DataStruct &data);
+  std::istream &operator>>(std::istream &in, DataStruct &data);
+  bool operator<(const DataStruct &lhs, const DataStruct &rhs);
+  struct DelimiterIO
+  {
+    char delim;
+  };
+  std::istream &operator>>(std::istream &in, DelimiterIO &&dest);
+
+  struct UllIO
+  {
+    unsigned long long int &ref;
+  };
+  std::istream &operator>>(std::istream &in, UllIO &&dest);
+
+  struct StringIO
+  {
+    std::string &ref;
+  };
+  std::istream &operator>>(std::istream &in, StringIO &&dest);
 }
 
 #endif
