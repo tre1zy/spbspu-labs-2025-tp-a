a5f1a195a73c682ddacb1f2b43469b9d43a7fae7
root <root@kryloff.>
2025-04-17 22:14:43 +0300

    Implmenetation of stream ops added

---
+++ krylov.matvey/T2/dataStruct.cpp
@@ -0,0 +1,121 @@
+#include "dataStruct.hpp"
+#include <iomanip>
+#include "streamGuard.hpp"
+
+std::ostream& krylov::operator<<(std::ostream& out, const DataStruct& data)
+{
+  std::ostream::sentry s(out);
+  if (!s)
+  {
+    return out;
+  }
+  StreamGuard guard(out);
+  out << "(";
+  out << ":key1 " << "0b" << data.key1;
+  out << ":key2 " << "0x" << data.key2;
+  out << ":key3 " << "\"" << data.key3 << "\"";
+  out << ":)";
+  return out;
+}
+
+std::istream& krylov::operator>>(std::istream& in, DelimiterIO&& dest)
+{
+  std::istream::sentry s(in);
+  if (!s)
+  {
+    return in;
+  }
+  char c = '0';
+  in >> c;
+  if (in && (c != dest.delim))
+  {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
+std::istream& krylov::operator>>(std::istream& in, UllIO&& dest)
+{
+  std::istream::sentry s(in);
+  if (!s)
+  {
+    return in;
+  }
+  in >> dest.ref;
+  return in;
+}
+std::istream& krylov::operator>>(std::istream& in, StringIO&& dest)
+{
+  std::istream::sentry s(in);
+  if (!s)
+  {
+    return in;
+  }
+  in >> DelimiterIO{ '"' };
+  return std::getline(in, dest.ref, '"');
+}
+
+std::istream& krylov::operator>>(std::istream &in, DataStruct &data)
+{
+  std::istream::sentry s(in);
+  if (!s)
+  {
+    return in;
+  }
+  DataStruct input;
+  in >> DelimiterIO{ '(' } >> DelimiterIO{ ':' };
+  std::string key;
+  constexpr size_t key_numbers = 3;
+  size_t count = 0;
+  while (count < key_numbers && in)
+  {
+    in >> key;
+    if (key == "key1")
+    {
+      in >> UllIO{ input.key1 };
+      in >> DelimiterIO{ ':' };
+      ++count;
+    }
+    else if (key == "key2")
+    {
+      in >> UllIO{ input.key2 };
+      in >> DelimiterIO{ ':' };
+      ++count;
+    }
+    else if (key == "key3")
+    {
+      in >> StringIO{ input.key3 };
+      in >> DelimiterIO{ ':' };
+      ++count;
+    }
+    else
+    {
+      in.setstate(std::ios::failbit);
+    }
+  }
+  in >> DelimiterIO{ ')' };
+  if (in)
+  {
+    data = input;
+  }
+  return in;
+}
+
+bool krylov::operator<(const DataStruct& lhs, const DataStruct& rhs)
+{
+  if (lhs.key1 == rhs.key1)
+  {
+    if (lhs.key2 == rhs.key2)
+    {
+      return lhs.key3.size() < rhs.key3.size();
+    }
+    else
+    {
+      return lhs.key2 < rhs.key2;
+    }
+  }
+  else
+  {
+    return lhs.key1 < rhs.key1;
+  }
+}
