6ec4f6b8f18599a3caee37ea16e46fc5041d8eb8
lee <likachich12@gmail.com>
2025-04-25 21:19:21 +0300

    main cpp

--- hismatova.elizaveta/T2/main.cpp
+++ hismatova.elizaveta/T2/main.cpp
@@ -1,2 +1,18 @@
+#include "DataStruct.hpp"
+
 int main()
-{}
+{
+  std::vector< DataStruct > dataVec;
+  std::copy(std::istream_iterator< DataStruct >(std::cin),
+    std::istream_iterator<DataStruct>(),
+    std::back_inserter(dataVec));
+  std::erase_if(dataVec, [](const DataStruct& d) { return d.empty(); });
+  std::sort(dataVec.begin(), dataVec.end(), [](const DataStruct& a, const DataStruct& b)
+  {
+    if (a.key1 != b.key1) return a.key1 < b.key1;
+    if (std::abs(a.key2) != std::abs(b.key2)) return std::abs(a.key2) < std::abs(b.key2);
+    return a.key3.length() < b.key3.length();
+  });
+  std::copy(dataVec.begin(), dataVec.end(), std::ostream_iterator< DataStruct >(std::cout, "\n"));
+  return 0;
+}
