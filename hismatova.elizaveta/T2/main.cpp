5d03970ff425b5f523d71fb71e29e42fbe27a784
lee <likachich12@gmail.com>
2025-04-25 22:01:41 +0300

    build fix

--- hismatova.elizaveta/T2/main.cpp
+++ hismatova.elizaveta/T2/main.cpp
@@ -6,7 +6,13 @@ int main()
   std::copy(std::istream_iterator< DataStruct >(std::cin),
     std::istream_iterator<DataStruct>(),
     std::back_inserter(dataVec));
-  std::enable_if(dataVec, [](const DataStruct& d) { return d.empty(); });
+  dataVec.erase(
+    std::remove_if(dataVec.begin(), dataVec.end(), [](const DataStruct& d)
+    {
+      return d.empty();
+    }),
+    dataVec.end()
+  );
   std::sort(dataVec.begin(), dataVec.end(), [](const DataStruct& a, const DataStruct& b)
   {
     if (a.key1 != b.key1) return a.key1 < b.key1;
