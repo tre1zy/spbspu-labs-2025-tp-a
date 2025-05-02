4fb6a419a0fdbea3bd31b0e02fcac4dcbd2a4377
KostyukovMichael <kostyukovmichael@yandex.ru>
2025-05-02 20:29:30 +0300

    fix scopeGuard.hpp

--- kostyukov.mikhail/T2/scopeGuard.hpp
+++ kostyukov.mikhail/T2/scopeGuard.hpp
@@ -1,20 +1,24 @@
 #ifndef SCOPE_GUARD_HPP
 #define SCOPE_GUARD_HPP
-#include <iostream>
+#include <ios>
 
 namespace kostyukov
 {
   class ScopeGuard
   {
-  public:
-    ScopeGuard(std::basic_ios< char > &s);
-    ~ScopeGuard();
-  private:
-    std::basic_ios< char > &s_;
-    std::streamsize width_;
-    char fill_;
-    std::streamsize precision_;
-    std::basic_ios< char >::fmtflags fmt_;
+    public:
+      explicit ScopeGuard(std::basic_ios< char > &s);
+      ScopeGuard(const ScopeGuard&) = delete;
+      ScopeGuard& operator=(const ScopeGuard&) = delete;
+      ScopeGuard(ScopeGuard&&) = delete;
+      ScopeGuard& operator=(ScopeGuard&&) = delete;
+      ~ScopeGuard();
+    private:
+      std::basic_ios< char > &s_;
+      std::basic_ios< char >::fmtflags fmt_;
+      std::streamsize precision_;
+      std::streamsize width_;
+      char fill_;
   };
 }
 #endif
