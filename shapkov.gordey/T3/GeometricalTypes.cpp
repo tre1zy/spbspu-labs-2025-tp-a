e1ea08e58369af74fc9e20b79634d99d31935b13
TheEmfield <emfield337@gmail.com>
2025-05-03 16:21:17 +0300

    raw input for Polygon, needs improvement

--- shapkov.gordey/T3/GeometricalTypes.cpp
+++ shapkov.gordey/T3/GeometricalTypes.cpp
@@ -1,4 +1,7 @@
 #include "GeometricalTypes.hpp"
+#include <algorithm>
+#include <iterator>
+#include <limits>
 #include <delimiter.hpp>
 
 using check = shapkov::DelimiterIO;
@@ -13,7 +16,7 @@ std::istream& shapkov::operator>>(std::istream& in, Point& point)
   int x = 0, y = 0;
   in >> check{ '(' };
   in >> x;
-  in >> check{ ',' };
+  in >> check{ ';' };
   in >> y;
   in >> check{ ')' };
   if (in)
@@ -31,5 +34,35 @@ std::ostream& shapkov::operator<<(std::ostream& out, const Point& point)
   {
     return out;
   }
-  return (out << '(' << point.x << ',' << point.y << ')');
+  return (out << " (" << point.x << ';' << point.y << ')');
+}
+
+std::istream& shapkov::operator>>(std::istream& in, Polygon& polygon)
+{
+  std::istream::sentry s(in);
+  if (!s)
+  {
+    return in;
+  };
+  int vertexes = 0;
+  in >> vertexes;
+  std::vector< Point > temp;
+  std::copy_n(std::istream_iterator< Point >(in), vertexes, std::back_inserter(temp));
+  if (in)
+  {
+    polygon.points = temp;
+  }
+  return in;
+}
+
+std::ostream& shapkov::operator<<(std::ostream& out, const Polygon& polygon)
+{
+  std::ostream::sentry s(out);
+  if (!s)
+  {
+    return out;
+  }
+  out << polygon.points.size();
+  std::copy(std::begin(polygon.points), std::end(polygon.points), std::ostream_iterator< Point >(out, ""));
+  return out;
 }
--- shapkov.gordey/T3/main.cpp
+++ shapkov.gordey/T3/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <iterator>
+#include <limits>
 #include "GeometricalTypes.hpp"
 
 int main()
 {
-  shapkov::Point p;
-  std::cin >> p;
-  std::cout << p << '\n';
+  using shapkov::Polygon;
+  std::vector< Polygon > data;
+  using inputIt = std::istream_iterator< Polygon >;
+  using outputIt = std::ostream_iterator< Polygon >;
+  while (!std::cin.eof())
+  {
+    std::copy(inputIt(std::cin), inputIt(), std::back_inserter(data));
+    if (!std::cin)
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+  }
+  std::copy(std::begin(data), std::end(data), outputIt(std::cout, "\n"));
+  return 0;
 }
