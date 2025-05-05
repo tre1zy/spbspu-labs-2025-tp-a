9acf6fa29d0682838b0d7334a77e3ea4416734fa
Нехведович <nehvedovich016@gmail.com>
2025-05-05 19:25:54 +0300

    ebanaya zapataya

--- nehvedovich.anastasiya/T2/format_guard.cpp
+++ nehvedovich.anastasiya/T2/format_guard.cpp
@@ -4,7 +4,7 @@ nehvedovich::FormatGuard::FormatGuard(std::basic_ios< char > &s):
   s_(s),
   width_(s.width()),
   precision_(s.precision()),
-  fmt_(s.flags())
+  fmt_(s.flags()),
   fill_(s.fill()),
 {
 }
