332f97ac41b48ba2839f8cccc20ff104f238d8b8
Нехведович <nehvedovich016@gmail.com>
2025-05-05 19:20:07 +0300

    ne vse mozg vkluchily

--- nehvedovich.anastasiya/T2/format_guard.cpp
+++ nehvedovich.anastasiya/T2/format_guard.cpp
@@ -3,9 +3,9 @@
 nehvedovich::FormatGuard::FormatGuard(std::basic_ios< char > &s):
   s_(s),
   width_(s.width()),
-  fill_(s.fill()),
   precision_(s.precision()),
   fmt_(s.flags())
+  fill_(s.fill()),
 {
 }
 
