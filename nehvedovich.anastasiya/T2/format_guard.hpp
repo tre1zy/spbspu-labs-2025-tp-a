71ced71c4a2372c563a81b4d93da32f9eb03b44f
Нехведович <nehvedovich016@gmail.com>
2025-05-05 19:09:18 +0300

    a nu ka

--- nehvedovich.anastasiya/T2/format_guard.hpp
+++ nehvedovich.anastasiya/T2/format_guard.hpp
@@ -19,9 +19,9 @@ namespace nehvedovich
   private:
     std::basic_ios< char > &s_;
     std::streamsize width_;
-    char fill_;
     std::streamsize precision_;
     std::basic_ios< char >::fmtflags fmt_;
+    char fill_;
   };
 } // namespace nehvedovich
 #endif
