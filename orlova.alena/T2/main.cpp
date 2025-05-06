44b5ee81444b3c913092efa2e1cf72b6e049ba9f
widgeiw <crazynas228@gmail.com>
2025-05-06 04:22:09 +0300

    i tried

--- orlova.alena/T2/main.cpp
+++ orlova.alena/T2/main.cpp
@@ -1,2 +1,39 @@
+#include <algorithm>
+#include <limits>
+#include "DataStruct.h"
+
+bool compareDataStructs(const orlova::DataStruct& a, const orlova::DataStruct& b)
+{
+  if (a.key1 != b.key1) return a.key1 < b.key1;
+  if (a.key2 != b.key2) return a.key2 < b.key2;
+  return a.key3.length() < b.key3.length();
+}
+
 int main()
-{}
+{
+  using orlova::DataStruct;
+
+  std::vector< DataStruct > data;
+  while (!std::cin.eof())
+  {
+    DataStruct temp;
+    if (std::cin.fail())
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+
+    std::copy(
+      std::istream_iterator< DataStruct >(std::cin),
+      std::istream_iterator< DataStruct >(),
+      std::back_inserter(data)
+    );
+  }
+  std::sort(data.begin(), data.end(), compareDataStructs);
+  std::copy(
+    std::begin(data),
+    std::end(data),
+    std::ostream_iterator< DataStruct >(std::cout, "\n")
+  );
+  return 0;
+}
