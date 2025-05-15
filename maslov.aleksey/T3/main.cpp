8a3257ec986f111b4b96da813918698365b41892
A1exMas1ov <ae1ex.mas1ov@gmail.com>
2025-05-15 14:39:13 +0300

    file input

--- maslov.aleksey/T3/main.cpp
+++ maslov.aleksey/T3/main.cpp
@@ -1,2 +1,88 @@
-int main()
-{}
+#include <functional>
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <limits>
+#include <map>
+
+namespace maslov
+{
+  struct Point
+  {
+    int x, y;
+  };
+
+  struct Polygon
+  {
+    std::vector< Point > points;
+  };
+
+  void maslov::getArea(std::istream & in, std::ostream & out, const std::vector< Polygon > & polygons)
+  {}
+
+  void maslov::getMax(std::istream & in, std::ostream & out, const std::vector< Polygon > & polygons)
+  {}
+
+  void maslov::getMin(std::istream & in, std::ostream & out, const std::vector< Polygon > & polygons)
+  {}
+
+  void maslov::getEcho(std::istream & in, std::ostream & out, const std::vector< Polygon > & polygons)
+  {}
+
+  void maslov::getRects(std::ostream & out, const std::vector< Polygon > & polygons)
+  {}
+}
+
+int main(int argc, char * argv[])
+{
+  if (argc != 2)
+  {
+    std::cerr << "ERROR: wrong arguments\n";
+    return 1;
+  }
+  std::ifstream file(argv[1]);
+  if (!file.is_open())
+  {
+    std::cout << "ERROR: there is no such file\n";
+    return 1;
+  }
+  using maslov::Polygon;
+  using iIterator = std::istream_iterator< Polygon >;
+  std::vector< Polygon > polygons;
+  while (!file.eof())
+  {
+    std::copy(iIterator(file), iIterator(), std::back_inserter(polygons));
+    if (!file)
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+
+  std::map< std::string, std::function< void() > > cmds;
+  cmds["AREA"] = std::bind(maslov::getArea, std::ref(std::cin), std::ref(std::cout), std::cref(polygons));
+  cmds["MAX"] = std::bind(maslov::getMax, std::ref(std::cin), std::ref(std::cout), std::cref(polygons));
+  cmds["MIN"] = std::bind(maslov::getMin, std::ref(std::cin), std::ref(std::cout), std::cref(polygons));
+  cmds["ECHO"] = std::bind(maslov::getEcho, std::ref(std::cin), std::ref(std::cout), std::cref(polygons));
+  cmds["RECTS"] = std::bind(maslov::getRects, std::ref(std::cout), std::cref(polygons));
+
+  std::string command;
+  while (!(std::cin >> command).eof())
+  {
+    try
+    {
+      cmds.at(command)();
+    }
+    catch (const std::exception &)
+    {
+      if (std::cin.fail())
+      {
+        std::cin.clear(std::cin.rdstate() ^ std::ios::failbit);
+      }
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+      std::cout << "<INVALID COMMAND>\n";
+    }
+  }
+}
