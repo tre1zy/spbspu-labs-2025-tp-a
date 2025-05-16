a927021ba9c1759da227406030cc6c8818d2a0f7
faxryzen <cheburek2006@gmail.com>
2025-05-16 17:04:19 +0300

    Реализована исполняемая программа. Готова к улучшениям

--- savintsev.ilya/T3/main.cpp
+++ savintsev.ilya/T3/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <functional>
+#include <limits>
+#include <string>
+#include <map>
 
 int main()
 {
-  
+  std::map< std::string, std::function< void() > > cmds;
+
+  for (std::string command; std::cin >> command;)
+  {
+    try
+    {
+      cmds.at(command)();
+    }
+    catch(...)
+    {
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+      std::cout << "<INVALID COMMAND>\n";
+    }
+  }
 }
