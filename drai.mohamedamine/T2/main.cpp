69cac21f02393409dd41f4478b3fd1f8d0719841
aminedraii19 <118990672+aminedraii19@users.noreply.github.com>
2025-05-18 22:43:32 +0200

    Last

--- drai.mohamedamine/T2/main.cpp
+++ drai.mohamedamine/T2/main.cpp
@@ -3,35 +3,28 @@
 #include <iterator>
 #include <limits>
 #include <vector>
-
 #include "data.hpp"
 int main()
 {
     using DataVector = std::vector<amine::DataStruct>;
     constexpr auto max_ignore = std::numeric_limits<std::streamsize>::max();
-    
     DataVector data;
-    
     while (!std::cin.eof()) {
         std::copy(
             std::istream_iterator<amine::DataStruct>(std::cin),
             std::istream_iterator<amine::DataStruct>(),
             std::back_inserter(data)
         );
-        
         if (std::cin.fail()) {
             std::cin.clear();
             std::cin.ignore(max_ignore, '\n');
         }
     }
-    
     std::sort(data.begin(), data.end());
-    
     std::copy(
         data.begin(),
         data.end(),
         std::ostream_iterator<amine::DataStruct>(std::cout, "\n")
     );
-    
     return 0;
 }
