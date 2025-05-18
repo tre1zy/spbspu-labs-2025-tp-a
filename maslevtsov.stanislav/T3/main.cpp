6d716acf58cea29430f54601f288a550b1c9f543
jambii1 <s.maslevtsov@gmail.com>
2025-05-18 17:10:03 +0300

    commands declaration, command dispatching

--- maslevtsov.stanislav/T3/main.cpp
+++ maslevtsov.stanislav/T3/main.cpp
@@ -3,6 +3,8 @@
 #include <functional>
 #include <iterator>
 #include <iostream>
+#include <limits>
+#include <map>
 #include <vector>
 
 namespace {
@@ -39,6 +41,13 @@ namespace maslevtsov {
 
   std::istream& operator>>(std::istream& in, Point& rhs);
   std::istream& operator>>(std::istream& in, Polygon& rhs);
+
+  void get_area(const std::vector< Polygon >& polygons, std::istream& in, std::ostream& out);
+  void get_max(const std::vector< Polygon >& polygons, std::istream& in, std::ostream& out);
+  void get_min(const std::vector< Polygon >& polygons, std::istream& in, std::ostream& out);
+  void count_vertexes(const std::vector< Polygon >& polygons, std::istream& in, std::ostream& out);
+  void echo(std::vector< Polygon >& polygons, std::istream& in, std::ostream& out);
+  void remove_echo(std::vector< Polygon >& polygons, std::istream& in, std::ostream& out);
 }
 
 std::istream& maslevtsov::operator>>(std::istream& in, Point& rhs)
@@ -71,6 +80,8 @@ std::istream& maslevtsov::operator>>(std::istream& in, Polygon& rhs)
 
 int main(int argc, char** argv)
 {
+  using namespace maslevtsov;
+
   if (argc != 2) {
     std::cerr << "<INVALID PARAMETERS NUMBER>\n";
     return 1;
@@ -79,5 +90,25 @@ int main(int argc, char** argv)
   if (!fin) {
     std::cerr << "<INVALID DATA FILE>\n";
     return 1;
+  }
+  std::vector< Polygon > polygons;
+  std::map< std::string, std::function< void(std::istream&, std::ostream&) > > commands;
+  commands["AREA"] = std::bind(get_area, std::cref(polygons), std::placeholders::_1, std::placeholders::_2);
+  commands["MAX"] = std::bind(get_max, std::cref(polygons), std::placeholders::_1, std::placeholders::_2);
+  commands["MIN"] = std::bind(get_min, std::cref(polygons), std::placeholders::_1, std::placeholders::_2);
+  commands["COUNT"] = std::bind(count_vertexes, std::cref(polygons), std::placeholders::_1, std::placeholders::_2);
+  commands["ECHO"] = std::bind(get_min, std::ref(polygons), std::placeholders::_1, std::placeholders::_2);
+  commands["RMECHO"] = std::bind(count_vertexes, std::ref(polygons), std::placeholders::_1, std::placeholders::_2);
+  std::string command;
+  while (!(fin >> command).eof()) {
+    try {
+      commands.at(command)(fin, std::cout);
+    } catch (...) {
+      if (fin.fail()) {
+        fin.clear(fin.rdstate() ^ std::ios::failbit);
+      }
+      fin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+      std::cout << "<INVALID COMMAND>\n";
+    }
   }
 }
