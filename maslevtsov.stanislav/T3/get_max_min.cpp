3d34073750526a1b414dc5ac588668db4c2f97c8
jambii1 <s.maslevtsov@gmail.com>
2025-05-20 19:26:21 +0300

    min/max subcommand dispatching

--- maslevtsov.stanislav/T3/get_max_min.cpp
+++ maslevtsov.stanislav/T3/get_max_min.cpp
@@ -1,26 +1,52 @@
 #include "get_max_min.hpp"
 #include <functional>
 #include <algorithm>
+#include <map>
 #include <io_fmt_guard.hpp>
 #include "functors.hpp"
+
+namespace {
+  void get_max_area(const std::vector< maslevtsov::Polygon >& polygons, std::ostream& out)
+  {
+    std::vector< double > areas;
+    std::transform(polygons.cbegin(), polygons.cend(), std::back_inserter(areas), maslevtsov::get_polygon_area);
+    maslevtsov::IOFmtGuard guard(out);
+    out << std::fixed << std::setprecision(1) << *std::max_element(areas.cbegin(), areas.cend());
+  }
+
+  void get_max_vertexes(const std::vector< maslevtsov::Polygon >& polygons, std::ostream& out)
+  {
+    out << std::max_element(polygons.cbegin(), polygons.cend(), maslevtsov::compare_vertex_num_less)->points.size();
+  }
+
+  void get_min_area(const std::vector< maslevtsov::Polygon >& polygons, std::ostream& out)
+  {
+    std::vector< double > areas;
+    std::transform(polygons.cbegin(), polygons.cend(), std::back_inserter(areas), maslevtsov::get_polygon_area);
+    maslevtsov::IOFmtGuard guard(out);
+    out << std::fixed << std::setprecision(1) << *std::min_element(areas.cbegin(), areas.cend());
+  }
+
+  void get_min_vertexes(const std::vector< maslevtsov::Polygon >& polygons, std::ostream& out)
+  {
+    out << std::min_element(polygons.cbegin(), polygons.cend(), maslevtsov::compare_vertex_num_less)->points.size();
+  }
+}
 
 void maslevtsov::get_max(const std::vector< Polygon >& polygons, std::istream& in, std::ostream& out)
 {
   if (polygons.empty()) {
     throw std::invalid_argument("no polygons");
   }
+
+  std::map< std::string, std::function< void(std::ostream&) > > subcommands;
+  using namespace std::placeholders;
+  subcommands["AREA"] = std::bind(get_max_area, std::cref(polygons), _1);
+  subcommands["VERTEXES"] = std::bind(get_max_vertexes, std::cref(polygons), _1);
+
   std::string subcommand;
   in >> subcommand;
-  if (subcommand == "AREA") {
-    std::vector< double > areas;
-    std::transform(polygons.cbegin(), polygons.cend(), std::back_inserter(areas), get_polygon_area);
-    maslevtsov::IOFmtGuard guard(out);
-    out << std::fixed << std::setprecision(1) << *std::max_element(areas.cbegin(), areas.cend());
-  } else if (subcommand == "VERTEXES") {
-    out << std::max_element(polygons.cbegin(), polygons.cend(), compare_vertex_num_less)->points.size();
-  } else {
-    throw std::invalid_argument("invalid subcommand");
-  }
+  subcommands.at(subcommand)(out);
 }
 
 void maslevtsov::get_min(const std::vector< Polygon >& polygons, std::istream& in, std::ostream& out)
@@ -28,16 +54,12 @@ void maslevtsov::get_min(const std::vector< Polygon >& polygons, std::istream& i
   if (polygons.empty()) {
     throw std::invalid_argument("no polygons");
   }
+  std::map< std::string, std::function< void(std::ostream&) > > subcommands;
+  using namespace std::placeholders;
+  subcommands["AREA"] = std::bind(get_min_area, std::cref(polygons), _1);
+  subcommands["VERTEXES"] = std::bind(get_min_vertexes, std::cref(polygons), _1);
+
   std::string subcommand;
   in >> subcommand;
-  if (subcommand == "AREA") {
-    std::vector< double > areas;
-    std::transform(polygons.cbegin(), polygons.cend(), std::back_inserter(areas), get_polygon_area);
-    maslevtsov::IOFmtGuard guard(out);
-    out << std::fixed << std::setprecision(1) << *std::min_element(areas.cbegin(), areas.cend());
-  } else if (subcommand == "VERTEXES") {
-    out << std::min_element(polygons.cbegin(), polygons.cend(), compare_vertex_num_less)->points.size();
-  } else {
-    throw std::invalid_argument("invalid subcommand");
-  }
+  subcommands.at(subcommand)(out);
 }
