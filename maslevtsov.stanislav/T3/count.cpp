8a12a495bbd45eefbf09295b482370e77e607ee6
jambii1 <s.maslevtsov@gmail.com>
2025-05-20 19:04:10 +0300

    count subcommand dispatching

--- maslevtsov.stanislav/T3/count.cpp
+++ maslevtsov.stanislav/T3/count.cpp
@@ -1,17 +1,33 @@
 #include "count.hpp"
 #include <functional>
 #include <algorithm>
+#include <map>
 #include "functors.hpp"
+
+namespace {
+  void count_even_vertexes(const std::vector< maslevtsov::Polygon >& polygons, std::istream& in, std::ostream& out)
+  {
+    out << std::count_if(polygons.cbegin(), polygons.cend(), maslevtsov::is_even_vertex_num);
+  }
+
+  void count_odd_vertexes(const std::vector< maslevtsov::Polygon >& polygons, std::istream& in, std::ostream& out)
+  {
+    out << std::count_if(polygons.cbegin(), polygons.cend(), maslevtsov::is_odd_vertex_num);
+  }
+}
 
 void maslevtsov::count_vertexes(const std::vector< Polygon >& polygons, std::istream& in, std::ostream& out)
 {
+  std::map< std::string, std::function< void(std::istream&, std::ostream&) > > subcommands;
+  using namespace std::placeholders;
+  subcommands["EVEN"] = std::bind(count_even_vertexes, std::cref(polygons), _1, _2);
+  subcommands["ODD"] = std::bind(count_odd_vertexes, std::cref(polygons), _1, _2);
+
   std::string subcommand;
   in >> subcommand;
-  if (subcommand == "EVEN") {
-    out << std::count_if(polygons.cbegin(), polygons.cend(), is_even_vertex_num);
-  } else if (subcommand == "ODD") {
-    out << std::count_if(polygons.cbegin(), polygons.cend(), is_odd_vertex_num);
-  } else {
+  try {
+    subcommands.at(subcommand)(in, out);
+  } catch (const std::out_of_range&) {
     std::size_t vertex_num = std::stoull(subcommand);
     if (vertex_num < 3) {
       throw std::invalid_argument("invalid polygon");
