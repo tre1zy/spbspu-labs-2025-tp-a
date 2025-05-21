a0818e746013e6a3bfa74d1fa636ed8d29b124f0
P3rCh1 <okhantsev.vova@mail.ru>
2025-05-21 23:01:35 +0300

    Replace for to accumulate in vector hasher

--- ohantsev.vladimir/F0/vector_hash.h
+++ ohantsev.vladimir/F0/vector_hash.h
@@ -1,19 +1,29 @@
 #ifndef VECTOR_HASH_H
 #define VECTOR_HASH_H
 #include <vector>
+#include <numeric> 
 
-template< typename T, typename Hash >
-struct VectorHash
+namespace ohantsev
 {
-  std::size_t operator()(const std::vector< T >& vec) const
+  template<typename T, typename Hash>
+  struct VectorHash
   {
-    Hash hasher;
-    std::size_t seed = vec.size();
-    for (const auto& elem: vec)
+  public:
+    std::size_t operator()(const std::vector< T >& vec) const
     {
-      seed ^= hasher(elem) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+      return std::accumulate(vec.begin(), vec.end(), vec.size(), HashCombiner{ Hash{} });
     }
-    return seed;
-  }
-};
+
+  private:
+    struct HashCombiner
+    {
+        Hash hasher;
+
+        std::size_t operator()(std::size_t seed, const T& elem) const
+        {
+          return seed ^ (hasher(elem) + 0x9e3779b9 + (seed << 6) + (seed >> 2));
+        }
+    };
+  };
+}
 #endif
