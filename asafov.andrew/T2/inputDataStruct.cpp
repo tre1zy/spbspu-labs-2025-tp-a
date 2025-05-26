6b7e76821aa74e6ed10deaa7e8dfc0085d034baa
shycoshy <neforscist@gmail.com>
2025-05-26 12:16:28 +0000

    tests no.28

--- asafov.andrew/T2/inputDataStruct.cpp
+++ asafov.andrew/T2/inputDataStruct.cpp
@@ -1,99 +1,112 @@
 #include "datastruct.h"
 #include <stdexcept>
 #include <iterator>
+#include <cctype>
+#include <string>
 
 namespace
 {
-  int readDouble(double &num, std::istream_iterator<char>& in)
-  {
-    char buffer[64];
-    size_t size = 0;
-    while (std::isdigit(*in) && size < 64)
-    {
-      buffer[size++] = *(in++);
-    }
-    if (*(in++) != '.') return 1;
-    while (std::isdigit(*in) && size < 64)
-    {
-      buffer[size++] = *(in++);
-    }
-    num = atof(buffer);
-    return 0;
+  bool expect(char expected, std::istream_iterator<char>& in, const std::istream_iterator<char>& end) {
+    if (in == end || *in != expected) return false;
+    ++in;
+    return true;
   }
 
-  int readUllBin(unsigned long long &num, std::istream_iterator<char>& in)
-  {
+  bool readDouble(double& num, std::istream_iterator<char>& in, const std::istream_iterator<char>& end) {
+    std::string buffer;
+
+    if (in != end && (*in == '-' || *in == '+')) {
+      buffer += *in++;
+    }
+
+    while (in != end && std::isdigit(*in)) {
+      buffer += *in++;
+    }
+
+    if (in == end || *in != '.') return false;
+    buffer += *in++;  // consume '.'
+
+    while (in != end && std::isdigit(*in)) {
+      buffer += *in++;
+    }
+
+    try {
+      num = std::stod(buffer);
+    } catch (...) {
+      return false;
+    }
+
+    return true;
+  }
+
+  bool readUllBin(unsigned long long& num, std::istream_iterator<char>& in, const std::istream_iterator<char>& end) {
     num = 0;
-    while (*in != ':')
-    {
-      if (*in == '1') num++;
-      num = num << 1;
-      in++;
+    while (in != end && *in != ':') {
+      if (*in != '0' && *in != '1') return false;
+      num = (num << 1) | (*in - '0');
+      ++in;
     }
-    return 0;
+    return true;
   }
 
-  int readKey(asafov::DataStruct& data, std::istream_iterator<char>& in)
-  {
-    if (*(in++) != 'k') return 1;
-    if (*(in++) != 'e') return 1;
-    if (*(in++) != 'y') return 1;
-    if (*in == '1')
-    {
-      in++;
-      in++;
-      if (*(in++) != '0') return 1;
-      if (*(in++) != 'b') return 1;
-      unsigned long long temp;
-      if (readUllBin(temp, in)) return 1;
-      data.key1 = temp;
+  bool readKey(asafov::DataStruct& data, std::istream_iterator<char>& in, const std::istream_iterator<char>& end) {
+    if (!expect('k', in, end) || !expect('e', in, end) || !expect('y', in, end)) return false;
+
+    if (in == end) return false;
+    char keyType = *in++;
+
+    if (keyType == '1') {
+      if (!expect('0', in, end) || !expect('b', in, end)) return false;
+      unsigned long long val;
+      if (!readUllBin(val, in, end)) return false;
+      data.key1 = val;
     }
-    else if (*in == '2')
-    {
-      in++;
-      in++;
-      if (*(in++) != '#') return 1;
-      if (*(in++) != '(') return 1;
-      double temp1;
-      if (readDouble(temp1, in)) return 1;
-      in++;
-      double temp2;
-      if (readDouble(temp2, in)) return 1;
-      if (*(in++) != ')') return 1;
-      data.key2 = {temp1, temp2};
+    else if (keyType == '2') {
+      if (!expect('#', in, end) || !expect('c', in, end) || !expect('(', in, end)) return false;
+      double re, im;
+      if (!readDouble(re, in, end)) return false;
+      if (in == end || *in++ != ' ') return false;
+      if (!readDouble(im, in, end)) return false;
+      if (!expect(')', in, end)) return false;
+      data.key2 = {re, im};
     }
-    else if (*in == '3')
-    {
-      in++;
-      in++;
-      if (*(in++) != '"') return 1;
-      char buffer[512];
-      size_t size = 0;
-      while (*in != '"')
-      {
-        buffer[size++] = *(in++);
+    else if (keyType == '3') {
+      if (!expect('"', in, end)) return false;
+      std::string str;
+      while (in != end && *in != '"') {
+        str += *in++;
       }
-      data.key3 = buffer;
+      if (!expect('"', in, end)) return false;
+      data.key3 = str;
     }
-    else return 1;
-    return 0;
+    else {
+      return false;
+    }
+
+    return true;
   }
 }
 
-std::istream& asafov::operator>>(std::istream& is, DataStruct& data) //(:key1 0b0:key2 #c(0.5 -0.5):key3 "Data":)
-{
+std::istream& asafov::operator>>(std::istream& is, DataStruct& data) {
   is >> std::noskipws;
   std::istream_iterator<char> in(is);
+  std::istream_iterator<char> end;
 
-  if (*(in++) != '(') is.setstate(std::ios::failbit);
-  if (*(in++) != ':') is.setstate(std::ios::failbit);
-  if (readKey(data, in)) is.setstate(std::ios::failbit);
-  if (*(in++) != ':') is.setstate(std::ios::failbit);
-  if (readKey(data, in)) is.setstate(std::ios::failbit);
-  if (*(in++) != ':') is.setstate(std::ios::failbit);
-  if (readKey(data, in)) is.setstate(std::ios::failbit);
-  if (*(in++) != ':') is.setstate(std::ios::failbit);
-  if (*(in++) != ')') is.setstate(std::ios::failbit);
+  bool err = true;
+
+  err = err && expect('(', in, end);
+  err = err && expect(':', in, end);
+  err = err && readKey(data, in, end);
+  err = err && expect(':', in, end);
+  err = err && readKey(data, in, end);
+  err = err && expect(':', in, end);
+  err = err && readKey(data, in, end);
+  err = err && expect(':', in, end);
+  err = err && expect(')', in, end);
+
+  if (!err) {
+    is.setstate(std::ios::failbit);
+  }
 
   is >> std::skipws;
   return is;
