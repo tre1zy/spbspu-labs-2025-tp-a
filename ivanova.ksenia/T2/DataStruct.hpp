6ad0bf8500d90653e5ed71d487bdb579e6e123ef
Robotophile <ksyunya.iv@gmail.com>
2025-05-26 19:32:39 +0300

    variant

---
+++ ivanova.ksenia/T2/DataStruct.hpp
@@ -0,0 +1,15 @@
+#ifndef DATASTRUCT_HPP
+#define DATASTRUCT_HPP
+#include <string>
+
+namespace Ivanova
+{
+  struct DataStruct
+  {
+    double key1;
+    unsigned long long key2;
+    std::string key3;
+  };
+}
+
+#endif
