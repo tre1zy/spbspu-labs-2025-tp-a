0b5811040211554f80264af0b2b1a8bbe6a91b35
Drain-Doglazer <witchbtch64@gmail.com>
2025-05-28 17:13:25 +0300

    move guard and delimitr to common dir

---
+++ horoshilov.feodor/common/delimiter.h
@@ -0,0 +1,14 @@
+#ifndef DELIMITER_HPP
+#define DELIMITER_HPP
+#include <istream>
+
+namespace horoshilov
+{
+  struct DelimiterIO
+  {
+    char exp;
+  };
+
+  std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
+}
+#endif
---
+++ horoshilov.feodor/common/guard.cpp
@@ -0,0 +1,21 @@
+#include "guard.h"
+
+namespace horoshilov
+{
+  Guard::Guard(std::basic_ios< char >& s):
+    s_(s),
+    fmt_(s.flags()),
+    width_(s.width()),
+    precision_(s.precision()),
+    fill_(s.fill())
+  {}
+
+  Guard::~Guard()
+  {
+    s_.flags(fmt_);
+    s_.width(width_);
+    s_.precision(precision_);
+    s_.fill(fill_);
+  }
+}
+
