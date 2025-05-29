1cd1dbe55d2e7c7a0eaad8435dc9fa681915c059
Robotophile <ksyunya.iv@gmail.com>
2025-05-29 20:49:35 +0300

    operator>>

--- ivanova.ksenia/T2/dataStruct.cpp
+++ ivanova.ksenia/T2/dataStruct.cpp
@@ -1,9 +1,72 @@
+#include <sstream>
 #include "dataStruct.hpp"
 
 namespace ivanova
 {
   std::istream& operator>>(std::istream& in, dataStruct& ds)
   {
+    std::string line;
+    std::getline(in, line);
+
+    if (!line.empty() && line.front() == '(' && line.back() == ')')
+    {
+      line = line.substr(1, line.size() - 2);
+      std::istringstream iss(line);
+      std::string pair;
+
+      while (std::getline(iss, pair, ':'))
+      {
+        if (pair.empty()) continue;
+
+        std::istringstream pairStream(pair);
+        std::string key;
+        pairStream >> key;
+
+        if (key == "key1")
+        {
+          pairStream >> ds.key1;
+        }
+        else if (key == "key2")
+        {
+          std::string value;
+          pairStream >> value;
+
+          if (value.size() > 2 && (value.substr(0, 2) == "0b" || value.substr(0, 2) == "0B"))
+          {
+            try
+            {
+              ds.key2 = std::stoull(value.substr(2), nullptr, 2);
+            }
+            catch (...)
+            {
+              in.setstate(std::ios::failbit);
+            }
+          }
+          else
+          {
+            in.setstate(std::ios::failbit);
+          }
+        }
+        else if (key == "key3")
+        {
+          pairStream >> std::ws;
+          std::getline(pairStream, ds.key3);
+          if (!ds.key3.empty() && ds.key3.front() == '\"')
+          {
+            ds.key3 = ds.key3.substr(1);
+          }
+          if (!ds.key3.empty() && ds.key3.back() == '\"')
+          {
+            ds.key3.pop_back();
+          }
+        }
+      }
+    }
+    else
+    {
+      in.setstate(std::ios::failbit);
+    }
+    return in;
   }
 
   std::ostream& operator<<(std::ostream& out, const dataStruct& ds)
