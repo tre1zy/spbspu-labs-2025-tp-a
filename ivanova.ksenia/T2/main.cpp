b3974842a57ee87d9dd88b869743d4e7baaeefd0
Robotophile <ksyunya.iv@gmail.com>
2025-05-29 20:55:51 +0300

    main

--- ivanova.ksenia/T2/main.cpp
+++ ivanova.ksenia/T2/main.cpp
@@ -1,8 +1,28 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
+#include <limits>
+#include <iterator>
 
 #include "dataStruct.hpp"
 
 int main()
 {
+  using namespace ivanova;
+  using iIterator = std::istream_iterator<dataStruct>;
+  using oIterator = std::ostream_iterator<dataStruct>;
+
+  std::vector<dataStruct> data;
+  std::copy(iIterator(std::cin), iIterator(), std::back_inserter(data));
+
+  while (!std::cin.eof() && std::cin.fail())
+  {
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::copy(iIterator(std::cin), iIterator(), std::back_inserter(data));
+  }
+
+  std::sort(data.begin(), data.end(), compareDataStructs);
+  std::copy(data.begin(), data.end(), oIterator(std::cout, "\n"));
+  return 0;
 }
