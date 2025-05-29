4795ab260911c4453cc1ff77c7fdf089e3cf309e
Robotophile <ksyunya.iv@gmail.com>
2025-05-29 20:59:04 +0300

    try catch main

--- ivanova.ksenia/T2/main.cpp
+++ ivanova.ksenia/T2/main.cpp
@@ -1,8 +1,8 @@
 #include <algorithm>
-#include <iostream>
-#include <vector>
 #include <limits>
 #include <iterator>
+#include <iostream>
+#include <vector>
 
 #include "dataStruct.hpp"
 
@@ -13,16 +13,26 @@ int main()
   using oIterator = std::ostream_iterator<dataStruct>;
 
   std::vector<dataStruct> data;
-  std::copy(iIterator(std::cin), iIterator(), std::back_inserter(data));
 
-  while (!std::cin.eof() && std::cin.fail())
+  try
   {
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     std::copy(iIterator(std::cin), iIterator(), std::back_inserter(data));
+
+    while (!std::cin.eof() && std::cin.fail())
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::copy(iIterator(std::cin), iIterator(), std::back_inserter(data));
+    }
+
+    std::sort(data.begin(), data.end(), compareDataStructs);
+    std::copy(data.begin(), data.end(), oIterator(std::cout, "\n"));
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << "Error: " << e.what() << '\n';
+    return 1;
   }
 
-  std::sort(data.begin(), data.end(), compareDataStructs);
-  std::copy(data.begin(), data.end(), oIterator(std::cout, "\n"));
   return 0;
 }
