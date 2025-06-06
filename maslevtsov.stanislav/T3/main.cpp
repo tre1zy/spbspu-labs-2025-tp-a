d016cd3807214652b8ec36b3758cdc5f69700c8e
jambii1 <s.maslevtsov@gmail.com>
2025-06-06 21:54:33 +0300

    hello endless tests

--- maslevtsov.stanislav/T3/main.cpp
+++ maslevtsov.stanislav/T3/main.cpp
@@ -24,11 +24,11 @@ int main(int argc, char** argv)
   }
   std::vector< Polygon > polygons;
   while (!fin.eof()) {
-    std::copy(std::istream_iterator< Polygon >(fin), std::istream_iterator< Polygon >(), std::back_inserter(polygons));
     if (fin.fail()) {
       fin.clear(std::cin.rdstate() ^ std::ios::failbit);
       fin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
+    std::copy(std::istream_iterator< Polygon >(fin), std::istream_iterator< Polygon >(), std::back_inserter(polygons));
   }
   std::map< std::string, std::function< void(std::istream&, std::ostream&) > > commands;
   using namespace std::placeholders;
