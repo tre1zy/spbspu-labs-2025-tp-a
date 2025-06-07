220c2f4875e945b2154d0f4861b0a172c68abcb3
Zholobov Oleg <zholobov.of@edu.spbstu.ru>
2025-06-07 21:18:34 +0300

    minor fixes

--- zholobov.oleg/T2/main.cpp
+++ zholobov.oleg/T2/main.cpp
@@ -7,16 +7,16 @@
 
 int main()
 {
-  std::vector< zholobov::DataStruct > data;
+  using DataStruct = zholobov::DataStruct;
+  std::vector< DataStruct > data;
   while (!std::cin.eof()) {
-    std::copy(std::istream_iterator< zholobov::DataStruct >(std::cin), std::istream_iterator< zholobov::DataStruct >(),
-              std::back_inserter(data));
+    std::copy(std::istream_iterator< DataStruct >{std::cin}, std::istream_iterator< DataStruct >{}, std::back_inserter(data));
     if (std::cin.fail()) {
       std::cin.clear();
       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
   }
   std::sort(data.begin(), data.end());
-  std::copy(data.begin(), data.end(), std::ostream_iterator< zholobov::DataStruct >(std::cout, "\n"));
+  std::copy(data.begin(), data.end(), std::ostream_iterator< DataStruct >{std::cout, "\n"});
   return 0;
 }
