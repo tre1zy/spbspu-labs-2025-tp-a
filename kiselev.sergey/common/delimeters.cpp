783beb8af112f156b788df7f3149e5c259787f45
deonik3 <kis.sergey2006@gmail.com>
2025-06-08 13:14:11 +0300

    fix delimeters 2

--- kiselev.sergey/common/delimeters.cpp
+++ kiselev.sergey/common/delimeters.cpp
@@ -4,7 +4,17 @@
 #include <iterator>
 #include <ostream>
 #include <string>
-
+namespace
+{
+  struct DelimeterChecker
+  {
+    std::istream& input;
+    bool operator()(char exp) const
+    {
+      return static_cast< bool >(input >> kiselev::DelimeterIO{ exp });
+    }
+  };
+}
 std::istream& kiselev::operator>>(std::istream& input, DelimeterIO&& dest)
 {
   std::istream::sentry sentry(input);
@@ -23,9 +33,8 @@ std::istream& kiselev::operator>>(std::istream& input, DelimeterIO&& dest)
 
 std::istream& kiselev::operator>>(std::istream& input, DelimetersIO&& dest)
 {
-  std::string str;
-  std::copy_n(std::istream_iterator< char >(input), dest.exp.size(), str.begin());
-  if (str != dest.exp)
+  DelimeterChecker checker{ input };
+  if (!std::all_of(dest.exp.begin(), dest.exp.end(), checker))
   {
     input.setstate(std::ios::failbit);
   }
