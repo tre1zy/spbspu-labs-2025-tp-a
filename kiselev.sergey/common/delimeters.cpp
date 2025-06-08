c67f5a7fa215e5a99ab03273aa8c96aa5dd51e4f
deonik3 <kis.sergey2006@gmail.com>
2025-06-08 12:53:53 +0300

    use algorithm in delimeters

--- kiselev.sergey/common/delimeters.cpp
+++ kiselev.sergey/common/delimeters.cpp
@@ -1,5 +1,9 @@
 #include "delimeters.hpp"
+#include <algorithm>
 #include <istream>
+#include <iterator>
+#include <ostream>
+#include <string>
 
 std::istream& kiselev::operator>>(std::istream& input, DelimeterIO&& dest)
 {
@@ -19,9 +23,11 @@ std::istream& kiselev::operator>>(std::istream& input, DelimeterIO&& dest)
 
 std::istream& kiselev::operator>>(std::istream& input, DelimetersIO&& dest)
 {
-  for (char c: dest.exp)
+  std::string str;
+  std::copy_n(std::istream_iterator< char >(input), dest.exp.size(), str.begin());
+  if (str != dest.exp)
   {
-    input >> DelimeterIO{ c };
+    input.setstate(std::ios::failbit);
   }
   return input;
 }
