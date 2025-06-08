5350aa841125d1f55715f577578dd74ac6bca6d6
Drain-Doglazer <witchbtch64@gmail.com>
2025-06-08 19:45:19 +0300

    nu teper tocho slez

--- horoshilov.feodor/T2/guard.cpp
+++ horoshilov.feodor/T2/guard.cpp
@@ -1,20 +1,16 @@
 #include "guard.h"
+horoshilov::Guard::Guard(std::basic_ios< char >& s):
+  s_(s),
+  fmt_(s.flags()),
+  width_(s.width()),
+  precision_(s.precision()),
+  fill_(s.fill())
+{}
 
-namespace horoshilov
+horoshilov:: Guard::~Guard()
 {
- horoshilov::Guard::Guard(std::basic_ios< char >& s):
-    s_(s),
-    fmt_(s.flags()),
-    width_(s.width()),
-    precision_(s.precision()),
-    fill_(s.fill())
-  {}
-
- horoshilov:: Guard::~Guard()
-  {
-    s_.flags(fmt_);
-    s_.width(width_);
-    s_.precision(precision_);
-    s_.fill(fill_);
-  }
+  s_.flags(fmt_);
+  s_.width(width_);
+  s_.precision(precision_);
+  s_.fill(fill_);
 }
