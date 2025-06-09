92ce5c9610a27ea3a1d524577392ee7ced7c436b
CuatHimBong <egvika20051@gmail.com>
2025-06-09 15:47:30 +0300

    Input recovery refix

--- kazak.egor/T2/main.cpp
+++ kazak.egor/T2/main.cpp
@@ -2,6 +2,7 @@
 #include <iterator>
 #include <algorithm>
 #include <vector>
+#include <limits>
 #include "stream_guard.hpp"
 #include "datastruct.hpp"
 
@@ -12,19 +13,29 @@ int main()
 
   std::vector< DataStruct > data;
 
-  std::copy(
-    std::istream_iterator< DataStruct >(std::cin),
-    std::istream_iterator< DataStruct >(),
-    std::back_inserter(data)
-  );
+  while (!std::cin.eof())
+  {
+    DataStruct ds;
+
+    if (std::cin >> ds)
+    {
+      data.push_back(ds);
+    }
+    else
+    {
+      if (std::cin.eof())
+      {
+        break;
+      }
+
+      std::cin.clear();
+
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+  }
 
   std::sort(data.begin(), data.end());
-
-  std::copy(
-    std::begin(data),
-    std::end(data),
-    std::ostream_iterator< DataStruct >(std::cout, "\n")
-  );
+  std::copy(std::begin(data), std::end(data), std::ostream_iterator<DataStruct>(std::cout, "\n"));
 
   return 0;
 }
