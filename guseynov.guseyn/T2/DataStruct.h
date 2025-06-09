c24239746b9203ea5dd1ef575b36448656ad35b0
guseynov.guseyn <ganx.gamer@gmail.com>
2025-06-09 10:39:40 +0300

    add DataStruct

---
+++ guseynov.guseyn/T2/DataStruct.h
@@ -0,0 +1,14 @@
+#ifndef DATASTRUCT_H
+#define DATASTRUCT_H
+
+#include <complex>
+#include <string>
+
+struct DataStruct
+{
+    std::complex<double> key1;
+    unsigned long long key2;
+    std::string key3;
+};
+
+#endif
