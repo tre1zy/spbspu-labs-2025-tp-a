8eb3f2216bae480c3fd48606eb76137604e3cde5
TheEmfield <emfield337@gmail.com>
2025-06-09 18:10:47 +0300

    if in area cmd fix

--- shapkov.gordey/T3/cmds.cpp
+++ shapkov.gordey/T3/cmds.cpp
@@ -5,57 +5,80 @@
 #include <numeric>
 #include <iterator>
 #include <iomanip>
+#include <unordered_map>
 #include <scopeGuard.hpp>
 #include "polygonfunctors.hpp"
 
 using namespace std::placeholders;
 
+void shapkov::areaEven(std::ostream& out, const VecOfPolygons& src)
+{
+  double area = 0.0;
+  VecOfPolygons evenPolygons;
+  std::copy_if(src.begin(), src.end(), std::back_inserter(evenPolygons), isEven);
+  std::vector< double > areas(evenPolygons.size());
+  std::transform(evenPolygons.begin(), evenPolygons.end(), areas.begin(), getArea);
+  area = std::accumulate(areas.begin(), areas.end(), 0.0);
+  out << std::fixed << std::setprecision(1) << area << '\n';
+}
+
+void shapkov::areaOdd(std::ostream& out, const VecOfPolygons& src)
+{
+  double area = 0.0;
+  VecOfPolygons oddPolygons;
+  std::copy_if(src.begin(), src.end(), std::back_inserter(oddPolygons), isOdd);
+  std::vector< double > areas(oddPolygons.size());
+  std::transform(oddPolygons.begin(), oddPolygons.end(), areas.begin(), getArea);
+  area = std::accumulate(areas.begin(), areas.end(), 0.0);
+  out << std::fixed << std::setprecision(1) << area << '\n';
+}
+
+void shapkov::areaMean(std::ostream& out, const VecOfPolygons& src)
+{
+  double area = 0.0;
+  if (src.empty())
+  {
+    throw std::logic_error("no polygons");
+  }
+  std::vector< double > areas(src.size());
+  std::transform(src.begin(), src.end(), areas.begin(), getArea);
+  area = std::accumulate(areas.begin(), areas.end(), 0.0) / src.size();
+  out << std::fixed << std::setprecision(1) << area << '\n';
+}
+
+void shapkov::areaVertexes(std::ostream& out, const VecOfPolygons& src, size_t vertexes)
+{
+  double area = 0.0;
+  if (vertexes < 3)
+  {
+    throw std::logic_error("wrong number of vertexes");
+  }
+  VecOfPolygons polygons;
+  std::copy_if(src.begin(), src.end(), std::back_inserter(polygons), std::bind(isSize, _1, vertexes));
+  std::vector< double > areas(polygons.size());
+  std::transform(polygons.begin(), polygons.end(), areas.begin(), getArea);
+  area = std::accumulate(areas.begin(), areas.end(), 0.0);
+  out << std::fixed << std::setprecision(1) << area << '\n';
+}
+
 void shapkov::area(std::istream& in, std::ostream& out, const VecOfPolygons& src)
 {
+  std::unordered_map< std::string, std::function< void() > > areaCmds;
+  areaCmds["EVEN"] = std::bind(areaEven, std::ref(std::cout), std::cref(src));
+  areaCmds["ODD"] = std::bind(areaOdd, std::ref(std::cout), std::cref(src));
+  areaCmds["MEAN"] = std::bind(areaMean, std::ref(std::cout), std::cref(src));
   ScopeGuard scopeGuard(out);
   std::string subcommand;
   in >> subcommand;
-  double area = 0.0;
-  if (subcommand == "EVEN")
+  try
   {
-    VecOfPolygons evenPolygons;
-    std::copy_if(src.begin(), src.end(), std::back_inserter(evenPolygons), isEven);
-    std::vector< double > areas(evenPolygons.size());
-    std::transform(evenPolygons.begin(), evenPolygons.end(), areas.begin(), getArea);
-    area = std::accumulate(areas.begin(), areas.end(), 0.0);
+    areaCmds.at(subcommand)();
   }
-  else if (subcommand == "ODD")
-  {
-    VecOfPolygons oddPolygons;
-    std::copy_if(src.begin(), src.end(), std::back_inserter(oddPolygons), isOdd);
-    std::vector< double > areas(oddPolygons.size());
-    std::transform(oddPolygons.begin(), oddPolygons.end(), areas.begin(), getArea);
-    area = std::accumulate(areas.begin(), areas.end(), 0.0);
-  }
-  else if (subcommand == "MEAN")
-  {
-    if (src.empty())
-    {
-      throw std::logic_error("no polygons");
-    }
-    std::vector< double > areas(src.size());
-    std::transform(src.begin(), src.end(), areas.begin(), getArea);
-    area = std::accumulate(areas.begin(), areas.end(), 0.0) / src.size();
-  }
-  else
+  catch (...)
   {
     size_t vertexes = std::stoi(subcommand);
-    if (vertexes < 3)
-    {
-      throw std::logic_error("wrong number of vertexes");
-    }
-    VecOfPolygons polygons;
-    std::copy_if(src.begin(), src.end(), std::back_inserter(polygons), std::bind(isSize, _1, vertexes));
-    std::vector< double > areas(polygons.size());
-    std::transform(polygons.begin(), polygons.end(), areas.begin(), getArea);
-    area = std::accumulate(areas.begin(), areas.end(), 0.0);
+    shapkov::areaVertexes(out, src, vertexes);
   }
-  out << std::fixed << std::setprecision(1) << area << '\n';
 }
 void shapkov::max(std::istream& in, std::ostream& out, const VecOfPolygons& src)
 {
@@ -131,7 +154,6 @@ void shapkov::rects(std::ostream& out, const VecOfPolygons& src)
 {
   out << std::count_if(src.begin(), src.end(), isRectangle) << '\n';
 }
-
 void shapkov::same(std::istream& in, std::ostream& out, const VecOfPolygons& src)
 {
   Polygon polygon;
--- shapkov.gordey/T3/cmds.hpp
+++ shapkov.gordey/T3/cmds.hpp
@@ -8,6 +8,10 @@ namespace shapkov
 {
   using VecOfPolygons = std::vector< Polygon >;
   void area(std::istream& in, std::ostream& out, const VecOfPolygons& src);
+  void areaEven(std::ostream& out, const VecOfPolygons& src);
+  void areaOdd(std::ostream& out, const VecOfPolygons& src);
+  void areaMean(std::ostream& out, const VecOfPolygons& src);
+  void areaVertexes(std::ostream& out, const VecOfPolygons& src, size_t vertexes);
   void max(std::istream& in, std::ostream& out, const VecOfPolygons& src);
   void min(std::istream& in, std::ostream& out, const VecOfPolygons& src);
   void count(std::istream& in, std::ostream& out, const VecOfPolygons& src);
