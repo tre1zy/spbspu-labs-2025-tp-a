b0b528c0f121db925659cca3f289f53ee1e318e2
0Revaivel0 <kharlamov_k1999@bk.ru>
2025-06-10 23:57:48 +0300

    LUBLU SEREGU

--- kharlamov.kirill/T3/polygon.cpp
+++ kharlamov.kirill/T3/polygon.cpp
@@ -26,25 +26,7 @@ std::istream& kharlamov::operator>>(std::istream& in, Polygon& polygon)
   return in;
 }
 
-bool Polygon::operator==(const Polygon& other) const
+bool kharlamov::Polygon::operator==(const Polygon& other) const
 {
   return points == other.points;
 }
-
-std::istream& operator>>(std::istream& in, Polygon& poly)
-{
-  size_t vertexes;
-  in >> vertexes;
-  if (vertexes < 3)
-  {
-    in.setstate(std::ios::failbit);
-    return in;
-  }
-  poly.points.resize(vertexes);
-  for (auto& point : poly.points)
-  {
-    in >> point;
-  }
-  return in;
-}
-
