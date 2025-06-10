53308ce656be6bb94593799f18f85786413e4140
brev1ch <flirktv@gmail.com>
2025-06-10 20:04:32 +0300

    streamGuard

---
+++ brevnov.nikita/T3/streamGuard.hpp
@@ -0,0 +1,18 @@
+#ifndef STREAMGUARD_HPP
+#define STREAMGUARD_HPP
+namespace brevnov
+{
+  struct StreamGuard
+  {
+  public:
+    explicit StreamGuard(std::basic_ios< char > & s);
+    ~StreamGuard();
+  private:
+    std::basic_ios< char > & s_;
+    std::streamsize width_;
+    char fill_;
+    std::streamsize precision_;
+    std::basic_ios< char >::fmtflags fmt_;
+  };
+}
+#endif
