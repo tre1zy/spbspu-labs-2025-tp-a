98d11603554d2ca7a6e1bd0d4f400f8fef39273d
brev1ch <flirktv@gmail.com>
2025-06-10 20:13:00 +0300

    polygon.hpp

---
+++ brevnov.nikita/T3/polygon.hpp
@@ -0,0 +1,26 @@
+#ifndef POLYGON_HPP
+#define POLYGON_HPP
+#include <istream>
+#include <ostream>
+#include <vector>
+
+namespace brevnov
+{
+  struct Point
+  {
+    int x;
+    int y;
+  };
+
+  struct Polygon
+  {
+    std::vector< Point > points;
+  };
+
+  std::istream& operator>>(std::istream&, Point&);
+  std::istream& operator>>(std::istream&, Polygon&);
+  double get_area(const Polygon&);
+  bool is_rect(const Polygon&);
+
+}
+#endif
