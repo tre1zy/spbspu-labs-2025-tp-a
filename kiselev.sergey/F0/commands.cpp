265652fc96ad9432c1a35117f0eecdda2202c91e
deonik3 <kis.sergey2006@gmail.com>
2025-06-11 14:23:10 +0300

    crreate uniondict

--- kiselev.sergey/F0/commands.cpp
+++ kiselev.sergey/F0/commands.cpp
@@ -2,8 +2,10 @@
 #include <ostream>
 #include <string>
 #include <algorithm>
+#include <numeric>
 #include <iterator>
 #include <vector>
+#include <pthread.h>
 
 namespace
 {
@@ -12,6 +14,7 @@ namespace
   {
     return val.first;
   }
+
   struct WordPrinter
   {
     std::ostream& out;
@@ -20,6 +23,7 @@ namespace
       out << " " << word;
     }
   };
+
   struct DictPrinter
   {
     std::ostream& out;
@@ -34,15 +38,47 @@ namespace
       out << "\n";
     }
   };
+
   struct TranslationChecker
   {
     const std::string& str;
     bool operator()(const kiselev::Dict::value_type& val)
     {
-      return std::find(val.second.begin(), val.second.end(), str) != val.second.end();
+        return std::find(val.second.begin(), val.second.end(), str) == val.second.end();
     }
   };
 
+  struct VectorChecker
+  {
+    const std::vector<std::string>& vec;
+    bool operator()(const std::string& trans) const
+    {
+      return std::find(vec.begin(), vec.end(), trans) == vec.end();
+    }
+  };
+  struct DictMerger
+  {
+    kiselev::Dict operator()(kiselev::Dict res, const kiselev::Dict::value_type& val)
+    {
+      auto it = res.find(val.first);
+      if (it == res.end())
+      {
+        res.insert(val);
+      }
+      else
+      {
+        std::vector<std::string> newTranslations;
+        VectorChecker check{ it->second };
+        std::copy_if(val.second.begin(), val.second.end(), std::back_inserter(newTranslations), check);
+        it->second.insert(it->second.end(), newTranslations.begin(), newTranslations.end());
+      }
+      return res;
+    }
+  };
+  kiselev::Dict unionTwoDict(const kiselev::Dict& dict1, const kiselev::Dict& dict2)
+  {
+    return std::accumulate(dict2.begin(), dict2.end(), dict1, DictMerger{});
+  }
 }
 void kiselev::doNewDict(std::istream& in, std::ostream& out, Dicts& dicts)
 {
@@ -170,4 +206,41 @@ void kiselev::doTranslateWord(std::istream& in, std::ostream& out, const Dicts& 
     return;
   }
   out << "<WORD NOT FOUND>\n";
+}
+
+void kiselev::doUnionDict(std::istream& in, std::ostream& out, Dicts& dicts)
+{
+  std::string nameNewDict;
+  std::string firstDict;
+  std::string secondDict;
+  in >> nameNewDict >> firstDict >> secondDict;
+  if (dicts.find(nameNewDict) != dicts.end())
+  {
+    out << "<DICTIONARY ALREADY EXISTS>\n";
+    return;
+  }
+  auto first = dicts.find(firstDict);
+  auto second = dicts.find(secondDict);
+  if (first == dicts.end() || second == dicts.end())
+  {
+    out << "<DICTIONARY NOT FOUND>\n";
+    return;
+  }
+  Dict res = unionTwoDict(first->second, second->second);
+  std::string nextDict;
+  while (in >> nextDict)
+  {
+    auto it = dicts.find(nextDict);
+    if (it == dicts.end())
+    {
+      out << "<DICTIONARY NOT FOUND>\n";
+      return;
+    }
+    res = unionTwoDict(res, it->second);
+    if (in.get() == '\n')
+    {
+      break;
+    }
+  }
+  dicts[nameNewDict] = res;
 }
