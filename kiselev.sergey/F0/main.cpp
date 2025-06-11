2ccdb8aaefaa9f90f8a6c74b6317a23d92e26c61
deonik3 <kis.sergey2006@gmail.com>
2025-06-11 16:40:20 +0300

    create main

---
+++ kiselev.sergey/F0/main.cpp
@@ -0,0 +1,88 @@
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <istream>
+#include <string>
+#include <utility>
+#include <iterator>
+#include <limits>
+#include "commands.hpp"
+namespace
+{
+  void input(std::istream& in, kiselev::Dicts& dicts)
+  {
+    std::string dictName;
+    while (in >> dictName)
+    {
+        kiselev::Dict dict;
+      std::string eng;
+      while (in >> eng)
+      {
+        std::vector<std::string> ruswords;
+        std::copy(std::istream_iterator<std::string>(in), std::istream_iterator<std::string>(), std::back_inserter(ruswords));
+        if (!ruswords.empty() && ruswords.back().back() == '\n')
+        {
+          ruswords.back().pop_back();
+        }
+        dict[eng] = ruswords;
+        if (in.get() == '\n')
+        {
+          break;
+        }
+        else
+        {
+          in.unget();
+        }
+      }
+      dicts[dictName] = dict;
+    }
+  }
+}
+int main(int argc, char** argv)
+{
+  using namespace kiselev;
+  Dicts dicts;
+  if (argc > 1)
+  {
+    std::ifstream file(argv[1]);
+    if (!file)
+    {
+      std::cout << "FILE ERROR\n";
+      return 1;
+    }
+    input(file, dicts);
+    file.close();
+  }
+  std::map< std::string, std::function< void() > > commands;
+  using namespace std::placeholders;
+  commands.insert(std::make_pair("newdict", std::bind(doNewDict, std::ref(std::cin), std::ref(std::cout), std::ref(dicts))));
+  commands.insert(std::make_pair("deletedict", std::bind(doDeleteDict, std::ref(std::cin), std::ref(std::cout), std::ref(dicts))));
+  commands.insert(std::make_pair("addelement", std::bind(doAddElement, std::ref(std::cin), std::ref(std::cout), std::ref(dicts))));
+  commands.insert(std::make_pair("deleteelement", std::bind(doDeleteElement, std::ref(std::cin), std::ref(std::cout), std::ref(dicts))));
+  commands.insert(std::make_pair("listdict", std::bind(doListDict, std::ref(std::cout), std::cref(dicts))));
+  commands.insert(std::make_pair("printdict", std::bind(doPrintDict, std::ref(std::cin), std::ref(std::cout), std::cref(dicts))));
+  commands.insert(std::make_pair("translateword", std::bind(doTranslateWord, std::ref(std::cin), std::ref(std::cout), std::cref(dicts))));
+  commands.insert(std::make_pair("uniondict", std::bind(doUnionDict, std::ref(std::cin), std::ref(std::cout), std::ref(dicts))));
+  commands.insert(std::make_pair("savedict", std::bind(doSaveDict, std::ref(std::cin), std::ref(std::cout), std::cref(dicts))));
+  commands.insert(std::make_pair("countdict", std::bind(doCountWord, std::ref(std::cin), std::ref(std::cout), std::cref(dicts))));
+  commands.insert(std::make_pair("searchletter", std::bind(doSearchLetter, std::ref(std::cin), std::ref(std::cout), std::cref(dicts))));
+  commands.insert(std::make_pair("loaddict", std::bind(doLoadDict, std::ref(std::cin), std::ref(std::cout), std::ref(dicts))));
+  commands.insert(std::make_pair("cleardict", std::bind(doClearDict, std::ref(std::cin), std::ref(std::cout), std::ref(dicts))));
+  commands.insert(std::make_pair("intersectdict", std::bind(doIntersectDict, std::ref(std::cin), std::ref(std::cout), std::ref(dicts))));
+  commands.insert(std::make_pair("complementdict", std::bind(doComplementDict, std::ref(std::cin), std::ref(std::cout), std::ref(dicts))));
+  std::string command;
+  while (std::cin >> command)
+  {
+    try
+    {
+      commands.at(command)();
+    }
+    catch (...)
+    {
+      std::cout << "<INVALID COMMAND>\n";
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+  return 1;
+}
