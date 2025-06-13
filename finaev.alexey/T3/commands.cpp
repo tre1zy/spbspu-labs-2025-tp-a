805faabab2b8c61da9c2442806b811f148773a34
AlexeyFinaev02 <finaev0212212@mail.ru>
2025-06-13 16:39:09 +0300

    add main and area function

---
+++ finaev.alexey/T3/commands.cpp
@@ -0,0 +1,98 @@
+#include "commands.hpp"
+#include <iomanip>
+#include <streamGuard.hpp>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+
+namespace
+{
+  bool isOdd(const finaev::Polygon& poly)
+  {
+    return poly.points.size() % 2 == 0;
+  }
+  bool isEven(const finaev::Polygon& poly)
+  {
+    return poly.points.size() % 2 != 0;
+  }
+  bool isNumOfVertex(const finaev::Polygon& poly, size_t s)
+  {
+    return poly.points.size() == s;
+  }
+  double areaOnePoly(const finaev::Polygon& poly)
+  {
+    if (poly.points.size() < 3)
+    {
+      return 0.0;
+    }
+    double res = 0.0;
+    size_t s = poly.points.size();
+    for (size_t i = 0; i < s; ++i)
+    {
+      int j = (i + 1) % s;
+      res += (poly.points[i].x * poly.points[j].y) - (poly.points[i].y * poly.points[j].x);
+    }
+    return std::abs(res) / 2.0;
+  }
+}
+
+void finaev::area(std::istream& in, std::ostream& out, const std::vector< Polygon >& shapes)
+{
+  std::string substr;
+  in >> substr;
+  StreamGuard streamGuard(out);
+  out << std::fixed << std::setprecision(1);
+  if (substr == "ODD")
+  {
+    std::vector< Polygon > odd;
+    std::copy_if(shapes.begin(), shapes.end(), std::back_inserter(odd), isOdd);
+    double res = 0.0;
+    for (size_t i = 0; i < odd.size(); ++i)
+    {
+      res += areaOnePoly(odd[i]);
+    }
+    out << res;
+  }
+  else if (substr == "EVEN")
+  {
+    std::vector< Polygon > even;
+    std::copy_if(shapes.begin(), shapes.end(), std::back_inserter(even), isEven);
+    double res = 0.0;
+    for (size_t i = 0; i < even.size(); ++i)
+    {
+      res += areaOnePoly(even[i]);
+    }
+    out << res;
+  }
+  else if (substr == "MEAN")
+  {
+    if (shapes.empty())
+    {
+      throw std::invalid_argument("<INVALID COMMAND>");
+    }
+    double res = 0.0;
+    for (size_t i = 0; i < shapes.size(); ++i)
+    {
+      res += areaOnePoly(shapes[i]);
+    }
+    out << res / shapes.size(); 
+  }
+  else if (substr == "num-of-vertexes")
+  {
+    size_t s = 0; 
+    in >> s;
+    std::vector< Polygon > temp;
+    auto compare = std::bind(isNumOfVertex, std::placeholders::_1, s);
+    std::copy_if(shapes.begin(), shapes.end(), std::back_inserter(temp), compare); 
+    double res = 0.0;
+    for (size_t i = 0; i < temp.size(); ++i)
+    {
+      res += areaOnePoly(temp[i]);
+    }
+    out << res;
+  }
+  else
+  {
+    throw std::invalid_argument("<INVALID COMMAND>");
+  }
+}
--- finaev.alexey/T3/main.cpp
+++ finaev.alexey/T3/main.cpp
@@ -1,2 +1,62 @@
-int main()
-{}
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <functional>
+#include <limits>
+#include "shapes.hpp"
+#include "commands.hpp"
+
+int main(int argc, char* argv[])
+{
+  if (argc != 2)
+  {
+    std::cerr << "Uncorrect input\n";
+    return 1;
+  }
+  std::ifstream file(argv[1]);
+  if (!file)
+  {
+    std::cerr << "Error openning\n";
+    return 1;
+  }
+  using finaev::Polygon;
+  std::vector< Polygon > shapes;
+  while(!file.eof())
+  {
+    std::copy(std::istream_iterator< Polygon >{ file }, std::istream_iterator< Polygon >{}, std::back_inserter(shapes));
+    if (file.fail() && !file.eof())
+    {
+      file.clear();
+      file.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+  std::map< std::string, std::function< void() > > commands;
+  commands["AREA"] = std::bind(finaev::area, std::ref(std::cin), std::ref(std::cout), std::cref(shapes));
+  std::string cmd;
+  while (std::cin >> cmd)
+  {
+    try
+    {
+      auto func = commands.find(cmd);
+      if (func != commands.end())
+      {
+        func->second();
+      }
+      else
+      {
+        throw std::invalid_argument("<INVALID COMMAND>");
+      }
+    }
+    catch(const std::exception& e)
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+      std::cout << e.what();
+    }
+    std::cout << "\n";
+  }
+}
