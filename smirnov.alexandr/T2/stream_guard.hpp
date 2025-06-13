ac985920d4c7eebf3efc3f68001498a0763bbe39
A1exCRE <alsmmm2065@gmail.com>
2025-06-13 12:40:17 +0300

    add stream_guard.hpp

---
+++ smirnov.alexandr/T2/stream_guard.hpp
@@ -0,0 +1,22 @@
+#ifndef STREAM_GUARD_HPP
+#define STEAM_GUARD_HPP
+#include <iostream>
+
+namespace smirnov
+{
+  class StreamGuard
+  {
+  public:
+    explicit StreamGuard(std::basic_ios< char > & stream);
+    StreamGuard(const StreamGuard &) = delete;
+    StreamGuard & operator=(const StreamGuard &) = delete;
+    ~StreamGuard();
+  private:
+    std::basic_ios< char > & stream_;
+    std::basic_ios< char >::fmtflags fmt_;
+    std::streamsize width_;
+    std::streamsize precision_;
+    char fill_;
+  };
+}
+#endif
