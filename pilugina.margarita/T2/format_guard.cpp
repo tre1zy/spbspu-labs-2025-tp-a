5e8bc0c9a5e83bf9d8869dc7c3040e0e3337ffc3
MargotBush <margaritapilugina2006@gmail.com>
2025-06-13 14:18:50 +0300

    preparation for T3

--- pilugina.margarita/T2/format_guard.cpp
+++ pilugina.margarita/T2/format_guard.cpp
@@ -6,8 +6,7 @@ pilugina::FormatGuard::FormatGuard(std::basic_ios< char > &s):
   precision_(s.precision()),
   fmt_(s.flags()),
   fill_(s.fill())
-{
-}
+{}
 
 pilugina::FormatGuard::~FormatGuard()
 {
