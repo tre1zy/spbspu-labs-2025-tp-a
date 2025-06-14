001113d17340e4f14002df897e1ad1d39ed81d50
ZakirovMS <mz.08.08.06@gmail.com>
2025-06-14 20:44:14 +0300

    Object classes and input/output operators overloading definition

---
+++ zakirov.marat/T3/geometric_shape.hpp
@@ -0,0 +1,27 @@
+#ifndef GEOMETRIC_SHAPE_HPP
+#define GEOMETRIC_SHAPE_HPP
+#include <iostream>
+#include <vector>
+#include <utility>
+
+namespace zakirov
+{
+  class Polygon
+  {
+  public:
+    std::vector< Point > points_;
+  };
+
+  class Point
+  {
+  public:
+    int x_;
+    int y_;
+  };
+
+  std::istream & operator>>(std::istream & in, Point & point);
+  std::istream & operator>>(std::istream & in, Polygon & polygon);
+  std::ostream & operator<<(std::ostream & out, Polygon & polygon);
+}
+
+#endif
