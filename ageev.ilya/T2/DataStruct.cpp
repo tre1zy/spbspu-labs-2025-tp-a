f2df4ac95e361430edf2b11ac587e39ef5f18545
qyantor <ilyxaplay220@gmail.com>
2025-06-15 21:57:09 +0300

    some changes

--- ageev.ilya/T2/DataStruct.cpp
+++ ageev.ilya/T2/DataStruct.cpp
@@ -44,7 +44,7 @@ namespace ageev
     std::string str;
     try
     {
-      while (in >> c && (c != 'e' or c != 'E'))
+      while (in >> c && c != 'e' && c != 'E')
       {
         str += c;
       }
@@ -89,21 +89,21 @@ namespace ageev
     return in;
   }
 
-  std::istream& operator>>(std::istream& in, Data& dest)
+  std::istream& operator>>(std::istream& in, DataStruct& dest)
   {
     std::istream::sentry sentry(in);
     if (!sentry)
     {
       return in;
     }
-    Data input;
+    DataStruct input;
     {
       using sep = DelimiterIO;
       using label = LabelIO;
       using dbl = DoubleIO;
       using dblsci = DoubleSciIO;
       using str = StringIO;
-      in >> sep{ '(' } >> sep ( ':' );
+      in >> sep{ '(' } >> sep { ':' };
       in >> label{ "key1" } >> dbl{ input.key1 };
       in >> sep{ ':' };
       in >> label{ "key2" } >> dblsci{ input.key2 };
@@ -118,7 +118,7 @@ namespace ageev
     return in;
   }
 
-  std::ostream& operator<<(std::ostream& out, const Data& src)
+  std::ostream& operator<<(std::ostream& out, const DataStruct& src)
   {
     std::ostream::sentry sentry(out);
     if (!sentry)
@@ -128,9 +128,10 @@ namespace ageev
     iofmtguard fmtguard(out);
     out << "{ ";
     out << "\"key1\": ";
-    out << std::fixed;
     out << std::fixed << std::setprecision(1) << src.key1 << "d, ";
     out << "\"key2\": " << src.key2;
+    out << " }";
+    out << "\"key3\": \"" << src.key3 << "\"";
     out << " }";
     return out;
   }
