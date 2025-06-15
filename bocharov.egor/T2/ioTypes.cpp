c90aafc73e9fd6fa897d981a01754fbae765bb35
LuckyBichar <enbocharov2006@gmail.com>
2025-06-15 20:05:33 +0300

    del ioDelimiter iz ioTypes

--- bocharov.egor/T2/ioTypes.cpp
+++ bocharov.egor/T2/ioTypes.cpp
@@ -1,6 +1,7 @@
 #include "ioTypes.hpp"
 #include <iomanip>
 #include <cmath>
+#include <ioDelimiter.hpp>
 #include "streamGuard.hpp"
 
 std::ostream & bocharov::operator<<(std::ostream & out, const DataStruct & data)
@@ -18,22 +19,6 @@ std::ostream & bocharov::operator<<(std::ostream & out, const DataStruct & data)
   out << "':key3 \"" << data.key3;
   out << "\":)";
   return out;
-}
-
-std::istream & bocharov::operator>>(std::istream & in, DelimiterIO && dest)
-{
-  std::istream::sentry sentry(in);
-  if (!sentry)
-  {
-    return in;
-  }
-  char c = '0';
-  in >> c;
-  if (in && (c != dest.exp))
-  {
-    in.setstate(std::ios::failbit);
-  }
-  return in;
 }
 
 std::istream & bocharov::operator>>(std::istream & in, DoubleSciIO && dest)
