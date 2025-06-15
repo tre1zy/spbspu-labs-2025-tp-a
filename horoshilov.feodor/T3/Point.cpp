b4203a502d735a5f22785050441dc1753a6cc9b7
Drain-Doglazer <witchbtch64@gmail.com>
2025-06-15 23:26:54 +0300

    fix point

--- horoshilov.feodor/T3/Point.cpp
+++ horoshilov.feodor/T3/Point.cpp
@@ -12,13 +12,21 @@ bool horoshilov::Point::operator<(const Point& other) const
   return x < other.x || (x == other.x && y < other.y);
 }
 
-std::istream& horoshilov::operator>>(std::istream& in, Point& point)
+std::istream& kostyukov::operator>>(std::istream& in, Point& point)
 {
   std::istream::sentry sentry(in);
   if (!sentry)
   {
     return in;
   }
-  in >> DelimiterIO{ '(' } >> point.x >> DelimiterIO{ ';' } >> point.y >> DelimiterIO{ ')' };
+  Point temp;
+  {
+    using del = DelimiterIO;
+    in >> del{ '(' } >> temp.x >> del{ ';' } >> temp.y >> del{ ')' };
+    if (in)
+    {
+      point = temp;
+    }
+  }
   return in;
 }
