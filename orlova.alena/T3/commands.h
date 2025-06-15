3b37ca7f7a7f8af6b8080a702735e05c11380404
widgeiw <crazynas228@gmail.com>
2025-06-16 01:34:43 +0300

    editing main.cpp and adding commands.h

---
+++ orlova.alena/T3/commands.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "geom.h"
+
+namespace orlova
+{
+  void area(const std::vector< Polygon >&, std::istream&, std::ostream&);
+  double areaEven(const std::vector< Polygon >&);
+  double areaOdd(const std::vector< Polygon >&);
+  double areaMean(const std::vector< Polygon >&);
+  double areaNum(const std::vector< Polygon >&);
+
+  void max(const std::vector< Polygon >&, std::istream&, std::ostream&);
+  double maxArea(const std::vector< Polygon >&);
+  size_t maxVertexes(const std::vector< Polygon >&);
+
+  void min(const std::vector< Polygon >&, std::istream&, std::ostream&);
+  double minArea(const std::vector< Polygon >&);
+  size_t minVertexes(const std::vector< Polygon >&);
+
+  void count(const std::vector< Polygon >&, std::istream&, std::ostream&);
+  size_t countEven(const std::vector< Polygon >&);
+  size_t countOdd(const std::vector< Polygon >&);
+  size_t countNum(const std::vector< Polygon >&, size_t);
+}
+
+#endif
--- orlova.alena/T3/main.cpp
+++ orlova.alena/T3/main.cpp
@@ -1,2 +1,53 @@
-int main()
-{}
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <exception>
+#include <fstream>
+#include "geom.h"
+#include "commands.h"
+
+int main(int argc, char** argv) 
+{
+  using namespace orlova;
+
+  std::vector< Polygon > polygons;
+
+  if (argc != 2)
+  {
+    std::cerr << "<INCORRECT ARGUMENTS>\n";
+    return 1;
+  }
+
+  std::ifstream file(argv[1]);
+  if (!file)
+  {
+    std::cerr << "<INCORRECT FILE>\n";
+    return 1;
+  }
+
+  std::map< std::string, std::function< void() > > cmds;
+  cmds["area"] = std::bind(area, std::cref(polygons), std::ref(std::cin), std::ref(std::cout));
+  cmds["max"] = std::bind(max, std::cref(polygons), std::ref(std::cin), std::ref(std::cout));
+  cmds["min"] = std::bind(min, std::cref(polygons), std::ref(std::cin), std::ref(std::cout));
+  cmds["count"] = std::bind(count, std::cref(polygons), std::ref(std::cin), std::ref(std::cout));
+
+  std::string command;
+  while (!(std::cin >> command).eof())
+  {
+    try
+    {
+      cmds.at(command)();
+    }
+    catch (...)
+    {
+      if (std::cin.fail())
+      {
+        std::cin.clear(std::cin.rdstate() ^ std::ios::failbit);
+      }
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+      std::cout << "<INVALID COMMAND>\n";
+    }
+  }
+}
