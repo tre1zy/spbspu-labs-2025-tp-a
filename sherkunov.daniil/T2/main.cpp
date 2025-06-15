a488ea796077e07eccc31f8e9b5899551fe6ae83
kopinik <bk65763980@mail.ru>
2025-06-15 16:58:41 +0300

    fixed endless loop

--- sherkunov.daniil/T2/main.cpp
+++ sherkunov.daniil/T2/main.cpp
@@ -6,22 +6,30 @@
 #include "data_struct.hpp"
 
 int main() {
-  using InputStreamIterator = std::istream_iterator<sherkunov::DataStruct>;
-  using OutputStreamIterator = std::ostream_iterator<sherkunov::DataStruct>;
+    using InputStreamIterator = std::istream_iterator<sherkunov::DataStruct>;
+    using OutputStreamIterator = std::ostream_iterator<sherkunov::DataStruct>;
 
-  std::vector<sherkunov::DataStruct> data;
-  while (!std::cin.eof() || !std::cin.bad()) {
-    std::copy(InputStreamIterator{std::cin}, InputStreamIterator{}, std::back_inserter(data));
-    if (std::cin.fail()) {
-      std::cin.clear(std::cin.rdstate() & ~std::ios::failbit);
-      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::vector<sherkunov::DataStruct> data;
+    
+    InputStreamIterator it(std::cin);
+    while (it != InputStreamIterator{}) {
+        data.push_back(*it);
+        ++it;
+
+        if (std::cin.fail() && !std::cin.eof()) {
+            std::cin.clear(std::cin.rdstate() & ~std::ios::failbit);
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            it = InputStreamIterator(std::cin);
+        }
     }
-  }
-  if (std::cin.bad()) {
-        std::cerr <<"Error: Critical input failure \n";
+
+    if (std::cin.bad()) {
+        std::cerr << "Error: Critical input failure\n";
         return 1;
     }
-  std::sort(data.begin(), data.end());
-  std::copy(data.begin(), data.end(), OutputStreamIterator{std::cout, "\n"});
-  return 0;
+
+    std::sort(data.begin(), data.end());
+    std::copy(data.begin(), data.end(), OutputStreamIterator(std::cout, "\n"));
+    
+    return 0;
 }
