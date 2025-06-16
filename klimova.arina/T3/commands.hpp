02020b9bb69322bdfb30fe115bb9c7713b471fcc
arinaklimova <arina.klim.06@mail.ru>
2025-06-16 20:54:42 +0300

    fixic

--- klimova.arina/T3/commands.hpp
+++ klimova.arina/T3/commands.hpp
@@ -13,6 +13,7 @@ namespace klimova
     using AreaSubcommands = std::map< std::string, std::function< double(double, const Polygon&) > >;
     using MaxSubcommands = std::map< std::string, std::function< double(const Polygon&) > >;
     using MinSubcommands = std::map< std::string, std::function< double(const Polygon&) > >;
+    using CountSubcommands = std::map< std::string, std::function< bool(const Polygon&) > >;
 
     void area(const std::vector< Polygon >& polygons, std::istream& is, std::ostream& os);
     void max(const std::vector< Polygon >& polygons, std::istream& is, std::ostream& os);
