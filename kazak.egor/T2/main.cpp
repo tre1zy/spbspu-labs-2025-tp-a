1cd6339f1b4217dac5337fc730e14eaefdcb29e4
CuatHimBong <egvika20051@gmail.com>
2025-06-18 01:38:57 +0300

    Returned prev main

--- kazak.egor/T2/main.cpp
+++ kazak.egor/T2/main.cpp
@@ -2,25 +2,40 @@
 #include <iterator>
 #include <algorithm>
 #include <vector>
-#include "datastruct.hpp"
+#include <limits>
 #include "stream_guard.hpp"
+#include "datastruct.hpp"
 
 int main()
 {
   using namespace kazak;
-  using DataStructs = std::vector< DataStruct >;
+  using kazak::DataStruct;
 
-  DataStructs data;
+  std::vector< DataStruct > data;
 
-  std::copy(std::istream_iterator< DataStruct >(std::cin),
-            std::istream_iterator< DataStruct >(),
-            std::back_inserter(data));
+  while (!std::cin.eof())
+  {
+    DataStruct ds;
+
+    if (std::cin >> ds)
+    {
+      data.push_back(ds);
+    }
+    else
+    {
+      if (std::cin.eof())
+      {
+        break;
+      }
+
+      std::cin.clear();
+
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+  }
 
   std::sort(data.begin(), data.end());
-
-  std::copy(data.begin(), data.end(),
-            std::ostream_iterator< DataStruct >(std::cout, "\n"));
+  std::copy(std::begin(data), std::end(data), std::ostream_iterator<DataStruct>(std::cout, "\n"));
 
   return 0;
 }
-
