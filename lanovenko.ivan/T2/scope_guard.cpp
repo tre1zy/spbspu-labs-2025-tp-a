e82d632c96a9b0f81f6ab9fcabb92fbb7800d1a1
Tuc0Sa1amanka <sparowj765@gmail.com>
2025-06-19 02:07:22 +0300

    fixed CG and added explicit constructor

--- lanovenko.ivan/T2/scope_guard.cpp
+++ lanovenko.ivan/T2/scope_guard.cpp
@@ -1,17 +1,17 @@
 #include "scope_guard.hpp"
 
-lanovenko::iofmtguard::iofmtguard(std::basic_ios< char >& s):
+lanovenko::StreamGuard::StreamGuard(std::basic_ios< char >& s):
   s_(s),
   fill_(s.fill()),
   width_(s.width()),
   precision_(s.precision()),
-  fmt_(s.flags())
+  flags_(s.flags())
 {}
 
-lanovenko::iofmtguard::~iofmtguard()
+lanovenko::StreamGuard::~StreamGuard()
 {
-  s_.width(width_);
   s_.fill(fill_);
+  s_.width(width_);
   s_.precision(precision_);
-  s_.flags(fmt_);
+  s_.flags(flags_);
 }
