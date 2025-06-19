01ae57cb544c918b7b26ac56d69bfd5f6749a6ab
aminedraii19 <118990672+aminedraii19@users.noreply.github.com>
2025-06-19 21:48:17 +0200

    Up

--- drai.mohamedamine/T3/polygon.hpp
+++ drai.mohamedamine/T3/polygon.hpp
@@ -1,47 +1,29 @@
-#include "polygon.hpp"
+#ifndef AMINE_POLYGON_HPP
+#define AMINE_POLYGON_HPP
 
-#include <cmath>
-#include <numeric>
-#include <algorithm>
+#include <vector>
 
 namespace amine
 {
-  bool Point::operator==(const Point& other) const
+  struct Point
   {
-    return x == other.x && y == other.y;
-  }
+    double x;
+    double y;
 
-  Polygon::Polygon(const std::vector<Point>& points) : points_(points)
-  {}
+    bool operator==(const Point& other) const;
+  };
 
-  double Polygon::area() const
+  class Polygon
   {
-    if (points_.size() < 3)
-    {
-      return 0.0;
-    }
+  public:
+    explicit Polygon(const std::vector<Point>& points);
+    double area() const;
+    std::size_t vertexCount() const;
+    bool operator==(const Polygon& other) const;
 
-    double result = std::inner_product(
-      points_.begin(), points_.end() - 1, points_.begin() + 1, 0.0,
-      std::plus<>(),
-      [](const Point& a, const Point& b)
-      {
-        return a.x * b.y - b.x * a.y;
-      }
-    );
-
-    result += points_.back().x * points_.front().y - points_.front().x * points_.back().y;
-
-    return std::abs(result) * 0.5;
-  }
-
-  std::size_t Polygon::vertexCount() const
-  {
-    return points_.size();
-  }
-
-  bool Polygon::operator==(const Polygon& other) const
-  {
-    return points_ == other.points_;
-  }
+  private:
+    std::vector<Point> points_;
+  };
 }
+
+#endif
