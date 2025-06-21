ecf2af3903e0180773363e497abbdf1eb96a61e4
belyaevEDU <dubstephimself@outlook.com>
2025-06-21 20:06:10 +0300

    No file reading

--- belyaev.veniamin/F0/main.cpp
+++ belyaev.veniamin/F0/main.cpp
@@ -1,2 +1,31 @@
-int main()
-{}
+#include "command-handlers.hpp"
+#include "shapes.hpp"
+#include <algorithm>
+#include <iterator>
+#include <limits>
+
+int main(int argc, char* argv[])
+{
+  using namespace belyaev;
+  using namespace std::placeholders;
+
+  std::vector<Dictionary> data;
+  commandMap cmds = mapCommandHandlers(data);
+  std::string command;
+  while (!(std::cin >> command).eof())
+  {
+    try
+    {
+      cmds.at(command)(std::cin, std::cout);
+    }
+    catch (...)
+    {
+      if (std::cin.fail())
+      {
+        std::cin.clear(std::cin.rdstate() ^ std::ios::failbit);
+      }
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cout << "<INVALID COMMAND>\n";
+    }
+  }
+}
