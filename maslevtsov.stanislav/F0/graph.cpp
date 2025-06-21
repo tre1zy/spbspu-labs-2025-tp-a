76f707b9afb18d75159138f6ce7cc061a3c9af1d
jambii1 <s.maslevtsov@gmail.com>
2025-06-21 20:02:29 +0300

    single quotation marks fix

--- maslevtsov.stanislav/F0/graph.cpp
+++ maslevtsov.stanislav/F0/graph.cpp
@@ -6,7 +6,7 @@ void maslevtsov::Graph::print_adjacency_list(std::ostream& out)
   out << edges_.size() << '\n';
   out << edges_.cbegin()->first;
   for (auto i = ++edges_.cbegin(); i != edges_.cend(); ++i) {
-    out << '\n' << i->first << ' :';
+    out << '\n' << i->first << " :";
     for (auto j = i->second.cbegin(); j != i->second.cend(); ++j) {
       out << ' ' << *j;
     }
