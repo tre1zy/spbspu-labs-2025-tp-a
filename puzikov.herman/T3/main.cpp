7b2031a44f5a8395c063f249e6d299d4ea3f9c39
Segfault-chan <hermanpuzikov2004@gmail.com>
2025-06-21 20:46:26 +0300

    change to the main.cpp

--- puzikov.herman/T3/main.cpp
+++ puzikov.herman/T3/main.cpp
@@ -5,25 +5,25 @@
 
 int main(int argc, char **argv)
 {
-  using puzikov::Polygon;
+  using namespace puzikov;
 
   if (argc != 2)
   {
-    std::cerr << "You need to provide a filename 👺\n";
+    std::cerr << "You need to provide a filename\n";
     return 1;
   }
 
   std::ifstream inputFile{argv[1]};
   if (!inputFile)
   {
-    std::cerr << "Couldn't open the file 😢\n";
+    std::cerr << "Couldn't open the file\n";
     return 1;
   }
 
-  std::vector< puzikov::Polygon > polygons;
-  puzikov::readPolygons(inputFile, polygons);
+  std::vector< Polygon > polygons;
+  readPolygons(inputFile, polygons);
   inputFile.close();
 
-  puzikov::CommandHandler cmdHandler(polygons);
+  CommandHandler cmdHandler(polygons);
   cmdHandler.readCommands(std::cin, std::cout);
 }
