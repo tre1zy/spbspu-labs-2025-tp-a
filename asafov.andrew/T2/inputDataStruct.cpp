bed736fbdb49447262a95963c57c74f868a93cdc
shycoshy <neforscist@gmail.com>
2025-06-22 23:38:25 +0000

    tests no.71

--- asafov.andrew/T2/inputDataStruct.cpp
+++ asafov.andrew/T2/inputDataStruct.cpp
@@ -4,120 +4,129 @@
 
 namespace
 {
-  unsigned long long parseULLBin(const std::string& str)
+  unsigned long long parseULLBin(std::istream& is)
   {
-    if (str.length() < 3 || (str[0] != '0' && str[1] != 'b'))
+    char ch;
+    if (!(is >> ch) || ch != '0' || !(is >> ch) || ch != 'b')
     {
       return 0;
     }
 
     unsigned long long result = 0;
-    for (size_t i = 2; i < str.size(); ++i)
+    while (is >> ch)
     {
-      result = result << 1;
-      if (str[i] == '1')
+      if (ch == '0' || ch == '1')
       {
-        result = result | 1;
+        result = (result << 1) | (ch - '0');
       }
-      else if (str[i] != '0')
+      else
       {
-        return 0;
+        is.putback(ch);
+        break;
       }
     }
     return result;
   }
 
-  std::complex< double > parseCmpLsp(const std::string& str)
+  std::complex< double > parseCmpLsp(std::istream& is)
   {
-    if (str.length() < 5 || str[0] != '#' || str[1] != 'c' || str[2] != '(')
+    char ch;
+    if (!(is >> ch) || ch != '#' || !(is >> ch) || ch != 'c' || !(is >> ch) || ch != '(')
     {
       return {0.0, 0.0};
     }
 
-    size_t start = 3;
-    size_t end = str.find_first_of(" )", start);
     double real = 0.0;
     double imag = 0.0;
-
-    std::string real_str = str.substr(start, end - start);
-    try
-    {
-      real = std::stod(real_str);
-    }
-    catch (const std::exception&)
+    is >> real;
+    if (!is)
     {
       return {0.0, 0.0};
     }
-    if (end != std::string::npos && str[end] != ')')
+
+    is >> ch;
+    if (ch != ')')
     {
-      start = end + 1;
-      end = str.find_first_of(" )", start);
-      std::string imag_str = str.substr(start, end - start);
-      try
+      is.putback(ch);
+      is >> imag;
+      if (!is)
       {
-        imag = std::stod(imag_str);
+        return {0.0, 0.0};
       }
-      catch (const std::exception&)
+      is >> ch;
+      if (ch != ')')
       {
         return {0.0, 0.0};
       }
     }
+
     return {real, imag};
   }
 }
 
 std::istream& asafov::operator>>(std::istream& is, DataStruct& data)
 {
-  std::string line;
-  while (std::getline(is, line))
-  {
-    DataStruct temp;
-    bool has_key1 = false;
-    bool has_key2 = false;
-    bool has_key3 = false;
+  DataStruct temp;
+  bool has_key1 = false;
+  bool has_key2 = false;
+  bool has_key3 = false;
 
-    size_t key1_pos = line.find(":key1 ");
-    if (key1_pos != std::string::npos)
+  char ch;
+  while (is >> ch)
+  {
+    if (ch == ':')
     {
-      size_t key1_end = line.find(':', key1_pos + 1);
-      if (key1_end != std::string::npos)
+      std::string key;
+      while (is >> ch && ch != ' ' && ch != ':')
       {
-        std::string key1_str = line.substr(key1_pos + 6, key1_end - (key1_pos + 6));
-        temp.key1 = parseULLBin(key1_str);
-        if (temp.key1 != 0 || key1_str == "0b0")
+        key.push_back(ch);
+      }
+
+      if (key == "key1")
+      {
+        unsigned long long value = parseULLBin(is);
+        if (value != 0 || (is.peek() == '0' && is.get() == '0' && is.peek() ==
+          'b' && is.get() == 'b' && is.peek() ==
+          '0' && is.get() == '0'))
         {
+          temp.key1 = value;
           has_key1 = true;
         }
       }
-    }
-
-    size_t key2_pos = line.find(":key2 ");
-    if (key2_pos != std::string::npos)
-    {
-      size_t key2_end = line.find(':', key2_pos + 1);
-      if (key2_end != std::string::npos)
+      else if (key == "key2")
       {
-        std::string key2_str = line.substr(key2_pos + 6, key2_end - (key2_pos + 6));
-        temp.key2 = parseCmpLsp(key2_str);
-        if (temp.key2 != std::complex< double >{0.0, 0.0} || key2_str == "#c(0.0 0.0)" || key2_str == "#c(0 0)" ||
-          key2_str == "#c(0. 0.)")
+        std::complex< double > value = parseCmpLsp(is);
+        if (value != std::complex< double >{0.0, 0.0} ||
+          (is.peek() == '#' && is.get() == '#' && is.peek() == 'c' && is.get() ==
+            'c' && is.peek() == '(' && is.get() ==
+            '(' &&
+            is.peek() == '0' && is.get() == '0' && is.peek() == '.' && is.get() ==
+            '.' && is.peek() == '0' && is.get()
+            == '0' &&
+            is.peek() == ' ' && is.get() == ' ' &&
+            is.peek() == '0' && is.get() == '0' && is.peek() == '.' && is.get() ==
+            '.' && is.peek() == '0' && is.get()
+            == '0' &&
+            is.peek() == ')' && is.get() == ')'))
         {
+          temp.key2 = value;
           has_key2 = true;
         }
       }
-    }
-
-    size_t key3_pos = line.find(":key3 \"");
-    if (key3_pos != std::string::npos)
-    {
-      size_t quote_pos = key3_pos + 7;
-      size_t closing_quote = line.find('"', quote_pos);
-      if (closing_quote != std::string::npos)
+      else if (key == "key3")
       {
-        temp.key3 = line.substr(quote_pos, closing_quote - quote_pos);
-        if (line.find(':', closing_quote + 1) != std::string::npos)
+        if (is >> ch && ch == '"')
         {
-          has_key3 = true;
+          std::string value;
+          while (is.get(ch) && ch != '"')
+          {
+            value.push_back(ch);
+          }
+          if (ch == '"')
+          {
+            temp.key3 = value;
+            has_key3 = true;
+          }
         }
       }
     }
@@ -127,6 +136,12 @@ std::istream& asafov::operator>>(std::istream& is, DataStruct& data)
       data = temp;
       return is;
     }
+  }
+
+  if (has_key1 && has_key2 && has_key3)
+  {
+    data = temp;
+    return is;
   }
 
   is.setstate(std::ios::failbit);
