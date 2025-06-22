b3091bacfbb200c8d61a8c9a227406a88c6dfca1
shycoshy <neforscist@gmail.com>
2025-06-22 11:38:34 +0000

    tests no.62

--- asafov.andrew/T2/inputDataStruct.cpp
+++ asafov.andrew/T2/inputDataStruct.cpp
@@ -1,133 +1,175 @@
 #include "datastruct.h"
 #include <cctype>
-#include <algorithm>
 
 namespace
 {
-  unsigned long long parseULLBin(const std::string& str)
+  void skipWhitespace(std::istream& in)
   {
-    if (str.length() < 3 || (str[0] != '0' && str[1] != 'b'))
+    while (std::isspace(in.peek()))
+        {
+            in.ignore();
+        }
+  }
+
+  bool expect(std::istream& in, char expected)
+  {
+    skipWhitespace(in);
+    if (in.peek() != expected)
+    {
+      in.setstate(std::ios::failbit);
+      return false;
+    }
+    in.ignore();
+    return true;
+  }
+
+  unsigned long long parseULLBin(std::istream& in)
+  {
+    if (!expect(in, '0'))
+    {
+      return 0;
+    }
+    if (!expect(in, 'b'))
     {
       return 0;
     }
 
     unsigned long long result = 0;
-    for (size_t i = 2; i < str.size(); ++i)
+    while (in.peek() == '0' || in.peek() == '1')
     {
-      result = result << 1;
-      if (str[i] == '1')
-      {
-        result = result | 1;
-      }
-      else if (str[i] != '0')
-      {
-        return 0;
-      }
+      result = (result << 1) | (in.get() - '0');
     }
     return result;
   }
 
-  std::complex< double > parseCmpLsp(const std::string& str)
+  std::complex<double> parseCmpLsp(std::istream& in)
   {
-    if (str.length() < 5 || str[0] != '#' || str[1] != 'c' || str[2] != '(')
+    if (!expect(in, '#'))
+    {
+      return {0.0, 0.0};
+    }
+    if (!expect(in, 'c'))
+    {
+      return {0.0, 0.0};
+    }
+    if (!expect(in, '('))
     {
       return {0.0, 0.0};
     }
 
-    size_t start = 3;
-    size_t end = str.find_first_of(" )", start);
     double real = 0.0;
-    double imag = 0.0;
+    in >> real;
 
-    std::string real_str = str.substr(start, end - start);
-    try
-    {
-      real = std::stod(real_str);
-    }
-    catch (const std::exception&)
+    double imag = 0.0;
+    in >> imag;
+
+    if (!expect(in, ')'))
     {
       return {0.0, 0.0};
     }
-    if (end != std::string::npos && str[end] != ')')
-    {
-      start = end + 1;
-      end = str.find_first_of(" )", start);
-      std::string imag_str = str.substr(start, end - start);
-      try
-      {
-        imag = std::stod(imag_str);
-      }
-      catch (const std::exception&)
-      {
-        return {0.0, 0.0};
-      }
-    }
+
     return {real, imag};
+  }
+
+  std::string parseQuotedString(std::istream& in)
+  {
+    if (!expect(in, '"'))
+    {
+      return "";
+    }
+
+    std::string result;
+    char c;
+    while (in.get(c) && c != '"')
+    {
+      result += c;
+    }
+
+    if (c != '"')
+    {
+      in.setstate(std::ios::failbit);
+      return "";
+    }
+
+    return result;
   }
 }
 
-std::istream& asafov::operator>>(std::istream& is, DataStruct& data)
+std::istream& asafov::operator>>(std::istream& in, DataStruct& data)
 {
-  std::string line;
-  while (std::getline(is, line))
+  DataStruct temp;
+
+  if (!expect(in, '('))
   {
-    DataStruct temp;
-    bool has_key1 = false;
-    bool has_key2 = false;
-    bool has_key3 = false;
-
-    size_t key1_pos = line.find(":key1 ");
-    if (key1_pos != std::string::npos)
-    {
-      size_t key1_end = line.find(':', key1_pos + 1);
-      if (key1_end != std::string::npos)
-      {
-        std::string key1_str = line.substr(key1_pos + 6, key1_end - (key1_pos + 6));
-        temp.key1 = parseULLBin(key1_str);
-        if (temp.key1 != 0 || key1_str == "0b0")
-        {
-          has_key1 = true;
-        }
-      }
-    }
-
-    size_t key2_pos = line.find(":key2 ");
-    if (key2_pos != std::string::npos)
-    {
-      size_t key2_end = line.find(':', key2_pos + 1);
-      if (key2_end != std::string::npos)
-      {
-        std::string key2_str = line.substr(key2_pos + 6, key2_end - (key2_pos + 6));
-        temp.key2 = parseCmpLsp(key2_str);
-        if (temp.key2 != std::complex< double >{0.0, 0.0} || key2_str == "#c(0.0 0.0)" || key2_str == "#c(0 0)" || key2_str == "#c(0. 0.)")
-        {
-          has_key2 = true;
-        }
-      }
-    }
-
-    size_t key3_pos = line.find(":key3 \"");
-    if (key3_pos != std::string::npos)
-    {
-      size_t quote_pos = key3_pos + 7;
-      size_t closing_quote = line.find('"', quote_pos);
-      if (closing_quote != std::string::npos)
-      {
-        temp.key3 = line.substr(quote_pos, closing_quote - quote_pos);
-        if (line.find(':', closing_quote + 1) != std::string::npos)
-        {
-          has_key3 = true;
-        }
-      }
-    }
-
-    if (has_key1 && has_key2 && has_key3)
-    {
-      data = temp;
-      return is;
-    }
+    return in;
+  }
+  if (!expect(in, ':'))
+  {
+    return in;
   }
 
-  is.setstate(std::ios::failbit);
-  return is;
+  std::string key1_tag;
+  in >> key1_tag;
+  if (key1_tag != "key1" || !expect(in, ' '))
+  {
+    in.setstate(std::ios::failbit);
+    return in;
+  }
+
+  temp.key1 = parseULLBin(in);
+  if (temp.key1 == 0 && in.peek() != '0')
+  {
+    in.setstate(std::ios::failbit);
+    return in;
+  }
+
+  if (!expect(in, ':'))
+  {
+    return in;
+  }
+  std::string key2_tag;
+  in >> key2_tag;
+  if (key2_tag != "key2" || !expect(in, ' '))
+  {
+    in.setstate(std::ios::failbit);
+    return in;
+  }
+
+  temp.key2 = parseCmpLsp(in);
+  if (temp.key2 == std::complex<double>{0.0, 0.0})
+  {
+    in.setstate(std::ios::failbit);
+    return in;
+  }
+
+  if (!expect(in, ':'))
+  {
+    return in;
+  }
+  std::string key3_tag;
+  in >> key3_tag;
+  if (key3_tag != "key3" || !expect(in, ' '))
+  {
+    in.setstate(std::ios::failbit);
+    return in;
+  }
+
+  temp.key3 = parseQuotedString(in);
+  if (temp.key3.empty())
+  {
+    in.setstate(std::ios::failbit);
+    return in;
+  }
+
+  if (!expect(in, ':'))
+  {
+    return in;
+  }
+  if (!expect(in, ')'))
+  {
+    return in;
+  }
+
+  data = temp;
+  return in;
 }
