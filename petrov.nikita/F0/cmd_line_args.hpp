39909424282e914669ef2613ba6dc884d9b2430d
Nekich06 <p.nikita.73526@gmail.com>
2025-06-24 22:48:05 +0300

    Implemented processing of some command line arguments --check will in some of next commits

---
+++ petrov.nikita/F0/cmd_line_args.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+namespace petrov
+{
+  void outputHelp();
+  void setPrecision(std::ostream & out, const size_t & precision);
+
+  void processCmdLineAgruments(const int agrc, const char * const * argv);
+}
--- petrov.nikita/F0/main.cpp
+++ petrov.nikita/F0/main.cpp
@@ -1,2 +1,29 @@
-int main()
-{}
+#include <map>
+#include <string>
+#include <functional>
+#include <iostream>
+#include "cmd_line_args.hpp"
+
+namespace
+{
+  int checkArguments(const int argc)
+  {
+    if (argc > 1)
+    {
+      return 1;
+    }
+    else
+    {
+      return 0;
+    }
+  }
+}
+
+int main(const int argc, const char * const * argv)
+{
+  using namespace petrov;
+  if (checkArguments(argc))
+  {
+    processCmdLineAgruments(argc, argv);
+  }
+}
