d95940d107eacb3f329e5f7af6e3861bd2bae602
faxryzen <cheburek2006@gmail.com>
2025-06-25 02:16:30 +0300

    Написал че мне предстоит завтра сделать =D

--- savintsev.ilya/F0/project-cmds.hpp
+++ savintsev.ilya/F0/project-cmds.hpp
@@ -15,7 +15,34 @@ namespace savintsev
   void render(std::istream & in, std::ostream & out, Projects & projs);
   void merge(std::istream & in, std::ostream & out, Projects & projs);
   void save_all(std::ostream & out, Projects & projs);
+
+  void create_rectangle(std::istream & in, std::ostream & out, Projects & projs);
+  void create_concave(std::istream & in, std::ostream & out, Projects & projs);
+  void create_complexquad(std::istream & in, std::ostream & out, Projects & projs);
+
+  void delete_shape(std::istream & in, std::ostream & out, Projects & projs);
+  void copy_shape(std::istream & in, std::ostream & out, Projects & projs);
+  void insert_shape(std::istream & in, std::ostream & out, Projects & projs);
+  void rename_shape(std::istream & in, std::ostream & out, Projects & projs);
+  void swap_shapes(std::istream & in, std::ostream & out, Projects & projs);
+
+  void sf(std::istream & in, std::ostream & out, Projects & projs);
+  void sf_above(std::istream & in, std::ostream & out, Projects & projs);
+  void sf_below(std::istream & in, std::ostream & out, Projects & projs);
+
+  void move_abs(std::istream & in, std::ostream & out, Projects & projs);
+  void move_rel(std::istream & in, std::ostream & out, Projects & projs);
+  void move_all(std::istream & in, std::ostream & out, Projects & projs);
+  void scale_shape(std::istream & in, std::ostream & out, Projects & projs);
+  void color_shape(std::istream & in, std::ostream & out, Projects & projs);
+
   void print(std::ostream & out, Projects & projs);
+  void print_shapes(std::istream & in, std::ostream & out, Projects & projs);
+  void print_info_about_shape(std::istream & in, std::ostream & out, Projects & projs);
+
+  void array_shapes(std::istream & in, std::ostream & out, Projects & projs);
+  void reverse_project(std::istream & in, std::ostream & out, Projects & projs);
+  void clear_project(std::istream & in, std::ostream & out, Projects & projs);
 }
 
 #endif
