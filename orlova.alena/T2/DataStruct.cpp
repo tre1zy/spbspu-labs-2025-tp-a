aff241e9dac97d0edf987c567d34055a27171b18
widgeiw <crazynas228@gmail.com>
2025-06-24 12:43:49 +0300

    adding common

--- orlova.alena/T2/DataStruct.cpp
+++ orlova.alena/T2/DataStruct.cpp
@@ -1,20 +1,6 @@
 #include "DataStruct.h"
-
-std::istream& orlova::operator>>(std::istream& in, DelimiterIO&& dest)
-{
-  std::istream::sentry sentry(in);
-  if (!sentry)
-  {
-    return in;
-  }
-  char c = '0';
-  in >> c;
-  if (in && (c != dest.exp))
-  {
-    in.setstate(std::ios::failbit);
-  }
-  return in;
-}
+#include <io-guard.h>
+#include <delimiter.h>
 
 std::istream& orlova::operator>>(std::istream& in, LongLongIO&& dest)
 {
