889b5bbefe97e3dd3cbed15f7098b2d14a653c01
mipxe <mariamozegova8726@gmail.com>
2025-06-24 17:52:28 +0300

    create interface

--- mozhegova.maria/F0/main.cpp
+++ mozhegova.maria/F0/main.cpp
@@ -1,2 +1,55 @@
-int main()
-{}
+#include <map>
+#include <list>
+#include <iostream>
+#include <fstream>
+#include <limits>
+#include <iterator>
+#include <algorithm>
+#include <functional>
+#include "commands.hpp"
+
+int main(int argc, char * argv[])
+{
+  using namespace mozhegova;
+  if (argc == 2 && std::string(argv[1]) == "--help")
+  {
+    printHelp(std::cout);
+    return 0;
+  }
+
+  std::unordered_map< std::string, std::unordered_map< std::string, std::list< std::pair< size_t, size_t > > > > texts;
+  std::map< std::string, std::function< void() > > cmds;
+  cmds["generatelinks"] = std::bind(generateLinks, std::ref(std::cin), std::ref(texts));
+  cmds["removelinks"] = std::bind(removeLinks, std::ref(std::cin), std::ref(texts));
+  cmds["printlinks"] = std::bind(printLinks, std::ref(std::cin), std::ref(std::cout), std::cref(texts));
+  cmds["printtext"] = std::bind(printText, std::ref(std::cin), std::ref(std::cout), std::cref(texts));
+  // cmds["printtextin"] = std::bind(printText, std::ref(std::cin), std::ref(std::cout), std::cref(texts));
+  cmds["merge"] = std::bind(mergeTexts, std::ref(std::cin), std::ref(texts));
+  cmds["insert"] = std::bind(insertText, std::ref(std::cin), std::ref(texts));
+  cmds["removelines"] = std::bind(removeLines, std::ref(std::cin), std::ref(texts));
+  cmds["move"] = std::bind(moveText, std::ref(std::cin), std::ref(texts));
+  cmds["sidemerge"] = std::bind(sideMergeTexts, std::ref(std::cin), std::ref(texts));
+  cmds["split"] = std::bind(splitTexts, std::ref(std::cin), std::ref(texts));
+  cmds["invertlines"] = std::bind(invertLines, std::ref(std::cin), std::ref(texts));
+  cmds["invertwords"] = std::bind(invertWords, std::ref(std::cin), std::ref(texts));
+  cmds["replaceword"] = std::bind(replaceWord, std::ref(std::cin), std::ref(texts));
+  cmds["save"] = std::bind(save, std::ref(std::cin), std::cref(texts));
+
+  std::string command;
+  while (!(std::cin >> command).eof())
+  {
+    try
+    {
+      cmds.at(command)();
+    }
+    catch (...)
+    {
+      if (std::cin.fail())
+      {
+        std::cin.clear(std::cin.rdstate() ^ std::ios::failbit);
+      }
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+      std::cout << "<INVALID COMMAND>\n";
+    }
+  }
+}
