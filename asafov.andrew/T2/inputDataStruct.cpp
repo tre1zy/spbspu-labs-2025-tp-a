7645155e6938c51b68dbfc31fcd6bcf83e9a9f4a
shycoshy <neforscist@gmail.com>
2025-06-24 17:11:28 +0000

    tests no.84

--- asafov.andrew/T2/inputDataStruct.cpp
+++ asafov.andrew/T2/inputDataStruct.cpp
@@ -1,6 +1,7 @@
 #include "datastruct.h"
 #include <cctype>
 #include <algorithm>
+#include <sstream>
 
 namespace
 {
@@ -88,111 +89,74 @@ namespace
 
 std::istream& asafov::operator>>(std::istream& is, DataStruct& data)
 {
-  DataStruct temp;
-  char ch;
-
-  is >> std::ws;
-  if (!is.get(ch) || ch != '(')
+  std::string line;
+  while (std::getline(is, line))
   {
-    is.setstate(std::ios::failbit);
-    return is;
-  }
+    std::istringstream iss(line);
+    DataStruct temp;
+    bool has_key1 = false;
+    bool has_key2 = false;
+    bool has_key3 = false;
+    char ch;
 
-  is >> std::ws;
-  if (!is.get(ch) || ch != ':')
-  {
-    is.setstate(std::ios::failbit);
-    return is;
-  }
-
-  bool has_key1 = false;
-  bool has_key2 = false;
-  bool has_key3 = false;
-
-  while (true)
-  {
-    std::string key;
-    is >> std::ws;
-
-    while (is.get(ch) && ch != ' ' && ch != ':')
+    while (iss.get(ch))
     {
-      key += ch;
-    }
-
-    if (!is)
-    {
-      is.setstate(std::ios::failbit);
-      return is;
-    }
-
-    if (key == "key1")
-    {
-      is >> std::ws;
-      temp.key1 = parseULLBin(is);
-      if (is.fail())
+      if (ch == ':')
       {
-        return is;
-      }
-      has_key1 = true;
-    }
-    else if (key == "key2")
-    {
-      is >> std::ws;
-      temp.key2 = parseCmpLsp(is);
-      if (is.fail())
-      {
-        return is;
-      }
-      has_key2 = true;
-    }
-    else if (key == "key3")
-    {
-      is >> std::ws;
-      if (!is.get(ch) || ch != '"')
-      {
-        is.setstate(std::ios::failbit);
-        return is;
-      }
+        std::string key;
+        while (iss.get(ch) && ch != ' ' && ch != '\n' && ch != ':')
+        {
+          key.push_back(ch);
+        }
+        iss.unget();
 
-      temp.key3.clear();
-      while (is.get(ch) && ch != '"')
-      {
-        temp.key3 += ch;
+        if (key == "key1")
+        {
+          iss >> std::ws;
+          unsigned long long val = parseULLBin(iss);
+          if (!iss.fail())
+          {
+            temp.key1 = val;
+            has_key1 = true;
+          }
+        }
+        else if (key == "key2")
+        {
+          iss >> std::ws;
+          std::complex< double > val = parseCmpLsp(iss);
+          if (!iss.fail())
+          {
+            temp.key2 = val;
+            has_key2 = true;
+          }
+        }
+        else if (key == "key3")
+        {
+          iss >> std::ws;
+          if (iss.get(ch) && ch == '"')
+          {
+            std::string str;
+            while (iss.get(ch) && ch != '"' && ch != '\n')
+            {
+              str.push_back(ch);
+            }
+            if (ch == '"')
+            {
+              temp.key3 = str;
+              has_key3 = true;
+            }
+          }
+        }
       }
-
-      if (ch != '"')
-      {
-        is.setstate(std::ios::failbit);
-        return is;
-      }
-      has_key3 = true;
     }
 
-    is >> std::ws;
-    if (!is.get(ch))
+    if (has_key1 && has_key2 && has_key3)
     {
-      is.setstate(std::ios::failbit);
-      return is;
-    }
-
-    if (ch == ')')
-    {
-      break;
-    }
-    else if (ch != ':')
-    {
-      is.setstate(std::ios::failbit);
+      data = temp;
       return is;
     }
   }
 
-  if (has_key1 && has_key2 && has_key3)
-  {
-    data = temp;
-  }
-  else
-  {
-    is.setstate(std::ios::failbit);
-  }
+  is.setstate(std::ios::failbit);
   return is;
 }
