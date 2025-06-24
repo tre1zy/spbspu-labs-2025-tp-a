3a130f69b697a563561c4a88f5827a5f03889810
A1exCRE <alsmmm2065@gmail.com>
2025-06-25 00:27:02 +0300

    the billet main.cpp

--- smirnov.alexandr/F0/main.cpp
+++ smirnov.alexandr/F0/main.cpp
@@ -1,2 +1,27 @@
+#include <iostream>
+#include <functional>
+#include <limits>
+#include <guard.hpp>
+#include <delimetr.hpp>
+#include "dictionary.hpp"
+
 int main()
-{}
+{
+  using namespace smirnov;
+  Dicts dicts;
+  std::unordered_map< std::string, std::function< void(std::istream &) > > commands;
+  std::string cmd;
+  while (std::cin >> cmd)
+  {
+    try
+    {
+      std::cout << "Команда: " << cmd << "\n";
+    }
+    catch (const std::exception &)
+    {
+      std::cout << "<INVALID COMMAND>\n";
+    }
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+  }
+}
