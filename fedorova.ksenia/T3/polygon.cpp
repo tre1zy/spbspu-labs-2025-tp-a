9637c7235d0b5240d42e496c9d112f36640c3300
lolnyok <f.kcusha082@mail.ru>
2025-06-25 01:09:28 +0300

    2operator>>

--- fedorova.ksenia/T3/polygon.cpp
+++ fedorova.ksenia/T3/polygon.cpp
@@ -9,3 +9,35 @@ bool fedorova::operator==(const Polygon& lhs, const Polygon& rhs)
 {
     return lhs.points == rhs.points;
 }
+
+std::istream& fedorova::operator>>(std::istream& in, Point& p)
+{
+    char discard;
+    if (!(in >> discard >> p.x >> discard >> p.y >> discard) || discard != ')')
+    {
+        in.setstate(std::ios::failbit);
+    }
+    return in;
+}
+
+std::istream& fedorova::operator>>(std::istream& in, Polygon& poly)
+{
+    size_t vertexCount;
+    if (!(in >> vertexCount) || vertexCount < 3)
+    {
+        in.setstate(std::ios::failbit);
+        return in;
+    }
+
+    poly.points.resize(vertexCount);
+    for (auto& point : poly.points)
+    {
+        if (!(in >> point))
+        {
+            in.setstate(std::ios::failbit);
+            break;
+        }
+    }
+
+    return in;
+}
