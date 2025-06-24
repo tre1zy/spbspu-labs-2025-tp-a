7f9eebfca309227e92d5c31ee4f76c50e6199de3
lolnyok <f.kcusha082@mail.ru>
2025-06-25 01:08:28 +0300

    2operator==

---
+++ fedorova.ksenia/T3/polygon.cpp
@@ -0,0 +1,11 @@
+#include "polygon.h"
+
+bool fedorova::operator==(const Point& lhs, const Point& rhs)
+{
+    return lhs.x == rhs.x && lhs.y == rhs.y;
+}
+
+bool fedorova::operator==(const Polygon& lhs, const Polygon& rhs)
+{
+    return lhs.points == rhs.points;
+}
