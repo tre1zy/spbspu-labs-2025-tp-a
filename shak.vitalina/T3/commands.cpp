78010a9ac6a9295084de20b2c79f4208d8d10ee5
vitsh1 <vips65653@gmail.com>
2025-06-25 12:14:16 +0300

    add std::map in sumcmds

--- shak.vitalina/T3/commands.cpp
+++ shak.vitalina/T3/commands.cpp
@@ -6,6 +6,121 @@
 #include <string>
 #include "polygon.hpp"
 
+void getAreaEven(std::ostream &out, const std::vector< shak::Polygon > &polygons)
+{
+  std::vector< shak::Polygon > currentPolygons;
+  std::copy_if(std::begin(polygons), std::end(polygons), std::back_inserter(currentPolygons), shak::isEven);
+  std::vector< double > polygonAreas(currentPolygons.size());
+  std::transform(std::begin(currentPolygons), std::end(currentPolygons), std::begin(polygonAreas), shak::getArea);
+  out << std::fixed;
+  out.precision(1);
+  out << std::accumulate(polygonAreas.begin(), polygonAreas.end(), double{}) << "\n";
+}
+
+void getAreaOdd(std::ostream &out, const std::vector< shak::Polygon > &polygons)
+{
+  std::vector< shak::Polygon > currentPolygons;
+  std::copy_if(std::begin(polygons), std::end(polygons), std::back_inserter(currentPolygons), shak::isOdd);
+  std::vector< double > polygonAreas(currentPolygons.size());
+  std::transform(std::begin(currentPolygons), std::end(currentPolygons), std::begin(polygonAreas), shak::getArea);
+  out << std::fixed;
+  out.precision(1);
+  out << std::accumulate(polygonAreas.begin(), polygonAreas.end(), double{}) << "\n";
+}
+
+void getAreaMean(std::ostream &out, const std::vector< shak::Polygon > &polygons)
+{
+  if (polygons.size() == 0)
+  {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+  std::vector< double > polygonAreas(polygons.size());
+  std::transform(std::begin(polygons), std::end(polygons), std::begin(polygonAreas), shak::getArea);
+  out << std::fixed;
+  out.precision(1);
+  out << std::accumulate(polygonAreas.begin(), polygonAreas.end(), double{}) / polygons.size() << "\n";
+}
+
+void getAreaVertexes(std::ostream &out, const std::vector< shak::Polygon > &polygons, size_t vertexCount)
+{
+  std::vector< shak::Polygon > currentPolygons;
+  auto areEqualVertexCount = std::bind(shak::areEqualVertexes, vertexCount, std::placeholders::_1);
+  std::copy_if(polygons.begin(), polygons.end(), std::back_inserter(currentPolygons), areEqualVertexCount);
+  std::vector< double > polygonAreas(currentPolygons.size());
+  std::transform(std::begin(currentPolygons), std::end(currentPolygons), std::begin(polygonAreas), shak::getArea);
+  out << std::fixed;
+  out.precision(1);
+  out << std::accumulate(polygonAreas.begin(), polygonAreas.end(), double{}) << "\n";
+}
+
+void getMaxArea(std::ostream &out, const std::vector< shak::Polygon > &polygons)
+{
+  if (polygons.empty())
+  {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+  std::vector< double > polygonAreas(polygons.size());
+  std::transform(std::begin(polygons), std::end(polygons), polygonAreas.begin(), shak::getArea);
+  auto maxIter = std::max_element(polygonAreas.begin(), polygonAreas.end());
+  out << std::fixed << std::setprecision(1) << *maxIter << "\n";
+}
+
+void getMaxVertexes(std::ostream &out, const std::vector< shak::Polygon > &polygons)
+{
+  if (polygons.empty())
+  {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+  std::vector< size_t > currentPolygons(polygons.size());
+  std::transform(std::begin(polygons), std::end(polygons), currentPolygons.begin(), shak::getVertexes);
+  auto maxIter = std::max_element(currentPolygons.begin(), currentPolygons.end());
+  out << *maxIter << "\n";
+}
+
+void getMinArea(std::ostream &out, const std::vector< shak::Polygon > &polygons)
+{
+  if (polygons.empty())
+  {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+  std::vector< double > polygonAreas(polygons.size());
+  std::transform(std::begin(polygons), std::end(polygons), polygonAreas.begin(), shak::getArea);
+  auto minIter = std::min_element(polygonAreas.begin(), polygonAreas.end());
+  out << std::fixed << std::setprecision(1) << *minIter << "\n";
+}
+
+void getMinVertexes(std::ostream &out, const std::vector< shak::Polygon > &polygons)
+{
+  if (polygons.empty())
+  {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+  std::vector< size_t > polygonVertexes(polygons.size());
+  std::transform(std::begin(polygons), std::end(polygons), polygonVertexes.begin(), shak::getVertexes);
+  auto minIter = std::min_element(polygonVertexes.begin(), polygonVertexes.end());
+  out << *minIter << "\n";
+}
+
+void countEven(std::ostream &out, const std::vector< shak::Polygon > &polygons)
+{
+  out << std::count_if(polygons.begin(), polygons.end(), shak::isEven) << "\n";
+}
+
+void countOdd(std::ostream &out, const std::vector< shak::Polygon > &polygons)
+{
+  out << std::count_if(polygons.begin(), polygons.end(), shak::isOdd) << "\n";
+}
+
+void getCountVertexes(std::ostream &out, const std::vector< shak::Polygon > &polygons, size_t vertexCount)
+{
+  out << std::count_if(polygons.begin(), polygons.end(), std::bind(shak::areEqualVertexes, vertexCount, std::placeholders::_1)) << "\n";
+}
+
 void shak::createCommandHandler(std::map< std::string, std::function< void() > > &cmds, std::vector< Polygon > &polygons)
 {
   cmds["AREA"] = std::bind(cmdArea, std::cref(polygons), std::ref(std::cin), std::ref(std::cout));
@@ -18,156 +133,84 @@ void shak::createCommandHandler(std::map< std::string, std::function< void() > >
 
 void shak::cmdArea(const std::vector< Polygon > &polygons, std::istream &in, std::ostream &out)
 {
-  std::vector< Polygon > currentPolygons;
-  std::vector< double > polygonAreas;
-  size_t vertexCount = 0;
-  size_t counter = 0;
   std::string subcmd;
   in >> subcmd;
-  if (subcmd == "EVEN")
+  std::map< std::string, std::function< void() > > subcmds;
+  subcmds["EVEN"] = std::bind(getAreaEven, std::ref(out), std::cref(polygons));
+  subcmds["ODD"] = std::bind(getAreaOdd, std::ref(out), std::cref(polygons));
+  subcmds["MEAN"] = std::bind(getAreaMean, std::ref(out), std::cref(polygons));
+  try
   {
-    counter = count_if(std::begin(polygons), std::end(polygons), isEven);
-    currentPolygons.reserve(counter);
-    polygonAreas.reserve(counter);
-    std::copy_if(std::begin(polygons), std::end(polygons), std::back_inserter(currentPolygons), isEven);
+    subcmds.at(subcmd)();
   }
-  else if (subcmd == "ODD")
+  catch (const std::exception &e)
   {
-    counter = count_if(std::begin(polygons), std::end(polygons), isOdd);
-    currentPolygons.reserve(counter);
-    polygonAreas.reserve(counter);
-    std::copy_if(std::begin(polygons), std::end(polygons), std::back_inserter(currentPolygons), isOdd);
-  }
-  else if (subcmd == "MEAN")
-  {
-    if (polygons.size() == 0)
-    {
-      out << "<INVALID COMMAND>\n";
-      return;
-    }
-    std::transform(std::begin(polygons), std::end(polygons), std::back_inserter(polygonAreas), getArea);
-    out << std::fixed;
-    out.precision(1);
-    out << (std::accumulate(polygonAreas.begin(), polygonAreas.end(), 0.0, std::plus< double >{}) / polygons.size()) << "\n";
-    return;
-  }
-  else
-  {
-    vertexCount = std::stoull(subcmd);
+    size_t vertexCount = std::stoull(subcmd);
     if (vertexCount < 3)
     {
       out << "<INVALID COMMAND>\n";
       return;
     }
-    else
-    {
-      using namespace std::placeholders;
-      counter = count_if(std::begin(polygons), std::end(polygons), std::bind(areEqualVertexes, vertexCount, _1));
-      currentPolygons.reserve(counter);
-      polygonAreas.reserve(counter);
-      auto areEqualVertexCount = std::bind(areEqualVertexes, vertexCount, _1);
-      std::copy_if(polygons.begin(), polygons.end(), std::back_inserter(currentPolygons), areEqualVertexCount);
-    }
+    getAreaVertexes(out, polygons, vertexCount);
   }
-  std::transform(std::begin(currentPolygons), std::end(currentPolygons), std::back_inserter(polygonAreas), getArea);
-  out << std::fixed;
-  out.precision(1);
-  out << std::accumulate(polygonAreas.begin(), polygonAreas.end(), 0.0, std::plus< double >{}) << "\n";
 }
 
-void shak::cmdMax(const std::vector< Polygon > &polygon, std::istream &in, std::ostream &out)
+void shak::cmdMax(const std::vector< Polygon > &polygons, std::istream &in, std::ostream &out)
 {
-  if (polygon.size() == 0)
-  {
-    out << "<INVALID COMMAND>\n";
-    return;
-  }
-  else
-  {
-    std::string subcmd;
-    in >> subcmd;
-    if (subcmd == "AREA")
-    {
-      std::vector< double > currentPolygons(polygon.size());
-      std::transform(std::begin(polygon), std::end(polygon), currentPolygons.begin(), getArea);
-      auto maxIter = std::max_element(currentPolygons.begin(), currentPolygons.end());
-      out << std::fixed << std::setprecision(1) << *maxIter << "\n";
-    }
-    else if (subcmd == "VERTEXES")
-    {
-      std::vector< size_t > currentPolygons(polygon.size());
-      std::transform(std::begin(polygon), std::end(polygon), currentPolygons.begin(), getVertexes);
-      auto maxIter = std::max_element(currentPolygons.begin(), currentPolygons.end());
-      out << *maxIter << "\n";
-    }
-    else
-    {
-      out << "<INVALID COMMAND>\n";
-      return;
-    }
-  }
-}
-
-void shak::cmdMin(const std::vector< Polygon > &polygon, std::istream &in, std::ostream &out)
-{
-  if (polygon.size() == 0)
-  {
-    out << "<INVALID COMMAND>\n";
-    return;
-  }
-  else
-  {
-    std::string subcmd;
-    in >> subcmd;
-
-    if (subcmd == "AREA")
-    {
-      std::vector< double > currentPolygons(polygon.size());
-      std::transform(std::begin(polygon), std::end(polygon), currentPolygons.begin(), getArea);
-      auto minIter = std::min_element(currentPolygons.begin(), currentPolygons.end());
-      out << std::fixed << std::setprecision(1) << *minIter << "\n";
-    }
-    else if (subcmd == "VERTEXES")
-    {
-      std::vector< size_t > currentPolygons(polygon.size());
-      std::transform(std::begin(polygon), std::end(polygon), currentPolygons.begin(), getVertexes);
-      auto minIter = std::min_element(currentPolygons.begin(), currentPolygons.end());
-      out << *minIter << "\n";
-}
-    else
-    {
-      out << "<INVALID COMMAND>\n";
-      return;
-    }
-  }
-}
-
-void shak::cmdCount(const std::vector< Polygon > &polygon, std::istream &in, std::ostream &out)
-{
-  size_t vertexCount = 0;
   std::string subcmd;
   in >> subcmd;
-  if (subcmd == "EVEN")
+  std::map< std::string, std::function< void() > > subcmds;
+  subcmds["AREA"] = std::bind(getMaxArea, std::ref(out), std::cref(polygons));
+  subcmds["VERTEXES"] = std::bind(getMaxVertexes, std::ref(out), std::cref(polygons));
+  try
   {
-    out << std::count_if(polygon.begin(), polygon.end(), isEven) << "\n";
+    subcmds.at(subcmd)();
   }
-  else if (subcmd == "ODD")
+  catch (const std::exception &e)
   {
-    out << std::count_if(polygon.begin(), polygon.end(), isOdd) << "\n";
+    out << "<INVALID COMMAND>\n";
+    return;
   }
-  else
+}
+
+void shak::cmdMin(const std::vector< Polygon > &polygons, std::istream &in, std::ostream &out)
+{
+  std::string subcmd;
+  in >> subcmd;
+  std::map< std::string, std::function< void() > > subcmds;
+  subcmds["AREA"] = std::bind(getMinArea, std::ref(out), std::cref(polygons));
+  subcmds["VERTEXES"] = std::bind(getMinVertexes, std::ref(out), std::cref(polygons));
+  try
   {
-    vertexCount = std::stoull(subcmd);
+    subcmds.at(subcmd)();
+  }
+  catch (const std::exception &e)
+  {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+}
+
+void shak::cmdCount(const std::vector< Polygon > &polygons, std::istream &in, std::ostream &out)
+{
+  std::string subcmd;
+  in >> subcmd;
+  std::map< std::string, std::function< void() > > subcmds;
+  subcmds["EVEN"] = std::bind(countEven, std::ref(out), std::cref(polygons));
+  subcmds["ODD"] = std::bind(countOdd, std::ref(out), std::cref(polygons));
+  try
+  {
+    subcmds.at(subcmd)();
+  }
+  catch (const std::exception &e)
+  {
+    size_t vertexCount = std::stoull(subcmd);
     if (vertexCount < 3)
     {
       out << "<INVALID COMMAND>\n";
       return;
     }
-    else
-    {
-      using namespace std::placeholders;
-      out << std::count_if(polygon.begin(), polygon.end(), std::bind(areEqualVertexes, vertexCount, _1)) << "\n";
-    }
+    getCountVertexes(out, polygons, vertexCount);
   }
 }
 
