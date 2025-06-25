eedf33253140a20f8e8a2aeb7a3af5c52cf2b184
aminedraii19 <118990672+aminedraii19@users.noreply.github.com>
2025-06-25 17:44:12 +0200

    Fix

--- drai.mohamedamine/T3/polygon.hpp
+++ drai.mohamedamine/T3/polygon.hpp
@@ -1,32 +1,31 @@
 #ifndef POLYGON_HPP
 #define POLYGON_HPP
 
-#include <iostream>
-#include <string>
 #include <vector>
+#include <string>
+
+struct Point
+{
+  int x;
+  int y;
+
+  Point() = default;
+  Point(int x, int y):
+    x(x),
+    y(y)
+  {}
+};
+
+struct Polygon
+{
+  std::vector< Point > points;
+};
 
 namespace amine
 {
-
-  struct Point
-  {
-    int x;
-    int y;
-    Point(int x_val = 0, int y_val = 0):
-      x(x_val),
-      y(y_val)
-    {}
-  };
-
-  struct Polygon
-  {
-    std::vector< Point > points;
-  };
-
   Point operator-(const Point& a, const Point& b);
   int dot(const Point& a, const Point& b);
   int cross(const Point& a, const Point& b);
-
   double compute_area(const Polygon& poly);
   bool is_rectangle(const Polygon& poly);
   long long orient(const Point& p, const Point& q, const Point& r);
@@ -35,9 +34,6 @@ namespace amine
   bool point_in_polygon(const Point& pt, const Polygon& poly);
   bool polygons_intersect(const Polygon& a, const Polygon& b);
   bool parse_polygon(const std::string& str, Polygon& poly);
-
-  void process_commands(std::vector< Polygon >& polygons);
-
 }
 
 #endif
