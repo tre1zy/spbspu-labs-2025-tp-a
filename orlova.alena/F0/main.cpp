f8cbde11b8ac4bc4c96163c8dd3a29a450ecd926
widgeiw <crazynas228@gmail.com>
2025-06-25 16:27:54 +0300

    adding main.cpp

--- orlova.alena/F0/main.cpp
+++ orlova.alena/F0/main.cpp
@@ -1,3 +1,77 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+#include <limits>
+#include <functional>
+#include "commands.h"
 
+int main(int argc, char** argv)
+{
+  using namespace orlova;
+  using commands = std::map< std::string, std::function< void() > >;
+
+  if (argc > 2)
+  {
+    std::cerr << "<INVALID ARGUMENTS>\n";
+    return 1;
+  }
+  std::ifstream file;
+  if (argc == 2)
+  {
+    file.open(argv[1]);
+    if (!file.is_open())
+    {
+      std::cerr << "<INVALID FILE>\n";
+      return 1;
+    }
+  }
+
+  try
+  {
+    dictionaries dicts;
+    if (argc == 2)
+    {
+      file >> dicts;
+      if (file.fail() && !file.eof())
+      {
+        throw std::logic_error("<INVALID FILE>");
+      }
+    }
+    commands cmds;
+    cmds["addemptydictionary"] = std::bind(addEmptyDictionary, std::ref(std::cin), std::ref(std::cout), std::ref(dicts));
+    cmds["addtranslation"] = std::bind(addTranslation, std::ref(std::cout), std::cref(dicts));
+    cmds["adddictionary"] = std::bind(addDictionary, std::ref(std::cin), std::ref(std::cout), std::cref(dicts));
+    cmds["find"] = std::bind(find, std::ref(std::cin), std::ref(std::cout), std::ref(dicts));
+    cmds["merge"] = std::bind(merge, std::ref(std::cin), std::ref(std::cout), std::ref(dicts));
+    cmds["clear"] = std::bind(clear, std::ref(std::cin), std::ref(std::cout), std::ref(dicts));
+    cmds["removeword"] = std::bind(removeword, std::ref(std::cin), std::ref(std::cout), std::cref(dicts));
+    cmds["append"] = std::bind(append, std::ref(std::cin), std::ref(std::cout), std::cref(dicts));
+    cmds["dictionarysize"] = std::bind(dictionarySize, std::ref(std::cin), std::ref(std::cout), std::ref(dicts));
+    cmds["intersectionofdicts"] = std::bind(intersectionOfDicts, std::ref(std::cin), std::ref(std::cout), std::ref(dicts));
+    cmds["delete"] = std::bind(delete, std::ref(std::cin), std::ref(std::cout), std::cref(dicts));;
+    cmds["nonrepeatingwords"] = std::bind(nonrepeatingWords, std::ref(std::cin), std::ref(std::cout), std::ref(dicts));
+    cmds["residual"] = std::bind(residual, std::ref(std::cin), std::ref(std::cout), std::cref(dicts));
+
+    std::string command;
+    while (!(std::cin >> command).eof())
+    {
+      try
+      {
+        cmds.at(command)();
+        std::cout << '\n';
+      }
+      catch (const std::exception& e)
+      {
+        if (std::cin.fail())
+        {
+          std::cin.clear(std::cin.rdstate() ^ std::ios::failbit);
+        }
+        std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+        std::cerr << "<INVALID COMMAND>\n";
+      }
+    }
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+  }
+}
