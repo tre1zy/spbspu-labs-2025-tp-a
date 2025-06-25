4da1c5e09a6373800077be5ff34132930b172615
mipxe <mariamozegova8726@gmail.com>
2025-06-25 04:03:52 +0300

    create main

--- mozhegova.maria/F0/main.cpp
+++ mozhegova.maria/F0/main.cpp
@@ -1,5 +1,5 @@
 #include <map>
-#include <list>
+#include <vector>
 #include <iostream>
 #include <fstream>
 #include <limits>
@@ -17,13 +17,13 @@ int main(int argc, char * argv[])
     return 0;
   }
 
-  std::unordered_map< std::string, std::unordered_map< std::string, std::list< std::pair< size_t, size_t > > > > texts;
+  std::unordered_map< std::string, std::unordered_map< std::string, std::vector< std::pair< size_t, size_t > > > > texts;
   std::map< std::string, std::function< void() > > cmds;
   cmds["generatelinks"] = std::bind(generateLinks, std::ref(std::cin), std::ref(texts));
   cmds["removelinks"] = std::bind(removeLinks, std::ref(std::cin), std::ref(texts));
   cmds["printlinks"] = std::bind(printLinks, std::ref(std::cin), std::ref(std::cout), std::cref(texts));
   cmds["printtext"] = std::bind(printText, std::ref(std::cin), std::ref(std::cout), std::cref(texts));
-  // cmds["printtextin"] = std::bind(printText, std::ref(std::cin), std::ref(std::cout), std::cref(texts));
+  cmds["printtextinfile"] = std::bind(printTextInFile, std::ref(std::cin), std::cref(texts));
   cmds["merge"] = std::bind(mergeTexts, std::ref(std::cin), std::ref(texts));
   cmds["insert"] = std::bind(insertText, std::ref(std::cin), std::ref(texts));
   cmds["removelines"] = std::bind(removeLines, std::ref(std::cin), std::ref(texts));
@@ -31,7 +31,7 @@ int main(int argc, char * argv[])
   cmds["sidemerge"] = std::bind(sideMergeTexts, std::ref(std::cin), std::ref(texts));
   cmds["split"] = std::bind(splitTexts, std::ref(std::cin), std::ref(texts));
   cmds["invertlines"] = std::bind(invertLines, std::ref(std::cin), std::ref(texts));
-  cmds["invertwords"] = std::bind(invertWords, std::ref(std::cin), std::ref(texts));
+  // cmds["invertwords"] = std::bind(invertWords, std::ref(std::cin), std::ref(texts));
   cmds["replaceword"] = std::bind(replaceWord, std::ref(std::cin), std::ref(texts));
   cmds["save"] = std::bind(save, std::ref(std::cin), std::cref(texts));
 
