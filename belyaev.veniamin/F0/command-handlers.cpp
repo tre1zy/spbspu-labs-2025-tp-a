d07b79f921e769640294e642ca825157f6322425
belyaevEDU <dubstephimself@outlook.com>
2025-06-25 21:34:28 +0300

    УЖАС

--- belyaev.veniamin/F0/command-handlers.cpp
+++ belyaev.veniamin/F0/command-handlers.cpp
@@ -2,6 +2,8 @@
 #include <iterator>
 #include <algorithm>
 #include <numeric>
+#include <fstream>
+#include "getHelp.hpp"
 
 void belyaev::insertEntryCmd(Dictionaries& data, std::istream& in, std::ostream& out)
 {
@@ -341,53 +343,7 @@ void belyaev::deleteIntersectionsCmd(Dictionaries& data, std::istream& in, std::
 
 void belyaev::helpCmd(std::ostream& out)
 {
-  out << "1. INSERT <dictionary> <russian_word> <translation>\n"
-      << "Добавляет запись с переводом в словарь. Если словаря \"dictionary\" не существует, то такой создается."
-      << "Если в словаре такая запись уже существует, то вывести сообщение <THIS ENTRY ALREADY EXISTS>\n"
-      << "2. REMOVE <dictionary> <russian_word> <translation>\n"
-      << "Удаляет из словаря запись с переводом слова. Если в словаре таких не существует, то вывести сообщение"
-      << "<THIS ENTRY DOES NOT EXIST>. Если словаря не существует, вывести <THIS DICTIONARY DOES NOT EXIST>.\n"
-      << "3. SEARCH <dictionary> <russian_word>\n"
-      << "Ищет в словаре записи с переводом слова и выводит их. Если поиск неудачен, то вывести сообщение"
-      << "<THIS ENTRY DOES NOT EXIST>.\n"
-      << "4. SEARCH_BY_ENGLISH <dictionary> <english_word>\n"
-      << "Ищет в словаре записи, где переводом является указанное слово, и выводит их. Если поиск неудачен,"
-      << "то вывести сообщение <THIS ENTRY DOES NOT EXIST>.\n"
-      << "5. SEARCH_СONTAINS <dictionary> <russian_characters>\n"
-      << "Ищет в словаре записи, где в русском слове содержится указанный порядок символов. Если поиск неудачен,"
-      << "то вывести сообщение <NO ENTRIES>.\n"
-      << "6. SEARCH_CONTAINS_ENGLISH <dictionary> <english_characters>\n"
-      << "Ищет в словаре записи, где в переводе содержится указанный порядок символов. Если поиск неудачен,"
-      << "то вывести сообщение <NO ENTRIES>.\n"
-      << "7. PRINT <dictionary>\n"
-      << "Выводит все записи из словаря. Если словаря не существует, то вывести сообщение"
-      << "<THIS DICTIONARY DOES NOT EXIST>.\n"
-      << "8. PRINT_ALL\n"
-      << "Выводит все записи в системе.\n"
-      << "9. CLEAR <dictionary>\n"
-      << "Очищает все записи в словаре, как следствие, удаляя его. Если словаря не существует, то вывести сообщение"
-      << "<THIS DICTIONARY DOES NOT EXIST>.\n"
-      << "10. CLEAR_ALL\n"
-      << "Очищает все записи в системе.\n"
-      << "11. COPY <dictionary_old> <dictionary_new>\n"
-      << "Копирует словарь \"dictionary_old\", присваивая новому имя \"dictionary_new\". Если словаря в первом поле"
-      << "не существует, то вывести сообщение <THIS DICTIONARY DOES NOT EXIST>.\n"
-      << "12. RENAME <dictionary_old> <dictionary_new>\n"
-      << "Переименовывает словарь \"dictionary_old\", присваивая новому имя \"dictionary_new\". Если словаря в первом"
-      << "поле не существует, то вывести сообщение <THIS DICTIONARY DOES NOT EXIST>.\n"
-      << "13. MERGE <N> <dictionary_new> <dictionary1> ... <dictionaryN>\n"
-      << "N - натуральное число больше 1. Команда совмещает все записи в словарях с 1 по N в новом словаре"
-      << "\"dictionary_new\". Если одного из словарей не существует, то вывести сообщение"
-      << "<ONE OF THE DICTIONARIES DOES NOT EXIST>.\n"
-      << "14. INTERSECT <N> <dictionary_new> <dictionary1> ... <dictionaryN>\n"
-      << "N - натуральное число больше 1. Сохраняет все пересечения словарей с 1 по N в новый словарь"
-      << "\"dictionary_new\". Если одного из словарей не существует, то вывести сообщение"
-      << "<ONE OF THE DICTIONARIES DOES NOT EXIST>.\n"
-      << "15. DELETE_INTERSECTIONS <N> <dictionary_new> <dictionary1> ... <dictionaryN>\n"
-      << "N - натуральное число больше 1. Удаляет все пересечения в первом словаре с остальными, с 2 по N."
-      << "Если одного из словарей не существует, то вывести сообщение <ONE OF THE DICTIONARIES DOES NOT EXIST>.\n"
-      << "16. HELP\n"
-      << "Вывод всего списка команд со справкой.\n";
+  belyaev::getHelp(out);
 }
 
 belyaev::commandMap belyaev::mapCommandHandlers(Dictionaries& data)
