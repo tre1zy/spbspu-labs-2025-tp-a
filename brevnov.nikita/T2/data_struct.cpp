225e78adb083d2963d203044019fb9c42d31d0aa
brev1ch <flirktv@gmail.com>
2025-06-25 18:29:36 +0300

    data_struct

--- brevnov.nikita/T2/data_struct.cpp
+++ brevnov.nikita/T2/data_struct.cpp
@@ -3,52 +3,9 @@
 #include <vector>
 #include <iomanip>
 #include <cmath>
+#include <delimeters.hpp>
+#include <streamGuard.hpp>
 
-brevnov::StreamGuard::StreamGuard(std::basic_ios< char > & s):
-  s_(s),
-  width_(s.width()),
-  fill_(s.fill()),
-  precision_(s.precision()),
-  fmt_(s.flags())
-{}
-
-brevnov::StreamGuard::~StreamGuard()
-{
-  s_.width(width_);
-  s_.fill(fill_);
-  s_.precision(precision_);
-  s_.flags(fmt_);
-}
-
-std::istream& brevnov::operator>>(std::istream& input, DelimiterIO&& dest)
-{
-  std::istream::sentry sentry(input);
-  if (!sentry)
-  {
-    return input;
-  }
-  char c = '0';
-  input >> c;
-  if (input && (std::tolower(c) != dest.exp))
-  {
-    input.setstate(std::ios::failbit);
-  }
-  return input;
-}
-
-std::istream& brevnov::operator>>(std::istream& input, DelimitersIO&& dest)
-{
-  std::istream::sentry sentry(input);
-  if (!sentry)
-  {
-    return input;
-  }
-  for (char c: dest.exp)
-  {
-    input >> DelimiterIO{ c };
-  }
-  return input;
-}
 
 std::istream& brevnov::operator>>(std::istream& input, UnLongLongIO&& dest)
 {
