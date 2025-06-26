08dd4cfd2c360167d621d699759d43b2f249de9b
Sergey Dribas <serdribas@gmail.com>
2025-06-26 10:48:31 +0300

    cg fix

--- dribas.sergey/F0/commands.cpp
+++ dribas.sergey/F0/commands.cpp
@@ -642,18 +642,18 @@ void dribas::delete_training_by_key(std::istream& in, std::ostream& out, suite& 
 
 void dribas::delete_training_suite(std::istream& in, std::ostream& out, suite& trainings)
 {
-    size_t training_id;
-    in >> training_id;
-    if (!in) {
-      throw std::invalid_argument("Invalid training ID");
-    }
-    if (training_id == 1) {
-      throw std::invalid_argument("Cannot delete the main training suite (ID 1)");
-    }
-    auto it = trainings.find(training_id);
-    size_t removed_count = it->second.size();
-    trainings.erase(it);
-    out << "Deleted training suite ID " << training_id;
-    out<< " containing " << removed_count << " workouts\n";
-    out << "Note: The workouts still exist in the main training suite (ID 1)\n";
+  size_t training_id;
+  in >> training_id;
+  if (!in) {
+    throw std::invalid_argument("Invalid training ID");
+  }
+  if (training_id == 1) {
+    throw std::invalid_argument("Cannot delete the main training suite (ID 1)");
+  }
+  auto it = trainings.find(training_id);
+  size_t removed_count = it->second.size();
+  trainings.erase(it);
+  out << "Deleted training suite ID " << training_id;
+  out<< " containing " << removed_count << " workouts\n";
+  out << "Note: The workouts still exist in the main training suite (ID 1)\n";
 }
