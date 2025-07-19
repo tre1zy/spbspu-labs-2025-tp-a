f4dbe8fc5a0891c1f8eddb6d5435dc49183b33fe
shycoshy <neforscist@gmail.com>
2025-07-19 18:43:24 +0000

    tests no.100

--- asafov.andrew/T2/inputDataStruct.cpp
+++ asafov.andrew/T2/inputDataStruct.cpp
@@ -1,168 +1,167 @@
 #include "datastruct.h"
-#include <cctype>
+#include <istream>
 #include <complex>
-#include <iterator>
+#include <cctype>
 #include <string>
 
-namespace
-{
+namespace {
   using It = std::istreambuf_iterator<char>;
 
-  void skipWhitespace(It& it, const It& end)
-  {
-    while (it != end && std::isspace(*it))
+  void skipWhitespace(It& it, const It& end) {
+    while (it != end && std::isspace(static_cast<unsigned char>(*it))) {
       ++it;
+    }
   }
 
-  bool matchPrefix(It& it, const It& end, const std::string& prefix)
-  {
-    It temp = it;
-    for (char ch : prefix)
-    {
-      if (temp == end || *temp != ch)
+  bool matchPrefix(It& it, const It& end, const std::string& prefix) {
+    auto tmp = it;
+    for (char ch : prefix) {
+      if (tmp == end || *tmp != ch) {
         return false;
-      ++temp;
+      }
+      ++tmp;
     }
-    it = temp;
+    it = tmp;
     return true;
   }
 
-  std::string parseIdentifier(It& it, const It& end)
-  {
-    std::string result;
-    while (it != end && std::isalnum(*it))
-    {
-      result += *it;
+  std::string parseIdentifier(It& it, const It& end) {
+    std::string id;
+    while (it != end && std::isalpha(static_cast<unsigned char>(*it))) {
+      id += *it;
       ++it;
     }
-    return result;
+    return id;
   }
 
-  unsigned long long parseBinary(It& it, const It& end)
-  {
-    if (!matchPrefix(it, end, "0b"))
-      throw std::ios_base::failure("Expected binary prefix");
+  unsigned long long parseBinary(It& it, const It& end, std::istream& is) {
+    if (!matchPrefix(it, end, "0b")) {
+      is.setstate(std::ios::failbit);
+      return 0;
+    }
 
-    unsigned long long result = 0;
+    unsigned long long value = 0;
     bool hasDigits = false;
 
-    while (it != end && (*it == '0' || *it == '1'))
-    {
-      result = (result << 1) | (*it - '0');
+    while (it != end && (*it == '0' || *it == '1')) {
+      value = (value << 1) | (*it - '0');
       ++it;
       hasDigits = true;
     }
 
-    if (!hasDigits)
-      throw std::ios_base::failure("Empty binary number");
+    if (!hasDigits) {
+      is.setstate(std::ios::failbit);
+    }
 
-    return result;
+    return value;
   }
 
-  std::complex<double> parseComplex(It& it, const It& end)
-  {
-    if (!matchPrefix(it, end, "#c("))
-      throw std::ios_base::failure("Expected #c(");
+  std::complex<double> parseComplex(It& it, const It& end, std::istream& is) {
+    if (!matchPrefix(it, end, "#c(")) {
+      is.setstate(std::ios::failbit);
+      return {0.0, 0.0};
+    }
 
-    std::string number;
-    skipWhitespace(it, end);
-    while (it != end && (std::isdigit(*it) || *it == '.' || *it == '-' || *it == '+'))
-    {
-      number += *it;
+    std::string token;
+    while (it != end && !std::isspace(static_cast<unsigned char>(*it)) && *it != ')') {
+      token += *it;
       ++it;
     }
 
-    double real = std::stod(number);
+    std::istringstream re_stream(token);
+    double real = 0.0;
+    re_stream >> real;
+    if (re_stream.fail()) {
+      is.setstate(std::ios::failbit);
+      return {0.0, 0.0};
+    }
+
     skipWhitespace(it, end);
 
-    number.clear();
-    while (it != end && (std::isdigit(*it) || *it == '.' || *it == '-' || *it == '+'))
-    {
-      number += *it;
+    token.clear();
+    while (it != end && *it != ')') {
+      token += *it;
       ++it;
     }
 
-    double imag = std::stod(number);
-    skipWhitespace(it, end);
+    double imag = 0.0;
+    if (!token.empty()) {
+      std::istringstream im_stream(token);
+      im_stream >> imag;
+      if (im_stream.fail()) {
+        is.setstate(std::ios::failbit);
+        return {0.0, 0.0};
+      }
+    }
 
-    if (it == end || *it != ')')
-      throw std::ios_base::failure("Expected ')' in complex");
+    if (it == end || *it != ')') {
+      is.setstate(std::ios::failbit);
+      return {0.0, 0.0};
+    }
+
     ++it;
-
     return {real, imag};
   }
 
-  std::string parseQuotedString(It& it, const It& end)
-  {
-    if (it == end || *it != '"')
-      throw std::ios_base::failure("Expected opening quote");
-
+  std::string parseQuotedString(It& it, const It& end, std::istream& is) {
+    if (it == end || *it != '"') {
+      is.setstate(std::ios::failbit);
+      return {};
+    }
     ++it;
-    std::string result;
-    while (it != end && *it != '"')
-    {
-      result += *it;
+
+    std::string str;
+    while (it != end && *it != '"') {
+      str += *it;
       ++it;
     }
 
-    if (it == end || *it != '"')
-      throw std::ios_base::failure("Unterminated string");
-    ++it;
+    if (it == end || *it != '"') {
+      is.setstate(std::ios::failbit);
+      return {};
+    }
 
-    return result;
+    ++it;
+    return str;
   }
 }
 
-std::istream& asafov::operator>>(std::istream& is, DataStruct& data)
-{
+std::istream& asafov::operator>>(std::istream& is, DataStruct& data) {
   std::istreambuf_iterator<char> it(is), end;
   DataStruct temp;
   bool has_key1 = false, has_key2 = false, has_key3 = false;
 
-  try
-  {
-    while (it != end)
-    {
-      skipWhitespace(it, end);
-      if (it == end || *it != ':')
-        break;
-      ++it;
+  while (it != end && !is.fail()) {
+    skipWhitespace(it, end);
+    if (it == end || *it != ':') break;
+    ++it;
 
-      std::string key = parseIdentifier(it, end);
-      skipWhitespace(it, end);
+    skipWhitespace(it, end);
+    std::string key = parseIdentifier(it, end);
 
-      if (key == "key1")
-      {
-        temp.key1 = parseBinary(it, end);
-        has_key1 = true;
-      }
-      else if (key == "key2")
-      {
-        temp.key2 = parseComplex(it, end);
-        has_key2 = true;
-      }
-      else if (key == "key3")
-      {
-        temp.key3 = parseQuotedString(it, end);
-        has_key3 = true;
-      }
-      else
-      {
-        throw std::ios_base::failure("Unknown key");
-      }
+    skipWhitespace(it, end);
 
-      if (has_key1 && has_key2 && has_key3)
-      {
-        data = temp;
-        return is;
-      }
+    if (key == "key1") {
+      temp.key1 = parseBinary(it, end, is);
+      has_key1 = !is.fail();
+    } else if (key == "key2") {
+      temp.key2 = parseComplex(it, end, is);
+      has_key2 = !is.fail();
+    } else if (key == "key3") {
+      temp.key3 = parseQuotedString(it, end, is);
+      has_key3 = !is.fail();
+    } else {
+      is.setstate(std::ios::failbit);
     }
 
-    throw std::ios_base::failure("Missing required keys");
+    skipWhitespace(it, end);
   }
-  catch (...)
-  {
+
+  if (has_key1 && has_key2 && has_key3) {
+    data = temp;
+  } else {
     is.setstate(std::ios::failbit);
-    return is;
   }
+
+  return is;
 }
