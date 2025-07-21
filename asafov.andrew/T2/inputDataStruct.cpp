6379221d98ef5ce74e51bd7b673467b15c72e703
shycoshy <neforscist@gmail.com>
2025-07-21 16:36:13 +0000

    tests no.118

--- asafov.andrew/T2/inputDataStruct.cpp
+++ asafov.andrew/T2/inputDataStruct.cpp
@@ -2,79 +2,111 @@
 #include <iomanip>
 #include <string>
 
-namespace asafov
+std::istream& asafov::operator>>(std::istream& in, asafov::DataStruct& data)
 {
-  std::istream& operator>>(std::istream& in, DataStruct& data)
-  {
-    char ch;
-    in >> std::ws >> ch;
-    if (ch != '(') {
-      in.setstate(std::ios::failbit);
-      return in;
-    }
-
-    bool hasKey1 = false, hasKey2 = false, hasKey3 = false;
-
-    while (in >> std::ws >> ch && ch == ':') {
-      std::string key;
-      in >> key;
-
-      if (key == "key1") {
-        std::string val;
-        in >> val;
-        if (val.size() >= 3 && (val.substr(0, 2) == "0b" || val.substr(0, 2) == "0B")) {
-          try {
-            data.key1 = std::stoull(val.substr(2), nullptr, 2);
-            hasKey1 = true;
-          } catch (...) {
-            in.setstate(std::ios::failbit);
-            return in;
-          }
-        } else {
-          in.setstate(std::ios::failbit);
-          return in;
-        }
-      }
-      else if (key == "key2") {
-        char hash, c, open, close;
-        double re, im;
-        in >> std::ws >> hash >> c >> open;
-        if (hash == '#' && c == 'c' && open == '(') {
-          in >> re >> im >> close;
-          if (close == ')') {
-            data.key2 = std::complex<double>(re, im);
-            hasKey2 = true;
-          } else {
-            in.setstate(std::ios::failbit);
-            return in;
-          }
-        } else {
-          in.setstate(std::ios::failbit);
-          return in;
-        }
-      }
-      else if (key == "key3") {
-        in >> std::quoted(data.key3);
-        hasKey3 = true;
-      }
-      else {
-        std::string skip;
-        in >> skip;
-      }
-
-      in >> std::ws >> ch;
-      if (ch != ':') break;
-    }
-
-    if (ch != ')') {
-      in.setstate(std::ios::failbit);
-      return in;
-    }
-
-    if (!(hasKey1 && hasKey2 && hasKey3)) {
-      in.setstate(std::ios::failbit);
-    }
-
+  char ch = 0;
+  in >> std::ws >> ch;
+  if (!in || ch != '(') {
+    in.setstate(std::ios::failbit);
     return in;
   }
+
+  bool hasKey1 = false, hasKey2 = false, hasKey3 = false;
+
+  while (true)
+  {
+    in >> std::ws >> ch;
+    if (!in || ch != ':') {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+
+    std::string key;
+    in >> key;
+    if (!in) {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+
+    if (key == "key1")
+    {
+      std::string literal;
+      in >> literal;
+      if (!in) {
+        in.setstate(std::ios::failbit);
+        return in;
+      }
+      if (literal.substr(0, 2) == "0b" || literal.substr(0, 2) == "0B") {
+        try {
+          data.key1 = std::stoull(literal.substr(2), nullptr, 2);
+          hasKey1 = true;
+        } catch (...) {
+          in.setstate(std::ios::failbit);
+          return in;
+        }
+      } else {
+        in.setstate(std::ios::failbit);
+        return in;
+      }
+    }
+    else if (key == "key2")
+    {
+      char hash = 0, c = 0, open = 0;
+      in >> std::ws >> hash >> c >> open;
+      if (!in || hash != '#' || c != 'c' || open != '(') {
+        in.setstate(std::ios::failbit);
+        return in;
+      }
+
+      double re = 0.0, im = 0.0;
+      char close = 0;
+      in >> re >> im >> close;
+      if (!in || close != ')') {
+        in.setstate(std::ios::failbit);
+        return in;
+      }
+
+      data.key2 = std::complex<double>{re, im};
+      hasKey2 = true;
+    }
+    else if (key == "key3")
+    {
+      std::string quoted;
+      in >> std::quoted(quoted);
+      if (!in) {
+        in.setstate(std::ios::failbit);
+        return in;
+      }
+
+      data.key3 = quoted;
+      hasKey3 = true;
+    }
+    else
+    {
+      std::string dummy;
+      in >> dummy;
+      if (!in) {
+        in.setstate(std::ios::failbit);
+        return in;
+      }
+    }
+
+    in >> std::ws >> ch;
+    if (!in) {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+
+    if (ch == ')') break;
+    if (ch != ':') {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+  }
+
+  if (!(hasKey1 && hasKey2 && hasKey3)) {
+    in.setstate(std::ios::failbit);
+  }
+
+  return in;
 }
