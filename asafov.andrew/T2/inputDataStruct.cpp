a0fe551e0430a4ba87e85013a8fcdae13c5cd9f8
shycoshy <neforscist@gmail.com>
2025-07-24 01:18:42 +0000

    tests no.131

--- asafov.andrew/T2/inputDataStruct.cpp
+++ asafov.andrew/T2/inputDataStruct.cpp
@@ -1,387 +1,147 @@
 #include "datastruct.h"
 #include <iomanip>
 #include <string>
+#include <stdexcept>
+
+namespace
+{
+  void unsafe_read(std::istream& in, asafov::DataStruct& data)
+  {
+    char ch;
+
+    if (!(in >> ch) || ch != '(') throw std::logic_error("err");
+    if (!(in >> ch) || ch != ':') throw std::logic_error("err");
+    if (!(in >> ch) || ch != 'k') throw std::logic_error("err");
+    if (!(in >> ch) || ch != 'e') throw std::logic_error("err");
+    if (!(in >> ch) || ch != 'y') throw std::logic_error("err");
+
+    if (!(in >> ch) || ch == '1')
+    {
+      if (!(in >> ch) || ch != ' ') throw std::logic_error("err");
+      if (!(in >> ch) || ch != '0') throw std::logic_error("err");
+      if (!(in >> ch) || ch != 'b') throw std::logic_error("err");
+
+      std::string t2;
+      while (ch != ':') !(in >> ch) ? t2 += ch; in >> ch : throw std::logic_error("err");
+      data.key1 = std::stoull(t2);
+    }
+    else if (ch == '2')
+    {
+      double t3, t4;
+      if (!(in >> ch) || ch != ' ') throw std::logic_error("err");
+      if (!(in >> ch) || ch != '#') throw std::logic_error("err");
+      if (!(in >> ch) || ch != 'c') throw std::logic_error("err");
+      if (!(in >> ch) || ch != '(') throw std::logic_error("err");
+      in >> t3;
+      if (!(in >> ch) || ch != ' ') throw std::logic_error("err");
+      in >> t4;
+      if (!(in >> ch) || ch != ')') throw std::logic_error("err");
+      temp.key2 = std::complex<double>(t3, t4);
+      if (!(in >> ch) || ch != ':') throw std::logic_error("err");
+    }
+    else if (ch == '3')
+    {
+      if (!(in >> ch) || ch != ' ') throw std::logic_error("err");
+      if (!(in >> ch) || ch != '"') throw std::logic_error("err");
+
+      std::string t2;
+      while (ch != ':') !(in >> ch) ? t2 += ch; in >> ch : throw std::logic_error("err");
+      temp.key3 = t2;
+    }
+    else throw std::logic_error("err");
+    if (!(in >> ch) || ch != 'k') throw std::logic_error("err");
+    if (!(in >> ch) || ch != 'e') throw std::logic_error("err");
+    if (!(in >> ch) || ch != 'y') throw std::logic_error("err");
+
+    if (!(in >> ch) || ch == '1')
+    {
+      if (!(in >> ch) || ch != ' ') throw std::logic_error("err");
+      if (!(in >> ch) || ch != '0') throw std::logic_error("err");
+      if (!(in >> ch) || ch != 'b') throw std::logic_error("err");
+
+      std::string t2;
+      while (ch != ':') !(in >> ch) ? t2 += ch; in >> ch : throw std::logic_error("err");
+      data.key1 = std::stoull(t2);
+    }
+    else if (ch == '2')
+    {
+      double t3, t4;
+      if (!(in >> ch) || ch != ' ') throw std::logic_error("err");
+      if (!(in >> ch) || ch != '#') throw std::logic_error("err");
+      if (!(in >> ch) || ch != 'c') throw std::logic_error("err");
+      if (!(in >> ch) || ch != '(') throw std::logic_error("err");
+      in >> t3;
+      if (!(in >> ch) || ch != ' ') throw std::logic_error("err");
+      in >> t4;
+      if (!(in >> ch) || ch != ')') throw std::logic_error("err");
+      temp.key2 = std::complex<double>(t3, t4);
+      if (!(in >> ch) || ch != ':') throw std::logic_error("err");
+    }
+    else if (ch == '3')
+    {
+      if (!(in >> ch) || ch != ' ') throw std::logic_error("err");
+      if (!(in >> ch) || ch != '"') throw std::logic_error("err");
+
+      std::string t2;
+      while (ch != ':') !(in >> ch) ? t2 += ch; in >> ch : throw std::logic_error("err");
+      temp.key3 = t2;
+    }
+    else throw std::logic_error("err");
+    if (!(in >> ch) || ch != 'k') throw std::logic_error("err");
+    if (!(in >> ch) || ch != 'e') throw std::logic_error("err");
+    if (!(in >> ch) || ch != 'y') throw std::logic_error("err");
+
+    if (!(in >> ch) || ch == '1')
+    {
+      if (!(in >> ch) || ch != ' ') throw std::logic_error("err");
+      if (!(in >> ch) || ch != '0') throw std::logic_error("err");
+      if (!(in >> ch) || ch != 'b') throw std::logic_error("err");
+
+      std::string t2;
+      while (ch != ':') !(in >> ch) ? t2 += ch; in >> ch : throw std::logic_error("err");
+      data.key1 = std::stoull(t2);
+    }
+    else if (ch == '2')
+    {
+      double t3, t4;
+      if (!(in >> ch) || ch != ' ') throw std::logic_error("err");
+      if (!(in >> ch) || ch != '#') throw std::logic_error("err");
+      if (!(in >> ch) || ch != 'c') throw std::logic_error("err");
+      if (!(in >> ch) || ch != '(') throw std::logic_error("err");
+      in >> t3;
+      if (!(in >> ch) || ch != ' ') throw std::logic_error("err");
+      in >> t4;
+      if (!(in >> ch) || ch != ')') throw std::logic_error("err");
+      temp.key2 = std::complex<double>(t3, t4);
+      if (!(in >> ch) || ch != ':') throw std::logic_error("err");
+    }
+    else if (ch == '3')
+    {
+      if (!(in >> ch) || ch != ' ') throw std::logic_error("err");
+      if (!(in >> ch) || ch != '"') throw std::logic_error("err");
+
+      std::string t2;
+      while (ch != ':') !(in >> ch) ? t2 += ch; in >> ch : throw std::logic_error("err");
+      temp.key3 = t2;
+    }
+    else throw std::logic_error("err");
+    if (!(in >> ch) || ch != ')') throw std::logic_error("err");
+  }
+}
 
 std::istream& asafov::operator>>(std::istream& in, asafov::DataStruct& data)
 {
   DataStruct temp;
   in >> std::noskipws;
-  char ch;
-  in >> ch;
-  if (!(in >> ch) || ch != '(') {
-  in.setstate(std::ios::failbit); std::string line; getline(in, line);
-  return in;
-  }
-  in >> ch;
-  if (ch != ':')
+  try
   {
-    in.setstate(std::ios::failbit); std::string line; getline(in, line);
-    return in;
+    unsafe_read(in, temp);
   }
-  in >> ch;
-  if (ch != 'k')
+  catch (...)
   {
-    in.setstate(std::ios::failbit); std::string line; getline(in, line);
-    return in;
-  }
-  in >> ch;
-  if (ch != 'e')
-  {
-    in.setstate(std::ios::failbit); std::string line; getline(in, line);
-    return in;
-  }
-  in >> ch;
-  if (ch != 'y')
-  {
-    in.setstate(std::ios::failbit); std::string line; getline(in, line);
-    return in;
-  }
-  in >> ch;
-  if (ch == '1')
-  {
-    in >> ch;
-    if (ch != ' ')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> ch;
-    if (ch != '0')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> ch;
-    if (ch != 'b')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    std::string t2;
-    while (ch != ':')
-    {
-      if (ch != '0' || ch != '1'){
-        in.setstate(std::ios::failbit); std::string line; getline(in, line);
-        return in;
-      }
-      t2 += ch; in >> ch;
-    }
-    temp.key1 = std::stoull(t2);
-  }
-  else if (ch == '2')
-  {
-    double t3, t4;
-    in >> ch;
-    if (ch != ' ')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> ch;
-    if (ch != '#')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> ch;
-    if (ch != 'c')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> ch;
-    if (ch != '(')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> t3;
-    in >> ch;
-    if (ch != ' ')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> t4;
-    in >> ch;
-    if (ch != ')')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    temp.key2 = std::complex<double>(t3, t4);
-  }
-  else if (ch == '3')
-  {
-    in >> ch;
-    if (ch != ' ')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> ch;
-    if (ch != '"')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    std::string t2;
-    while (ch != ':')
-    {
-      t2 += ch; in >> ch;
-    }
-    t2.pop_back();
-    temp.key3 = t2;
-  }
-  else
-  {
-    in.setstate(std::ios::failbit); std::string line; getline(in, line);
-    return in;
-  }
-  in >> ch;
-  if (ch != 'k')
-  {
-    in.setstate(std::ios::failbit); std::string line; getline(in, line);
-    return in;
-  }
-  in >> ch;
-  if (ch != 'e')
-  {
-    in.setstate(std::ios::failbit); std::string line; getline(in, line);
-    return in;
-  }
-  in >> ch;
-  if (ch != 'y')
-  {
-    in.setstate(std::ios::failbit); std::string line; getline(in, line);
-    return in;
-  }
-  in >> ch;
-  if (ch == '1')
-  {
-    in >> ch;
-    if (ch != ' ')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> ch;
-    if (ch != '0')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> ch;
-    if (ch != 'b')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    std::string t2;
-    while (ch != ':')
-    {
-      if (ch != '0' || ch != '1'){
-        in.setstate(std::ios::failbit); std::string line; getline(in, line);
-        return in;
-      }
-      t2 += ch; in >> ch;
-    }
-    temp.key1 = std::stoull(t2);
-  }
-else if (ch == '2')
-  {
-    double t3, t4;
-    in >> ch;
-    if (ch != ' ')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> ch;
-    if (ch != '#')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> ch;
-    if (ch != 'c')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> ch;
-    if (ch != '(')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> t3;
-    in >> ch;
-    if (ch != ' ')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> t4;
-    in >> ch;
-    if (ch != ')')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    temp.key2 = std::complex<double>(t3, t4);
-  }
-  else if (ch == '3')
-  {
-    in >> ch;
-    if (ch != ' ')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> ch;
-    if (ch != '"')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    std::string t2;
-    while (ch != ':')
-    {
-      t2 += ch; in >> ch;
-    }
-    t2.pop_back();
-    temp.key3 = t2;
-  }
-  else
-  {
-    in.setstate(std::ios::failbit); std::string line; getline(in, line);
-    return in;
-  }
-  in >> ch;
-  if (ch != 'k')
-  {
-    in.setstate(std::ios::failbit); std::string line; getline(in, line);
-    return in;
-  }
-  in >> ch;
-  if (ch != 'e')
-  {
-    in.setstate(std::ios::failbit); std::string line; getline(in, line);
-    return in;
-  }
-  in >> ch;
-  if (ch != 'y')
-  {
-    in.setstate(std::ios::failbit); std::string line; getline(in, line);
-    return in;
-  }
-  in >> ch;
-  if (ch == '1')
-  {
-    in >> ch;
-    if (ch != ' ')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> ch;
-    if (ch != '0')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> ch;
-    if (ch != 'b')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    std::string t2;
-    while (ch != ':')
-    {
-      if (ch != '0' && ch != '1'){
-        in.setstate(std::ios::failbit); std::string line; getline(in, line);
-        return in;
-      }
-      t2 += ch; in >> ch;
-    }
-    temp.key1 = std::stoull(t2);
-  }
-  else if (ch == '2')
-  {
-    double t3, t4;
-    in >> ch;
-    if (ch != ' ')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> ch;
-    if (ch != '#')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> ch;
-    if (ch != 'c')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> ch;
-    if (ch != '(')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> t3;
-    in >> ch;
-    if (ch != ' ')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> t4;
-    in >> ch;
-    if (ch != ')')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    temp.key2 = std::complex<double>(t3, t4);
-  }
-  else if (ch == '3')
-  {
-    in >> ch;
-    if (ch != ' ')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    in >> ch;
-    if (ch != '"')
-    {
-      in.setstate(std::ios::failbit); std::string line; getline(in, line);
-      return in;
-    }
-    std::string t2;
-    while (ch != ':')
-    {
-      t2 += ch; in >> ch;
-    }
-    t2.pop_back();
-    temp.key3 = t2;
-  }
-  else
-  {
-    in.setstate(std::ios::failbit); std::string line; getline(in, line);
-    return in;
-  }
-  in >> ch;
-  if (ch != ')')
-  {
-    in.setstate(std::ios::failbit); std::string line; getline(in, line);
+    in.setstate(std::ios::failbit);
+    std::string line;
+    getline(in, line);
     return in;
   }
   data = temp;
