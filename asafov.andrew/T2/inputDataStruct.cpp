a72e7d46efdeba948e94fdca7e9428a28eb29ac4
shycoshy <neforscist@gmail.com>
2025-07-24 01:31:08 +0000

    tests no.135

--- asafov.andrew/T2/inputDataStruct.cpp
+++ asafov.andrew/T2/inputDataStruct.cpp
@@ -23,7 +23,12 @@ namespace
       if (!(in >> ch) || ch != 'b') throw std::logic_error("err");
 
       std::string t2;
-      while (ch != ':') !(in >> ch) ? t2 += ch; in >> ch : throw std::logic_error("err");
+      while (true)
+      {
+        if (!(in >> ch)) throw std::logic_error("err");
+        if (ch == ':') break;
+        t2 += ch;
+      }
       data.key1 = std::stoull(t2);
     }
     else if (ch == '2')
@@ -46,7 +51,12 @@ namespace
       if (!(in >> ch) || ch != '"') throw std::logic_error("err");
 
       std::string t2;
-      while (ch != ':') !(in >> ch) ? t2 += ch; in >> ch : throw std::logic_error("err");
+      while (true)
+      {
+        if (!(in >> ch)) throw std::logic_error("err");
+        if (ch == ':') break;
+        t2 += ch;
+      }
       data.key3 = t2;
     }
     else throw std::logic_error("err");
@@ -111,7 +121,12 @@ namespace
       if (!(in >> ch) || ch != 'b') throw std::logic_error("err");
 
       std::string t2;
-      while (ch != ':') !(in >> ch) ? t2 += ch; in >> ch : throw std::logic_error("err");
+      while (true)
+      {
+        if (!(in >> ch)) throw std::logic_error("err");
+        if (ch == ':') break;
+        t2 += ch;
+      }
       data.key1 = std::stoull(t2);
     }
     else if (ch == '2')
