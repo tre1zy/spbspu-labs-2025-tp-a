ef59f52dd5cd5121e6e4970cc50f8992714c96ac
7heli7 <arseniy.shchadilov3@gmail.com>
2025-09-03 03:28:43 +0300

    Stream guard and main.

---
+++ shchadilov.arseniy/T2/streamGuard.hpp
@@ -0,0 +1,26 @@
+#ifndef SCOPE_GUARD_HPP
+#define SCOPE_GUARD_HPP
+
+#include <iostream>
+
+namespace shchadilov
+{
+  class StreamGuard
+  {
+  public:
+    explicit StreamGuard(std::basic_ios< char >& s);
+    ~StreamGuard();
+    StreamGuard(const StreamGuard&) = delete;
+    StreamGuard(StreamGuard&&) = delete;
+    StreamGuard& operator=(const StreamGuard&) = delete;
+    StreamGuard& operator=(StreamGuard&&) = delete;
+  private:
+    std::streamsize precision_;
+    std::streamsize width_;
+    std::basic_ios< char >& s_;
+    std::basic_ios< char >::fmtflags fmt_;
+    char fill_;
+  };
+}
+
+#endif
