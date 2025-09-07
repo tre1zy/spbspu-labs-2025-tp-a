93c771e116bf139e2328f6c06ed97a28c585a242
uchuaip <d.smirnov2006@mail.ru>
2025-09-07 23:16:20 +0300

    add Binkey, DblLit, STRKey

--- smirnov.dmitriy/T2/KeyI.hpp
+++ smirnov.dmitriy/T2/KeyI.hpp
@@ -4,3 +4,29 @@
 #include <istream>
 #include <string>
 #include <utility>
+
+namespace smirnov
+{
+    struct BinKey
+    {
+        unsigned long long& value;
+    };
+
+    std::istream& operator>>(std::istream& in, BinKey&& key);
+
+    struct DblLit
+    {
+        double& key1;
+    };
+
+    std::istream& operator>>(std::istream& in, DblLit&& data);
+
+    struct STRKey
+    {
+        std::string& str;
+    };
+
+    std::istream& operator>>(std::istream& in, STRKey&& key);
+}
+
+#endif
