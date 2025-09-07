54356d736205d1f75b7fd4f782ba63df5244c7ed
uchuaip <d.smirnov2006@mail.ru>
2025-09-07 23:06:40 +0300

    add 1st op

--- smirnov.dmitriy/T2/KeyI.cpp
+++ smirnov.dmitriy/T2/KeyI.cpp
@@ -2,3 +2,20 @@
 #include <istream>
 #include <bitset>
 #include "DelimiterI.hpp"
+
+std::istream& smirnov::operator>>(std::istream& in, BinKey&& key)
+{
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+        in.setstate(std::ios::failbit);
+    }
+    std::bitset< 64 > bin;
+    in >> strictDel{ '0' } >> anyDel{ 'b' };
+    in >> bin;
+    if (in)
+    {
+        key.value = bin.to_ullong();
+    }
+    return in;
+}
