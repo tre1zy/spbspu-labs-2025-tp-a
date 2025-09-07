2ab33d2279ebc1e420472ad48eca14ff59f06be6
uchuaip <d.smirnov2006@mail.ru>
2025-09-08 01:10:35 +0300

    oper

--- smirnov.dmitriy/T2/DataStruct.cpp
+++ smirnov.dmitriy/T2/DataStruct.cpp
@@ -10,3 +10,18 @@ std::string getBinNumber(unsigned long long value)
   std::string binString = bin.to_string();
   return "0" + binString.erase(0, binString.find('1'));
 }
+
+bool smirnov::DataStruct::operator<(const DataStruct& other) const
+{
+  if (key1 != other.key1)
+  {
+    return key1 < other.key1;
+  }
+
+  else if (key2 != other.key2)
+  {
+    return key2 < other.key2;
+  }
+
+  return (key3.length() <= other.key3.length());
+}
