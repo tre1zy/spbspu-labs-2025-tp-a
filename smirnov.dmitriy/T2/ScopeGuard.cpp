dfcfeca62f184624d83048a9cd195611f0667ad0
uchuaip <d.smirnov2006@mail.ru>
2025-09-08 01:18:29 +0300

    add ScopeGuard.cpp

---
+++ smirnov.dmitriy/T2/ScopeGuard.cpp
@@ -0,0 +1,14 @@
+#include "ScopeGuard.hpp"
+
+smirnov::ScopeGuard::ScopeGuard(std::basic_ios< char >& s):
+  s_(s),
+  precision_(s.precision()),
+  flags_(s.flags())
+{}
+
+smirnov::ScopeGuard::~ScopeGuard()
+{
+  s_.precision(precision_);
+  s_.flags(flags_);
+}
+
