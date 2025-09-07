749e6ba5b22c7f9013b2afdeca44aa6e4b8af948
uchuaip <d.smirnov2006@mail.ru>
2025-09-08 01:09:57 +0300

    getBinNumber

--- smirnov.dmitriy/T2/DataStruct.cpp
+++ smirnov.dmitriy/T2/DataStruct.cpp
@@ -3,3 +3,10 @@
 #include <algorithm>
 #include "Delimiter.hpp"
 #include "ScopeGuard.hpp"
+
+std::string getBinNumber(unsigned long long value)
+{
+  std::bitset< 64 > bin(value);
+  std::string binString = bin.to_string();
+  return "0" + binString.erase(0, binString.find('1'));
+}
