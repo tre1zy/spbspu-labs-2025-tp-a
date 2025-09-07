a69e46046af3b5ab19820c18950451d3cd699ce8
uchuaip <d.smirnov2006@mail.ru>
2025-09-07 23:24:51 +0300

    add Delimiter

--- smirnov.dmitriy/T2/DelimiterI.hpp
+++ smirnov.dmitriy/T2/DelimiterI.hpp
@@ -2,3 +2,15 @@
 #define DELIMITER_I_HPP
 
 #include <istream>
+
+namespace smirnov
+{
+    struct Delimiter
+    {
+        const char* expected;
+    };
+
+    std::istream& operator>>(std::istream&, Delimiter&&);
+}
+
+#endif
