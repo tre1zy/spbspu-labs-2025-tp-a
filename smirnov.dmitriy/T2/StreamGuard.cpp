dae50189049d666f49c27fa39dd1f242889cbcb6
uchuaip <d.smirnov2006@mail.ru>
2025-09-07 23:30:37 +0300

    StreamGuard

--- smirnov.dmitriy/T2/StreamGuard.cpp
+++ smirnov.dmitriy/T2/StreamGuard.cpp
@@ -1,1 +1,13 @@
 #include "StreamGuard.hpp"
+
+smirnov::StreamGuard::StreamGuard(std::basic_ios< char > & ios):
+  ios_(ios),
+  precision_(ios.precision()),
+  flags_(ios.flags())
+{}
+
+smirnov::StreamGuard::~StreamGuard()
+{
+    ios_.precision(precision_);
+    ios_.flags(flags_);
+}
