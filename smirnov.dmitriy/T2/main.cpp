72fc06bd03ebdf4d5835ce9c3c2d18f681853908
uchuaip <d.smirnov2006@mail.ru>
2025-09-09 20:28:58 +0300

    std::cin

--- smirnov.dmitriy/T2/main.cpp
+++ smirnov.dmitriy/T2/main.cpp
@@ -2,25 +2,25 @@
 #include <iterator>
 #include <limits>
 #include "DataStruct.hpp"
-
+ 
 int main()
 {
   using namespace smirnov;
-  using inputIt = std::istream_iterator< DataStruct >;
-  using outputIt = std::ostream_iterator< DataStruct >;
+  using InputIt = std::istream_iterator< DataStruct >;
+  using OutputIt = std::ostream_iterator< DataStruct >;
 
   std::list< DataStruct > data{};
 
   while (!std::cin.eof())
   {
-    if (!std::cin)
+    if (std::cin.fail())
     {
       std::cin.clear();
       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
-    std::copy(inputIt{ std::cin }, inputIt{}, std::back_inserter(data));
+    std::copy(InputIt{ std::cin }, InputIt{}, std::back_inserter(data));
   }
   data.sort();
-  std::copy(data.cbegin(), data.cend(), outputIt{ std::cout, "\n" });
+  std::copy(data.cbegin(), data.cend(), OutputIt{ std::cout, "\n" });
   return 0;
 }
