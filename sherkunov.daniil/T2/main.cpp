1500424fdefe78985ab938f08290c4577208512d
kopinik <bk65763980@mail.ru>
2025-09-11 22:04:36 +0300

    i want good test =(

--- sherkunov.daniil/T2/main.cpp
+++ sherkunov.daniil/T2/main.cpp
@@ -14,6 +14,12 @@ int main()
 
   std::copy(InputStreamIterator(std::cin), InputStreamIterator{}, std::back_inserter(data));
 
+  while (std::cin.fail() && !std::cin.eof()) {
+    std::cin.clear(std::cin.rdstate() & ~std::ios::failbit);
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::copy(InputStreamIterator(std::cin), InputStreamIterator{}, std::back_inserter(data));
+  }
+
   if (std::cin.bad()) {
     std::cerr << "Error: Critical input failure\n";
     return 1;
