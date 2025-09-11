55a7d1b3c10717368edc5f2be204d557deabe097
kopinik <bk65763980@mail.ru>
2025-09-11 21:47:00 +0300

    fix symbol

--- sherkunov.daniil/T2/data_struct.cpp
+++ sherkunov.daniil/T2/data_struct.cpp
@@ -200,7 +200,7 @@ namespace sherkunov {
     long long key1_copy = value.key1;
     std::pair<long long, unsigned long long> key2_copy = value.key2;
     std::string key3_copy = value.key3;
-ч
+
     output << "(:key1 " << io_helpers::LongLongIO{value.key1};
     output << ":key2 (:" << io_helpers::PairIO{value.key2} << ":)";
     output << ":key3 " << io_helpers::StringIO{value.key3} << ":)";
