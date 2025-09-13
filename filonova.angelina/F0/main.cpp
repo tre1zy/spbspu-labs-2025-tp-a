c93b4158cf168088114a8ec0202e8dd70d60782f
filonova.angelina <filonova2906@yandex.ru>
2025-09-13 17:45:41 +0300

    build please2

--- filonova.angelina/F0/main.cpp
+++ filonova.angelina/F0/main.cpp
@@ -10,20 +10,20 @@ int main()
   using namespace std::placeholders;
 
   std::map< std::string, std::function< void(std::istream &, std::ostream &) > > commands;
-  commands["CREATE"] = std::bind(filonova::createDictionary, std::ref(dictionaries), _1, _2);
-  commands["DELETE"] = std::bind(filonova::deleteDictionary, std::ref(dictionaries), _1, _2);
-  commands["INSERT"] = std::bind(filonova::insert, std::ref(dictionaries), _1, _2);
-  commands["REMOVE"] = std::bind(filonova::remove, std::ref(dictionaries), _1, _2);
-  commands["CLEAR"] = std::bind(filonova::clear, std::ref(dictionaries), _1, _2);
+  commands["CREATE"] = std::bind(filonova::createDictionary, std::ref(dictionaries), _1);
+  commands["DELETE"] = std::bind(filonova::deleteDictionary, std::ref(dictionaries), _1);
+  commands["INSERT"] = std::bind(filonova::insert, std::ref(dictionaries), _1);
+  commands["REMOVE"] = std::bind(filonova::remove, std::ref(dictionaries), _1);
+  commands["CLEAR"] = std::bind(filonova::clear, std::ref(dictionaries), _1);
   commands["CONTAINS"] = std::bind(filonova::contains, std::ref(dictionaries), _1, _2);
-  commands["MERGE"] = std::bind(filonova::merge, std::ref(dictionaries), _1, _2);
+  commands["MERGE"] = std::bind(filonova::merge, std::ref(dictionaries), _1);
   commands["PRINT"] = std::bind(filonova::print, std::ref(dictionaries), _1, _2);
   commands["COUNT"] = std::bind(filonova::count, std::ref(dictionaries), _1, _2);
   commands["TOP"] = std::bind(filonova::top, std::ref(dictionaries), _1, _2);
   commands["UNIQUE"] = std::bind(filonova::unique, std::ref(dictionaries), _1, _2);
   commands["MOSTRARE"] = std::bind(filonova::mostrare, std::ref(dictionaries), _1, _2);
-  commands["INTERSECT"] = std::bind(filonova::intersectDictionary, std::ref(dictionaries), _1, _2);
-  commands["EXCLUDE"] = std::bind(filonova::excludeDictionary, std::ref(dictionaries), _1, _2);
+  commands["INTERSECT"] = std::bind(filonova::intersectDictionary, std::ref(dictionaries), _1);
+  commands["EXCLUDE"] = std::bind(filonova::excludeDictionary, std::ref(dictionaries), _1);
 
   std::string commandKey;
   while (std::cin >> commandKey)
