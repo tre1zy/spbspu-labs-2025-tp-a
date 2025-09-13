2cb7a0e7579c21e8bbbb7392506003822e4286d7
ysffmn <andrewnikonov387@gmail.com>
2025-09-13 14:11:19 +0000

    CG fix

--- nikonov.andrew/T2/DataStruct.cpp
+++ nikonov.andrew/T2/DataStruct.cpp
@@ -2,90 +2,87 @@
 #include <iomanip>
 #include "StreamGuard.hpp"
 #include "IOUtilities.hpp"
-namespace nikonov
+std::istream& nikonov::operator>>(std::istream& in, DataStruct& dest)
 {
-  std::istream& operator>>(std::istream& in, DataStruct& dest)
+  std::istream::sentry sentry(in);
+  if (!sentry)
   {
-    std::istream::sentry sentry(in);
-    if (!sentry)
+    return in;
+  }
+  DataStruct input;
+  {
+    using sep = detail::DelimiterIO;
+    using label = detail::LabelI;
+    using dbl = detail::DoubleI;
+    using ull = detail::UnsignedLongLongI;
+    using str = detail::StringI;
+    bool hasKey1 = false;
+    bool hasKey2 = false;
+    bool hasKey3 = false;
+    in >> sep{ '(' } >> sep{ ':' };
+    for (size_t i = 0; i < 3; ++i)
     {
-      return in;
-    }
-    DataStruct input;
-    {
-      using sep = detail::DelimiterIO;
-      using label = detail::LabelI;
-      using dbl = detail::DoubleI;
-      using ull = detail::UnsignedLongLongI;
-      using str = detail::StringI;
-      bool hasKey1 = false;
-      bool hasKey2 = false;
-      bool hasKey3 = false;
-      in >> sep{ '(' } >> sep{ ':' };
-      for (size_t i = 0; i < 3; ++i)
+      std::string keyName;
+      in >> label{ keyName };
+      if (keyName == "key1")
       {
-        std::string keyName;
-        in >> label{ keyName };
-        if (keyName == "key1")
-        {
-          if (hasKey1)
-          {
-            in.setstate(std::ios::failbit);
-            break;
-          }
-          in >> dbl{ input.key1 } >> sep{ ':' };
-          hasKey1 = true;
-        }
-        else if (keyName == "key2")
-        {
-          if (hasKey2)
-          {
-            in.setstate(std::ios::failbit);
-            break;
-          }
-          in >> ull{ input.key2 } >> sep{ ':' };
-          hasKey2 = true;
-        }
-        else if (keyName == "key3")
-        {
-          if (hasKey3)
-          {
-            in.setstate(std::ios::failbit);
-            break;
-          }
-          in >> str{ input.key3 } >> sep{ ':' };
-          hasKey3 = true;
-        }
-        else
+        if (hasKey1)
         {
           in.setstate(std::ios::failbit);
+          break;
         }
+        in >> dbl{ input.key1 } >> sep{ ':' };
+        hasKey1 = true;
       }
-      in >> sep{ ')' };
-      if (!(hasKey1 && hasKey2 && hasKey3))
+      else if (keyName == "key2")
+      {
+        if (hasKey2)
+        {
+          in.setstate(std::ios::failbit);
+          break;
+        }
+        in >> ull{ input.key2 } >> sep{ ':' };
+        hasKey2 = true;
+      }
+      else if (keyName == "key3")
+      {
+        if (hasKey3)
+        {
+          in.setstate(std::ios::failbit);
+          break;
+        }
+        in >> str{ input.key3 } >> sep{ ':' };
+        hasKey3 = true;
+      }
+      else
       {
         in.setstate(std::ios::failbit);
       }
     }
-    if (in)
+    in >> sep{ ')' };
+    if (!(hasKey1 && hasKey2 && hasKey3))
     {
-      dest = input;
+      in.setstate(std::ios::failbit);
     }
-    return in;
   }
-  std::ostream& operator<<(std::ostream& out, const DataStruct& toOut)
+  if (in)
   {
-    std::ostream::sentry sentry(out);
-    if (!sentry)
-    {
-      return out;
-    }
-    StreamGuard outGuard(out);
-    out << "(";
-    out << ":key1 " << detail::DoubleO{ toOut.key1 };
-    out << ":key2 " << detail::UnsignedLongLongO{ toOut.key2 };
-    out << ":key3 " << detail::StringO{ toOut.key3 };
-    out << ":)";
+    dest = input;
+  }
+  return in;
+}
+std::ostream& nikonov::operator<<(std::ostream& out, const DataStruct& toOut)
+{
+  std::ostream::sentry sentry(out);
+  if (!sentry)
+  {
     return out;
   }
+  StreamGuard outGuard(out);
+  out << "(";
+  out << ":key1 " << detail::DoubleO{ toOut.key1 };
+  out << ":key2 " << detail::UnsignedLongLongO{ toOut.key2 };
+  out << ":key3 " << detail::StringO{ toOut.key3 };
+  out << ":)";
+  return out;
 }
