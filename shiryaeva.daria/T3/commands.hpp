7836c89b2ae6d7c5e18fce33673eb7519e9fa339
Дарья <shyriaevaea@gmail.com>
2025-09-15 00:00:12 +0300

    fix7

--- shiryaeva.daria/T3/commands.hpp
+++ shiryaeva.daria/T3/commands.hpp
@@ -1,9 +1,10 @@
 #ifndef COMMANDS_HPP
 #define COMMANDS_HPP
 
-#include "polygon.hpp"
+
 #include <iostream>
 #include <string>
+#include "polygon.hpp"
 
 constexpr size_t RECTANGLE_SIDES = 4;
 constexpr double EPS = 1e-9;
@@ -12,7 +13,7 @@ namespace shiryaeva
 {
   bool hasVertexCount(const Polygon &p, size_t count);
   bool isRectangle(const Polygon &polygon);
-  bool HasDuplicates(const Polygon& p)
+  bool HasDuplicates(const Polygon& p);
   void area(std::istream& in, std::ostream& out, const std::vector< Polygon >& polygons);
   void max(std::istream& in, std::ostream& out, const std::vector< Polygon >& polygons);
   void min(std::istream& in, std::ostream& out, const std::vector< Polygon >& polygons);
