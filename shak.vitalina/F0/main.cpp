f875145ee63742d7caf13875d4bfcaf15bca8918
vitsh1 <vips65653@gmail.com>
2025-09-17 00:16:12 +0300

    fix fix unused out

--- shak.vitalina/F0/main.cpp
+++ shak.vitalina/F0/main.cpp
@@ -10,12 +10,12 @@ int main()
   using namespace shak;
   shak::Dictionaries dicts;
   std::map< std::string, std::function< void() > > cmds;
-  cmds["create"] = std::bind(createDict, std::ref(dicts), std::ref(std::cin), std::ref(std::cout));
-  cmds["clear"] = std::bind(clearDict, std::ref(dicts), std::ref(std::cin), std::ref(std::cout));
-  cmds["delete"] = std::bind(deleteDict, std::ref(dicts), std::ref(std::cin), std::ref(std::cout));
-  cmds["insert"] = std::bind(insertW, std::ref(dicts), std::ref(std::cin), std::ref(std::cout));
-  cmds["remove"] = std::bind(removeW, std::ref(dicts), std::ref(std::cin), std::ref(std::cout));
+  cmds["create"] = std::bind(createDict, std::ref(dicts), std::ref(std::cin));
+  cmds["clear"] = std::bind(clearDict, std::ref(dicts), std::ref(std::cin));
+  cmds["delete"] = std::bind(deleteDict, std::ref(dicts), std::ref(std::cin));
   cmds["print"] = std::bind(printDict, std::ref(dicts), std::ref(std::cin), std::ref(std::cout));
+  cmds["insert"] = std::bind(insertW, std::ref(dicts), std::ref(std::cin));
+  cmds["remove"] = std::bind(removeW, std::ref(dicts), std::ref(std::cin));
   cmds["frequency"] = std::bind(getFreq, std::ref(dicts), std::ref(std::cin), std::ref(std::cout));
   cmds["uniqe"] = std::bind(getUniqe, std::ref(dicts), std::ref(std::cin), std::ref(std::cout));
   cmds["equal"] = std::bind(equal, std::ref(dicts), std::ref(std::cin), std::ref(std::cout));
