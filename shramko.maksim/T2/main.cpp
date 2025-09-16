868f09e165bae5926eeb5da2e4c6a60111d7fa13
Maksim_Shramko <maxramko@gmail.com>
2025-09-16 18:18:32 +0300

    Creating main

--- shramko.maksim/T2/main.cpp
+++ shramko.maksim/T2/main.cpp
@@ -1,2 +1,37 @@
+#include <algorithm>
+#include <limits>
+#include <iterator>
+#include <iostream>
+#include <vector>
+
+#include "dataStruct.hpp"
+
 int main()
-{}
+{
+  using namespace shramko;
+  using iIterator = std::istream_iterator< dataStruct >;
+  using oIterator = std::ostream_iterator< dataStruct >;
+
+  std::vector< dataStruct > data;
+
+  try
+  {
+    std::copy(iIterator(std::cin), iIterator(), std::back_inserter(data));
+
+    while (!std::cin.eof() && std::cin.fail())
+    {
+      std::cin.clear();
+      std::copy(iIterator(std::cin), iIterator(), std::back_inserter(data));
+    }
+
+    std::sort(data.begin(), data.end());
+    std::copy(data.begin(), data.end(), oIterator(std::cout, "\n"));
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << "Error: " << e.what() << '\n';
+    return 1;
+  }
+
+  return 0;
+}
