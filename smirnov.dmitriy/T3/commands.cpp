1419c32f590dc5c390bc10340a158c3454876196
uchuaip <d.smirnov2006@mail.ru>
2025-09-16 14:24:13 +0300

    commands libraries

---
+++ smirnov.dmitriy/T3/commands.cpp
@@ -0,0 +1,9 @@
+#include "commands.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+#include <functional>
+#include <map>
+#include "polygon.hpp"
