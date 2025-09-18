0c47d3cee0e7e1ef7aea2af3fd54af16b5e510fc
widgeiw <crazynas228@gmail.com>
2025-09-18 20:31:52 +0300

    dividing details.h and details.cpp

--- orlova.alena/F0/details.h
+++ orlova.alena/F0/details.h
@@ -2,47 +2,15 @@
 #define DETAILS_H
 
 #include <algorithm>
-#include <iterator>
 #include "commands.h"
 #include "io-utils.h"
 
 namespace orlova
 {
-  bool dictionaryExists(const Dictionaries& dicts, const std::string& name)
-  {
-    return dicts.find(name) != dicts.end();
-  }
-
-  void printTranslations(std::ostream& out, const Translations& translations)
-  {
-    if (translations.empty())
-    {
-      return;
-    }
-    auto it = translations.begin();
-    out << *it << ", ";
-    std::advance(it, 1);
-    std::copy(it, --translations.end(), std::ostream_iterator< std::string >(out, ", "));
-    out << *(--translations.end());
-  }
-
-  Translations intersectLists(Translations& list1, Translations& list2)
-  {
-    list1.sort();
-    list2.sort();
-    Translations res;
-    std::set_intersection(list1.begin(), list1.end(), list2.begin(), list2.end(), std::back_inserter(res));
-    return res;
-  }
-
-  std::pair< std::string, Translations > intersectListDict(const std::pair< std::string, Translations >& pair,
-    const orlova::Dictionary& intersected)
-  {
-    Translations list1 = pair.second;
-    Translations list2 = intersected.at(pair.first);
-    Translations res = intersectLists(list1, list2);
-    return { pair.first, res };
-  }
+  bool dictionaryExists(const Dictionaries&, const std::string&);
+  void printTranslations(std::ostream&, const Translations&);
+  Translations intersectLists(Translations&, Translations&);
+  std::pair< std::string, Translations > intersectListDict(const std::pair< std::string, Translations >&, const orlova::Dictionary&);
 
   struct WordPairGeneratorIterator
   {
