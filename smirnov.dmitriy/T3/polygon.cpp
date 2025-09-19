1cf788e23181001c213f84a99d11591b4881fb66
uchuaip <d.smirnov2006@mail.ru>
2025-09-20 01:00:36 +0300

    polygon fixed2

--- smirnov.dmitriy/T3/polygon.cpp
+++ smirnov.dmitriy/T3/polygon.cpp
@@ -69,18 +69,18 @@ std::istream& smirnov::operator>>(std::istream& in, Polygon& value)
   using inputItT = std::istream_iterator< smirnov::Point >;
   std::vector < Point > vec;
   vec.reserve(n);
-  std::copy_n(inputItT{ in }, n , std::back_inserter(vec));
+  std::copy_n(inputItT{ in }, n - 1, std::back_inserter(vec));
   if (in.peek() != '\n')
   {
     std::copy_n(inputItT{ in }, 1, std::back_inserter(vec));
   }
-  if (in && vec.size() == n && in.peek() == '\n')
+  if (!in || vec.size() != n || in.peek() != '\n')
   {
-    value = Polygon{ vec };
+    in.setstate(std::ios::failbit);
   }
   else
   {
-    in.setstate(std::ios::failbit);
+    value = Polygon{ vec };
   }
   return in;
 }
