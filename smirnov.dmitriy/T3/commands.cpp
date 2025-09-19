cc02da37449480753ce042d47d04a8c6d1cc3533
uchuaip <d.smirnov2006@mail.ru>
2025-09-20 00:09:40 +0300

    commandsfix

--- smirnov.dmitriy/T3/commands.cpp
+++ smirnov.dmitriy/T3/commands.cpp
@@ -26,6 +26,7 @@ namespace smirnov
     double result = 0.0;
     std::map< std::string, std::function< double() > > subcommand;
     {
+      using namespace std::placeholders;
       subcommand["EVEN"] = std::bind(doAreaEven, polygons);
       subcommand["ODD"] = std::bind(doAreaOdd, polygons);
       subcommand["MEAN"] = std::bind(doAreaMean, polygons);
@@ -43,6 +44,7 @@ namespace smirnov
     {
       result = subcommand[s]();
     }
+    std::vector< double > areas;
     out << std::fixed << std::setprecision(1) << result << "\n";
   }
 
@@ -157,119 +159,23 @@ namespace smirnov
     }
   }
 
-  bool onSegment(const smirnov::Point& p, const smirnov::Point& q, const smirnov::Point& r) {
-    return q.x <= std::max(p.x, r.x) && q.x >= std::min(p.x, r.x) &&
-           q.y <= std::max(p.y, r.y) && q.y >= std::min(p.y, r.y);
-  }
-
-  int orientation(const smirnov::Point& p, const smirnov::Point& q, const smirnov::Point& r) {
-    long long val = static_cast<long long>(q.y - p.y) * (r.x - q.x) -
-                    static_cast<long long>(q.x - p.x) * (r.y - q.y);
-    if (val == 0) return 0;
-    return (val > 0) ? 1 : 2;
-  }
-
-  bool doIntersect(const smirnov::Point& p1, const smirnov::Point& q1,
-                   const smirnov::Point& p2, const smirnov::Point& q2) {
-    int o1 = orientation(p1, q1, p2);
-    int o2 = orientation(p1, q1, q2);
-    int o3 = orientation(p2, q2, p1);
-    int o4 = orientation(p2, q2, q1);
-
-    if (o1 != o2 && o3 != o4) return true;
-    if (o1 == 0 && onSegment(p1, p2, q1)) return true;
-    if (o2 == 0 && onSegment(p1, q2, q1)) return true;
-    if (o3 == 0 && onSegment(p2, p1, q2)) return true;
-    if (o4 == 0 && onSegment(p2, q1, q2)) return true;
-
-    return false;
-  }
-
-  bool pointOnEdge(const Polygon& poly, const smirnov::Point& p)
+  bool hasIntersection(const Polygon &p1, const Polygon &p2)
   {
-    for (size_t i = 0; i < poly.points.size(); ++i) {
-      smirnov::Point a = poly.points[i];
-      smirnov::Point b = poly.points[(i + 1) % poly.points.size()];
-
-      long long cross = (p.y - a.y) * (b.x - a.x) - (p.x - a.x) * (b.y - a.y);
-      if (cross == 0 &&
-          std::min(a.x, b.x) <= p.x && p.x <= std::max(a.x, b.x) &&
-          std::min(a.y, b.y) <= p.y && p.y <= std::max(a.y, b.y)) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  bool isPointInside(const Polygon& poly, const smirnov::Point& p)
-  {
-    if (pointOnEdge(poly, p)) {
-      return true;
-    }
-    bool inside = false;
-    for (size_t i = 0, j = poly.points.size() - 1; i < poly.points.size(); j = i++) {
-      const smirnov::Point& pi = poly.points[i];
-      const smirnov::Point& pj = poly.points[j];
-      bool intersect = ((pi.y > p.y) != (pj.y > p.y)) &&
-                       (p.x < (pj.x - pi.x) * (p.y - pi.y) / double(pj.y - pi.y) + pi.x);
-      if (intersect)
-        inside = !inside;
-    }
-    return inside;
-  }
-
-  bool hasIntersection(const Polygon& first, const Polygon& second)
-  {
-    for (size_t i = 0; i < first.points.size(); ++i) {
-      smirnov::Point a1 = first.points[i];
-      smirnov::Point a2 = first.points[(i + 1) % first.points.size()];
-      for (size_t j = 0; j < second.points.size(); ++j) {
-        smirnov::Point b1 = second.points[j];
-        smirnov::Point b2 = second.points[(j + 1) % second.points.size()];
-        if (doIntersect(a1, a2, b1, b2)) {
-          return true;
-        }
-      }
-    }
-    for (const auto& p1 : first.points) {
-      for (const auto& p2 : second.points) {
-        if (p1.x == p2.x && p1.y == p2.y) {
-          return true;
-        }
-      }
-    }
-    for (const auto& p : second.points) {
-      if (isPointInside(first, p)) return true;
-    }
-    for (const auto& p : first.points) {
-      if (isPointInside(second, p)) return true;
-    }
-    return false;
+    auto pair_p1 = std::minmax_element(p1.points.cbegin(), p1.points.cend());
+    auto pair_p2 = std::minmax_element(p2.points.cbegin(), p2.points.cend());
+    return !(*pair_p1.second < *pair_p2.first || *pair_p2.second < *pair_p1.first);
   }
 
   void doIntersections(const std::vector< Polygon > & data, std::istream & in, std::ostream & out)
   {
-    std::string rest;
-    std::getline(in, rest);
-    if (rest.empty())
-    {
-      throw std::logic_error("Wrong argument");
-    }
-
-    std::istringstream iss(rest);
     Polygon polygon;
-    if (!(iss >> polygon))
-    {
-      throw std::logic_error("Wrong argument");
-    }
-    iss >> std::ws;
-    if (!iss.eof())
+    in >> polygon;
+    if (!in)
     {
       throw std::logic_error("Wrong argument");
     }
 
-    auto isIntersected = std::bind(hasIntersection, std::cref(polygon), _1);
-    out << std::count_if(data.begin(), data.end(), isIntersected) << '\n';
+    out << std::count_if(data.begin(), data.end(), std::bind(hasIntersection,  std::placeholders::_1, polygon)) << '\n';
   }
 
   bool PolygonMaxSeq::operator()(const Polygon& polygon, const Polygon& data)
@@ -325,4 +231,3 @@ namespace smirnov
     return getSumArea(polygons, std::bind(isNCountVertexes, _1, n));
   }
 }
-
