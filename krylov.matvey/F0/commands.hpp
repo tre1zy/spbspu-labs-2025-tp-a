eb24190f6f339cf1b52784e7ec844f78f52e3d4d
root <root@kryloff.>
2025-09-20 04:03:54 +0300

    Commands interface done with processor

---
+++ krylov.matvey/F0/commands.hpp
@@ -0,0 +1,40 @@
+#ifndef COMMANDS_HPP
+#define COMMANDS_HPP
+
+#include <string>
+#include <iosfwd>
+#include <functional>
+#include "index.hpp"
+
+namespace krylov
+{
+  class CommandProcessor
+  {
+  public:
+    CommandProcessor(std::ostream& output);
+    void execute(const std::string& line);
+  private:
+    IndexStorage storage_;
+    std::ostream& out_;
+    std::map< std::string, IndexDocument > indexes_;
+    std::map< std::string, std::function< void(const std::vector< std::string >&) > > commands;
+    void initializeCommands();
+    void createIndex(const std::string& indexName, const std::string& filename);
+    void searchWord(const std::string& indexName, const std::string& word);
+    void showIndex(const std::string& indexName);
+    void clearIndex(const std::string& indexName);
+    void listIndexes();
+    void mergeIndexes(const std::string& index1, const std::string& index2, const std::string& newIndex);
+    void mergeByLines(const std::string& index1, const std::string& index2, const std::string& newIndex);
+
+    void createIndexCmd(const std::vector< std::string >& args);
+    void showIndexCmd(const std::vector< std::string >& args);
+    void searchWordCmd(const std::vector< std::string >& args);
+    void mergeIndexesCmd(const std::vector< std::string >& args);
+    void mergeByLinesCmd(const std::vector< std::string >& args);
+    void listIndexesCmd(const std::vector< std::string >& args);
+    void clearIndexCmd(const std::vector< std::string >& args);
+  };
+}
+
+#endif
