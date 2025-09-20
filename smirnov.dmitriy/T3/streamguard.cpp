67e209a2c9da620036afac025797756c3992f0e3
uchuaip <d.smirnov2006@mail.ru>
2025-09-20 18:20:19 +0300

    Fdsf

--- smirnov.dmitriy/T3/streamguard.cpp
+++ smirnov.dmitriy/T3/streamguard.cpp
@@ -1,12 +1,12 @@
 #include "streamguard.hpp"
 
-smirnov::streamguard::streamguard(std::basic_ios< char >& s):
+smirnov::StreamGuard::StreamGuard(std::basic_ios< char >& s):
   s_(s),
   precision_(s.precision()),
   flags_(s.flags())
   {}
 
-smirnov::streamguard::~streamguard()
+smirnov::StreamGuard::~StreamGuard()
 {
     s_.precision(precision_);
     s_.flags(flags_);
