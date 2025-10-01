aa648db01e22cb38511ef6e0d81c5c7dc1f2267b
abdelrhmanbaha <152448589+abdelrhmanbaha@users.noreply.github.com>
2025-10-01 06:59:04 +0300

    q

--- hegazy.abdelrhman/T3/geometry.cpp
+++ hegazy.abdelrhman/T3/geometry.cpp
@@ -65,14 +65,12 @@ namespace geom
     }
     std::vector<Point> pts;
     pts.reserve(count);
-    std::streampos start = in.tellg();
     for (size_t i = 0; i < count; ++i)
     {
       Point p;
       if (!(in >> p))
       {
         in.setstate(std::ios::failbit);
-        in.seekg(start);
         return in;
       }
       pts.push_back(p);
@@ -80,7 +78,6 @@ namespace geom
     if (pts.size() != count)
     {
       in.setstate(std::ios::failbit);
-      in.seekg(start);
       return in;
     }
     poly.points = std::move(pts);
