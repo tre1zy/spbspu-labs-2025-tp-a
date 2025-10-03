907c6cffb4985c41e92dc11ddd30d9e3716043e1
identicalaffiliation <fre1fsi.biz.c@gmail.com>
2025-10-03 17:16:23 +0300

    huffman structure realisation add

---
+++ mazitov.vladislav/F0/dataset.hpp
@@ -0,0 +1,32 @@
+#ifndef DATASET_HPP
+#define DATASET_HPP
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <cstddef>
+
+namespace mazitov
+{
+  struct DataSet
+  {
+    std::string originalText;
+    std::unordered_map< char, std::string > huffCodes;
+  };
+
+  class DataSetManager
+  {
+  public:
+    bool createDataSet(const std::string &);
+    bool dataSetExists(const std::string &) const;
+    bool deleteDataSet(const std::string &);
+    bool compressDataSet(const std::string &);
+    DataSet* getDataSet(const std::string &);
+    std::string getCompressedData(const std::string &) const;
+  private:
+    std::map< std::string, DataSet > dataSets;
+  };
+}
+
+#endif
