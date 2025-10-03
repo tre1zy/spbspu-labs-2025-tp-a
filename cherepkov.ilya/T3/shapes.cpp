c44d729290a7a7cc61270550688b05d57a8134e1
SparklePussy <ilyacherepkov11@gmail.com>
2025-10-03 23:02:12 +0300

    fix ...

--- cherepkov.ilya/T3/shapes.cpp
+++ cherepkov.ilya/T3/shapes.cpp
@@ -34,38 +34,25 @@ std::istream& cherepkov::operator>>(std::istream& in, Point& point)
 
 std::istream& cherepkov::operator>>(std::istream& in, Polygon& polygon)
 {
-  using namespace std::placeholders;
-  using input_it = std::istream_iterator< Point >;
-
-  std::istream::sentry sent(in);
-  if (!sent)
+  std::istream::sentry sentry(in);
+  if (!sentry)
   {
     return in;
   }
-
-  size_t vertex_count;
-  in >> vertex_count;
-  if (!in || vertex_count < 3)
+  size_t count = 0;
+  if (!(in >> count) || count < 3)
   {
     in.setstate(std::ios::failbit);
     return in;
   }
-
-  std::vector< Point > points;
-  auto reader = std::bind(handleNewLine, std::ref(in), _1);
-
-  std::transform(input_it{in}, input_it{}, std::back_inserter(points), reader);
-
-  if (points.size() == vertex_count && in.eof())
-  {
-    in.clear();
-    polygon.points = std::move(points);
-  }
-  else
+  std::vector< Point > temp(count);
+  std::copy_n(std::istream_iterator< Point >(in), count, temp.begin());
+  if (!in)
   {
     in.setstate(std::ios::failbit);
+    return in;
   }
-
+  polygon.points = std::move(temp);
   return in;
 }
 
