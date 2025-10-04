29900382fe5dcde6bcfc9b42cd4fae65f209be1d
cherkasoov <cherkasov5509@gmail.com>
2025-10-04 12:13:46 +0300

    ...

--- cherkasov.nikita/common/streamGuard.hpp
+++ cherkasov.nikita/common/streamGuard.hpp
@@ -10,7 +10,6 @@ namespace cherkasov
     StreamGuard(std::basic_ios< char >& s);
     StreamGuard(const StreamGuard&) = delete;
     StreamGuard(StreamGuard&&) = delete;
-    explicit StreamGuard(std::basic_ios< char >& s);
     ~StreamGuard();
   private:
     std::basic_ios< char >& s_;
