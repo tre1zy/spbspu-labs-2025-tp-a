bf0386a356d47053be834c44fcb435806171e7d9
abdelrhmanbaha <152448589+abdelrhmanbaha@users.noreply.github.com>
2025-10-04 16:56:14 +0300

    fix

--- hegazy.abdelrhman/F0/main.cpp
+++ hegazy.abdelrhman/F0/main.cpp
@@ -9,55 +9,55 @@
 
 int main(int argc, char* argv[])
 {
-    using namespace bob;
-    Dicts dicts;
-    if (argc == 2)
+  using namespace bob;
+  Dicts dicts;
+  if (argc == 2)
+  {
+    if (std::string(argv[1]) == "--help")
     {
-        if (std::string(argv[1]) == "--help")
-        {
-            printHelp(std::cout);
-            return 0;
-        }
-        try
-        {
-            loadFile(std::string(argv[1]), dicts);
-        }
-        catch (const std::exception& e)
-        {
-            std::cerr << e.what() << '\n';
-            return 1;
-        }
+        printHelp(std::cout);
+        return 0;
+    }
+    try
+    {
+        loadFile(std::string(argv[1]), dicts);
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return 1;
+    }
     }
 
-    std::map< std::string, std::function< void() > > cmds;
-    cmds["createdict"] = std::bind(createDictionary, std::ref(std::cin), std::ref(dicts));
-    cmds["showdicts"] = std::bind(showDictionary, std::ref(std::cout), std::cref(dicts));
-    cmds["loadtext"] = std::bind(loadText, std::ref(std::cin), std::ref(dicts));
-    cmds["union"] = std::bind(unionDictionary, std::ref(std::cin), std::ref(dicts));
-    cmds["intersect"] = std::bind(intersectDictionary, std::ref(std::cin), std::ref(dicts));
-    cmds["copy"] = std::bind(copyDictionary, std::ref(std::cin), std::ref(dicts));
-    cmds["addword"] = std::bind(addWord, std::ref(std::cin), std::ref(dicts));
-    cmds["size"] = std::bind(printSize, std::ref(std::cin), std::ref(std::cout), std::cref(dicts));
-    cmds["cleanword"] = std::bind(cleanWord, std::ref(std::cin), std::ref(dicts));
-    cmds["cleandict"] = std::bind(cleanDictionary, std::ref(std::cin), std::ref(dicts));
+  std::map< std::string, std::function< void() > > cmds;
+  cmds["createdict"] = std::bind(createDictionary, std::ref(std::cin), std::ref(dicts));
+  cmds["showdicts"] = std::bind(showDictionary, std::ref(std::cout), std::cref(dicts));
+  cmds["loadtext"] = std::bind(loadText, std::ref(std::cin), std::ref(dicts));
+  cmds["union"] = std::bind(unionDictionary, std::ref(std::cin), std::ref(dicts));
+  cmds["intersect"] = std::bind(intersectDictionary, std::ref(std::cin), std::ref(dicts));
+  cmds["copy"] = std::bind(copyDictionary, std::ref(std::cin), std::ref(dicts));
+  cmds["addword"] = std::bind(addWord, std::ref(std::cin), std::ref(dicts));
+  cmds["size"] = std::bind(printSize, std::ref(std::cin), std::ref(std::cout), std::cref(dicts));
+  cmds["cleanword"] = std::bind(cleanWord, std::ref(std::cin), std::ref(dicts));
+  cmds["cleandict"] = std::bind(cleanDictionary, std::ref(std::cin), std::ref(dicts));
 
-    cmds["top"] = [&]() {
-        printTopRare(std::ref(std::cin), std::ref(std::cout), std::cref(dicts), "descending");
-        };
-    cmds["rare"] = [&]() {
-        printTopRare(std::ref(std::cin), std::ref(std::cout), std::cref(dicts), "ascending");
-        };
+  cmds["top"] = [&]() {
+    printTopRare(std::ref(std::cin), std::ref(std::cout), std::cref(dicts), "descending");
+    };
+  cmds["rare"] = [&]() {
+    printTopRare(std::ref(std::cin), std::ref(std::cout), std::cref(dicts), "ascending");
+    };
 
-    cmds["frequency"] = std::bind(printFrequency, std::ref(std::cin), std::ref(std::cout), std::cref(dicts));
-    cmds["wordrange"] = std::bind(createWordRange, std::ref(std::cin), std::ref(dicts));
-    cmds["save"] = std::bind(saveDictionaries, std::ref(std::cin), std::cref(dicts));
+  cmds["frequency"] = std::bind(printFrequency, std::ref(std::cin), std::ref(std::cout), std::cref(dicts));
+  cmds["wordrange"] = std::bind(createWordRange, std::ref(std::cin), std::ref(dicts));
+  cmds["save"] = std::bind(saveDictionaries, std::ref(std::cin), std::cref(dicts));
 
-    std::string command;
-    while (std::cin >> command)
+  std::string command;
+  while (std::cin >> command)
+  {
+    try
     {
-        try
-        {
-            cmds.at(command)();
+         cmds.at(command)();
         }
         catch (const std::out_of_range&)
         {
